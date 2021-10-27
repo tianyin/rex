caa1edfc50b8ba562b0d6cc76ccd840f46133825
Dan Williams <djwillia@vt.edu>
2021-10-27 15:41:41 -0400

    macro for bpf_trace_printk

--- user-framework/rust_test/src/main.rs
+++ user-framework/rust_test/src/main.rs
@@ -4,12 +4,25 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn bpf_test_call() {
     let ptr = 0x0000000000401370 as *const ();
     let code: extern "C" fn() = unsafe { core::mem::transmute(ptr) };
     (code)();
-    loop {}
+}
+
+macro_rules! bpf_trace_printk {
+    ($s:literal,$($a:expr),*;$($t:ty),*) => {
+        let ptr = 0x0000000000401200 as *const ();
+        let code: extern "C" fn(&str, $($t),*) = unsafe { core::mem::transmute(ptr) };
+        code($s, $($a),*);
+    }
+}
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> () {
+    bpf_test_call();
+    bpf_trace_printk!("BPF triggered from PID %d.\n", 29, 10; u32, u32);
 }
 
 /// This function is called on panic.
