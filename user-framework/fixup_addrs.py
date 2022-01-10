2892c79ee1fec3c2b5ce99cdf5b97469b9f579d4
Jinghao Jia <jinghao7@illinois.edu>
2022-01-09 19:48:44 -0600

    add rust support to fixup_addrs.py

--- user-framework/fixup_addrs.py
+++ user-framework/fixup_addrs.py
@@ -8,10 +8,14 @@ funcs = [
     'commit_creds'
 ]
 
-func_ptr_fmt = """static __u64 (*bpf_get_current_pid_tgid)(void) = (void *)0x%s;
+c_func_fmt = """static __u64 (*bpf_get_current_pid_tgid)(void) = (void *)0x%s;
 static long (*bpf_trace_printk)(const char *fmt, __u32 fmt_size, __u64 arg1, __u64 arg2, __u64 arg3) = (void *)0x%s;
 static void *(*prepare_kernel_cred)(void *) = (void *)0x%s;
 static int (*commit_creds)(void *) = (void *)0x%s;
+"""
+
+rs_func_fmt = """pub const STUB_BPF_GET_CURRENT_PID_TGID: u64 = 0x%s;
+pub const STUB_BPF_TRACE_PRINTK: u64 = 0x%s;
 """
 
 def filter_text(nm_line):
@@ -26,8 +30,10 @@ def get_symbols(vmlinux):
 def main(argv):
     text_syms = dict(map(lambda l: (l[2], l[0]),
                      filter(filter_text, get_symbols(argv[1]))))
-    with open('interface-kernel.h', 'w') as out_f:
-        out_f.write(func_ptr_fmt % tuple(map(lambda f: text_syms[f], funcs)))
+    with open('interface-kernel.h', 'w') as fd:
+        fd.write(c_func_fmt % tuple(map(lambda f: text_syms[f], funcs)))
+    with open('rust_test/src/interface.rs', 'w') as fd:
+        fd.write(rs_func_fmt % tuple(map(lambda f: text_syms[f], funcs[:2])))
 
 if __name__ == '__main__':
     main(sys.argv)
