1af551796cbfe0fc3585a9a71b36c918c3afca67
Jinghao Jia <jinghao7@illinois.edu>
2022-02-21 21:35:31 -0600

    simple map impl

---
+++ user-framework/event-trigger.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <linux/unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+	setuid(1000);
+	fprintf(stdout, "pid=%d\n", getpid());
+	fprintf(stdout, "Before: uid=%ld, euid=%ld\n", syscall(__NR_getuid), syscall(__NR_geteuid));
+	syscall(__NR_dup, 1);
+	fprintf(stdout, "After: uid=%ld, euid=%ld\n", syscall(__NR_getuid), syscall(__NR_geteuid));
+	return 0;
+}
