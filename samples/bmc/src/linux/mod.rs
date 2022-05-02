c4a9c268a1824728cd0f8989bbebb66de9f95916
Jinghao Jia <jinghao7@illinois.edu>
2022-05-01 23:02:37 -0500

    move to new infrastructure

--- samples/bmc/src/linux/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod tcp;
-pub mod udp;
-pub mod if_ether;
-pub mod bpf;
-pub mod ip;
--- samples/bmc/src/stub.rs
+++
@@ -1,7 +0,0 @@
-
-pub const STUB_BPF_MAP_LOOKUP_ELEM: u64 = 0xffffffff8124a1c0;
-pub const STUB_BPF_SPIN_LOCK: u64 = 0xffffffff8124a410;
-pub const STUB_BPF_SPIN_UNLOCK: u64 = 0xffffffff8124a480;
-pub const STUB_BPF_XDP_ADJUST_HEAD: u64 = 0xffffffff81c73a00;
-pub const STUB_BPF_XDP_ADJUST_TAIL: u64 = 0xffffffff81c73a70;
-pub const STUB_BPF_TRACE_PRINTK: u64 = 0xffffffff812146d0;
