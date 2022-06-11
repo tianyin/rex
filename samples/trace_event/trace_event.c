2cb204efec3d25e79cc0ee3b39e5884d9484babf
wtj <zhangwt1997@gmail.com>
2022-06-11 17:19:52 +0800

    user: cleanup; wrap with original functions

--- samples/trace_event/trace_event.c
+++ samples/trace_event/trace_event.c
@@ -15,6 +15,7 @@
 #define SAMPLE_FREQ 3
 
 static int pid;
+static int prog_fd;
 
 static inline long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 							int cpu, int  group_fd, unsigned long flags)
@@ -22,9 +23,21 @@ static inline long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
 	return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
 }
 
-int main(void)
-{
-	int base_fd, prog_fd, pmu_fd, trace_pipe_fd;
+static void read_trace_pipe(void) {
+	int trace_pipe_fd;
+
+	trace_pipe_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/trace_pipe",
+		O_RDONLY);
+
+	for (;;) {
+		char c;
+		if (read(trace_pipe_fd, &c, 1) == 1)
+			putchar(c);
+	}
+}
+
+static void test_bpf_perf_event(void) {
+	int pmu_fd;
 
 	// TODO: For now, only one out of the six events.
 	struct perf_event_attr attr_type_sw = {
@@ -35,6 +48,20 @@ int main(void)
 	};
 
 	struct perf_event_attr *attr = &attr_type_sw;
+
+	// TODO: For now, only _all_cpu tests (without _task tests)
+	// TODO: For now, only the first CPU
+	pmu_fd = perf_event_open(attr, -1, 0 /* cpu_idx */, -1, 0);
+
+	ioctl(pmu_fd, PERF_EVENT_IOC_SET_BPF, prog_fd);
+	ioctl(pmu_fd, PERF_EVENT_IOC_ENABLE, 0);
+
+	system("dd if=/dev/zero of=/dev/null count=5000k status=none");
+}
+
+int main(void)
+{
+	int base_fd;
 
 	iu_set_debug(1); // enable debug info
 
@@ -52,27 +79,14 @@ int main(void)
 
 	pid = fork();
 	if (pid == 0) {
-		trace_pipe_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/trace_pipe",
-			O_RDONLY);
-		for (;;) {
-			char c;
-			if (read(trace_pipe_fd, &c, 1) == 1)
-				putchar(c);
-		}
+		read_trace_pipe();
 		return 0;
 	} else if (pid == -1) {
 		printf("couldn't spawn process\n");
 		return -1;
 	}
 
-	// TODO: For now, only _all_cpu tests (without _task tests)
-	// TODO: For now, only the first CPU
-	pmu_fd = perf_event_open(attr, -1, 0 /* cpu_idx */, -1, 0);
-
-	ioctl(pmu_fd, PERF_EVENT_IOC_SET_BPF, prog_fd);
-	ioctl(pmu_fd, PERF_EVENT_IOC_ENABLE, 0);
-
-	system("dd if=/dev/zero of=/dev/null count=5000k status=none");
+	test_bpf_perf_event();
 
 	return 0;
 }
