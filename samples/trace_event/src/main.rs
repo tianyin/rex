5af620f456a1e647e9aa4d3845b697214b6c2748
wtj <zhangwt1997@gmail.com>
2022-06-11 16:06:10 +0800

    kern: remove old demo map and its logic

--- samples/trace_event/src/main.rs
+++ samples/trace_event/src/main.rs
@@ -15,8 +15,6 @@ use crate::helper::*;
 mod linux;
 use crate::linux::bpf::*;
 
-MAP_DEF!(map1, __map_1, i32, i64, BPF_MAP_TYPE_HASH, 1024, 0);
-
 MAP_DEF!(
     counts, __counts,
     key_t, u64, BPF_MAP_TYPE_HASH, 10000, 0
@@ -35,8 +33,6 @@ pub const USER_STACKID_FLAGS: u64 = (0 | BPF_F_FAST_STACK_CMP | BPF_F_USER_STACK
 #[no_mangle]
 #[link_section = "tracepoint/"]
 fn iu_prog1() -> i32 {
-    let key: i32 = 0;
-
     let pe_key: key_t = key_t {
         comm: [0; TASK_COMM_LEN],
         kernstack: 0,
@@ -54,19 +50,6 @@ fn iu_prog1() -> i32 {
         }
     }
 
-    match bpf_map_lookup_elem::<i32, i64>(map1, key) {
-        None => {
-            bpf_trace_printk!("Not found.\n");
-        }
-        Some(val) => {
-            bpf_trace_printk!("Val=%llu.\n", i64: val);
-        }
-    }
-
-    let pid = (bpf_get_current_pid_tgid() & 0xFFFFFFFF) as u32;
-    bpf_trace_printk!("Rust program triggered from PID %u.\n", u32: pid);
-
-    bpf_map_update_elem(map1, key, pid as i64, BPF_ANY.into());
     return 0;
 }
 
