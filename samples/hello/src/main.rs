c7fe2b96bb6a9035a0deaf263da93222ffc8de68
Jinghao Jia <jinghao7@illinois.edu>
2022-10-04 00:08:48 -0500

    samples/hello: use new bpf_trace_printk method
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- samples/hello/src/main.rs
+++ samples/hello/src/main.rs
@@ -7,11 +7,10 @@ extern crate rlibc;
 use core::panic::PanicInfo;
 
 use inner_unikernel_rt::tracepoint::*;
-use inner_unikernel_rt::{bpf_trace_printk, bpf_trace_printk_fn, terminate_str};
 
 pub fn iu_prog1(obj: &tracepoint, ctx: &tp_ctx) -> u32 {
     let pid = (obj.bpf_get_current_pid_tgid() & 0xFFFFFFFF) as u32;
-    bpf_trace_printk!("Rust triggered from PID %u.\n", pid);
+    obj.bpf_trace_printk("Rust triggered from PID %u.\n", pid as u64, 0, 0);
     return 0;
 }
 
