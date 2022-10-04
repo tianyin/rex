f5bb4f4b89c8d8c4b030269f0e29d821da0d6c28
Dan Williams <djwillia@vt.edu>
2022-10-04 09:03:03 -0400

    debug qemu

--- qemu-run.sh
+++ qemu-run.sh
@@ -28,7 +28,7 @@ sudo $QEMU -M microvm,rtc=on \
     -netdev tap,id=n0,ifname=$TAP,script=no,downscript=no \
     -no-reboot \
     -no-acpi \
-    2>&1 > /dev/null &
+#    2>&1 > /dev/null &
 #    -s -S \
 
 
