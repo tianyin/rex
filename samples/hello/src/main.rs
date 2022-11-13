3163fc04cc9c7b517739599bde8130ca0e094ec4
Jinghao Jia <jinghao7@illinois.edu>
2022-11-12 20:49:54 -0600

    samples/hello: cleanup after using new rt crate
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- samples/hello/src/main.rs
+++ samples/hello/src/main.rs
@@ -4,11 +4,9 @@
 extern crate inner_unikernel_rt;
 extern crate rlibc;
 
-use core::panic::PanicInfo;
-
 use inner_unikernel_rt::tracepoint::*;
 
-pub fn iu_prog1_fn(obj: &tracepoint, ctx: &tp_ctx) -> u32 {
+fn iu_prog1_fn(obj: &tracepoint, ctx: &tp_ctx) -> u32 {
     let pid = (obj.bpf_get_current_pid_tgid() & 0xFFFFFFFF) as u32;
     obj.bpf_trace_printk("Rust triggered from PID %u.\n", pid as u64, 0, 0);
     return 0;
