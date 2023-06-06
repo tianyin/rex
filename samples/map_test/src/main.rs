141e5f98f3046854718d9f301ceacf22e1e53484
Ruowen Qin <chinqrw@gmail.com>
2023-06-06 16:24:08 -0500

    samples/map_test: update unwrap to .as

--- samples/map_test/src/main.rs
+++ samples/map_test/src/main.rs
@@ -15,18 +15,14 @@ MAP_DEF!(map_array, __map_2, u32, u64, BPF_MAP_TYPE_ARRAY, 256, 0);
 fn map_test1<const MT: bpf_map_type, K, V>(obj: &tracepoint, map: &IUMap<MT, K, V>) -> u32 {
     let key: u32 = 0;
 
-    bpf_printk!(
-        obj,
-        "Map Testing 1 Start with key %u\n",
-        key.try_into().unwrap()
-    );
+    bpf_printk!(obj, "Map Testing 1 Start with key %u\n", key as u64);
 
     match obj.bpf_map_lookup_elem(map_hash, key) {
         None => {
             bpf_printk!(obj, "Not found.\n");
         }
         Some(val) => {
-            bpf_printk!(obj, "Found Val=%llu.\n", (*val).try_into().unwrap());
+            bpf_printk!(obj, "Found Val=%llu.\n", (*val) as u64);
         }
     }
 
@@ -37,7 +33,7 @@ fn map_test1<const MT: bpf_map_type, K, V>(obj: &tracepoint, map: &IUMap<MT, K, 
     };
     bpf_printk!(obj, "Rust program triggered from PID %llu\n", pid as u64);
 
-    obj.bpf_map_update_elem(map_hash, key, pid as i64, BPF_ANY.try_into().unwrap());
+    obj.bpf_map_update_elem(map_hash, key, pid as i64, BPF_ANY.into());
     obj.bpf_trace_printk("Map Updated\n", 0, 0, 0);
 
     match obj.bpf_map_lookup_elem(map_hash, key) {
@@ -45,7 +41,7 @@ fn map_test1<const MT: bpf_map_type, K, V>(obj: &tracepoint, map: &IUMap<MT, K, 
             bpf_printk!(obj, "Not found.\n");
         }
         Some(val) => {
-            bpf_printk!(obj, "Found Val=%llu.\n", (*val).try_into().unwrap());
+            bpf_printk!(obj, "Found Val=%llu.\n", (*val) as u64);
         }
     }
 
@@ -57,7 +53,7 @@ fn map_test1<const MT: bpf_map_type, K, V>(obj: &tracepoint, map: &IUMap<MT, K, 
             bpf_printk!(obj, "Not found.\n");
         }
         Some(val) => {
-            bpf_printk!(obj, "Found Val=%llu.\n", (*val).try_into().unwrap());
+            bpf_printk!(obj, "Found Val=%llu.\n", (*val) as u64);
         }
     }
 
