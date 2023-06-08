ca1c347db709727dc10a6347dcd471ba85405d1b
Ruowen Qin <chinqrw@gmail.com>
2023-06-07 21:38:01 -0500

    inner_unikernel_rt: add new ebpf rewrite function bpf_ktime_get_ns and bpf_ktime_get_boot_ns

---
+++ inner_unikernel_rt/src/seqlock.rs
@@ -0,0 +1,14 @@
+use crate::stub;
+
+
+// typedef struct seqcount {
+// 	unsigned sequence;
+// #ifdef CONFIG_DEBUG_LOCK_ALLOC
+// 	struct lockdep_map dep_map;
+// #endif
+// } seqcount_t;
+
+
+struct SeqcountLatchType {
+    field: Type
+}
