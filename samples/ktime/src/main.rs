8d6e5ded6f19a95814b74a5f4e6c05611cecbf94
Ruowen Qin <chinqrw@gmail.com>
2023-06-15 00:50:32 -0500

    samples/ktime: add test case for new added bpf_ktime_get_coarse_ns() and bpf_get_prandom_u32()

--- samples/ktime/src/main.rs
+++ samples/ktime/src/main.rs
@@ -23,6 +23,12 @@ fn iu_prog1_fn(obj: &tracepoint, ctx: &tp_ctx) -> u32 {
     bpf_printk!(obj, "Origin Boot Time: %llu\n", origin_boot_time);
     assert!(origin_boot_time - boot_time < u64::MAX / 10000);
 
+    let coarse_time = obj.bpf_ktime_get_coarse_ns();
+    bpf_printk!(obj, "Coarse Time: %llu\n", coarse_time);
+
+    let u32_random = obj.bpf_get_prandom_u32();
+    bpf_printk!(obj, "Random: %llu\n", u32_random as u64);
+
     0
 }
 
