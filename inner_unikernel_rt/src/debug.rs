1c2aefb02253037a5ffb57adabc0c9cddcb0a2da
Ruowen Qin <chinqrw@gmail.com>
2023-08-02 22:15:49 -0500

    inner_unikernel_rt/xdp: add eth, ip, tcp, udp header struct helpers for xdp
    Now inner_unikernel can parse the network header struct from xdp ctx

--- inner_unikernel_rt/src/debug.rs
+++ inner_unikernel_rt/src/debug.rs
@@ -5,6 +5,7 @@ use core::ffi::{c_int, CStr};
 
 use crate::stub;
 
+// WARN must add "\0" at the end of &str, c_str is differ from rust str
 #[allow(improper_ctypes_definitions)]
 pub(crate) unsafe extern "C" fn printk(fmt: &str, mut ap: ...) -> c_int {
     let printk_kern: extern "C" fn(*const u8, ...) -> i32 =
--- inner_unikernel_rt/src/xdp/xdp_impl.rs
+++ inner_unikernel_rt/src/xdp/xdp_impl.rs
@@ -1,20 +1,30 @@
-use crate::bindings::linux::kernel::xdp_buff;
-use crate::bindings::uapi::linux::bpf::{bpf_map_type, BPF_PROG_TYPE_XDP};
-use crate::map::*;
-use crate::prog_type::iu_prog;
+use crate::base_helper::bpf_trace_printk;
 use crate::stub;
-use core::ffi::{c_char, c_void};
-use core::slice;
+
+use crate::bindings::linux::kernel::{ethhdr, iphdr, tcphdr, udphdr, xdp_buff};
+use crate::bindings::uapi::linux::bpf::bpf_map_type;
+// use crate::bindings::uapi::linux::r#in::{IPPROTO_TCP, IPPROTO_UDP};
+use crate::debug::printk;
+use crate::prog_type::iu_prog;
+use crate::{bpf_printk, map::*};
+use core::ffi::{c_char, c_uint, c_void};
+use core::{mem, slice};
+
+// expose the following constants to the user
+pub use crate::bindings::uapi::linux::bpf::{
+    BPF_PROG_TYPE_SCHED_CLS, BPF_PROG_TYPE_XDP, XDP_ABORTED, XDP_DROP,
+    XDP_PASS, XDP_REDIRECT, XDP_TX,
+};
 
 // pub type pt_regs = super::binding::pt_regs;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct xdp_md<'b> {
     // TODO check the kernel version xdp_md
     // pub regs: bpf_user_pt_regs_t,
     data: usize,
     data_end: usize,
-    pub data_slice: &'b [c_void],
+    pub data_slice: &'b [c_char],
     pub data_length: usize,
     pub data_meta: usize,
     pub ingress_ifindex: u32,
@@ -36,7 +46,7 @@ pub struct xdp_md<'b> {
 pub struct xdp<'a> {
     rtti: u64,
     // TODO update it
-    prog: fn(&Self, &mut xdp_md) -> u32,
+    prog: fn(&Self, &xdp_md) -> u32,
     name: &'a str,
 }
 
@@ -45,11 +55,12 @@ impl<'a> xdp<'a> {
 
     pub const fn new(
         // TODO update based on signature
-        f: fn(&xdp<'a>, &mut xdp_md) -> u32,
+        f: fn(&xdp<'a>, &xdp_md) -> u32,
         nm: &'a str,
+        rtti: u64,
     ) -> xdp<'a> {
         Self {
-            rtti: BPF_PROG_TYPE_XDP as u64,
+            rtti,
             prog: f,
             name: nm,
         }
@@ -64,20 +75,25 @@ impl<'a> xdp<'a> {
             &*core::mem::transmute::<*const (), *const xdp_buff>(ctx)
         };
 
-        // BUG may not work since directly truncate the pointer to u32
+        // BUG may not work since directly truncate the pointer
         let data = kptr.data as usize;
         let data_end = kptr.data_end as usize;
         let data_meta = kptr.data_meta as usize;
-        let data_length = (data_end - data) as usize;
+        let data_length = data_end - data;
 
         // TODO should we use slice::from_raw_parts or slice::copy_from_slice?
         let data_slice = unsafe {
-            slice::from_raw_parts(kptr.data as *const c_void, data_length)
+            slice::from_raw_parts(kptr.data as *const c_char, data_length)
         };
 
         let ingress_ifindex = kptr.rxq as u32;
+
         let rx_qeueu_index = unsafe { (*kptr.rxq).queue_index };
-        let egress_ifindex = unsafe { (*(*kptr.txq).dev).ifindex as u32 };
+
+        // TODO https://elixir.bootlin.com/linux/v5.15.123/source/net/core/filter.c#L8271
+        // egress_ifindex is valid only for BPF_XDP_DEVMAP option
+        let egress_ifindex = 0;
+        // let egress_ifindex = unsafe { (*(*kptr.txq).dev).ifindex as u32 };
 
         xdp_md {
             data,
@@ -97,6 +113,86 @@ impl<'a> xdp<'a> {
         let helper: extern "C" fn(*mut xdp_buff, i32) -> i32 =
             unsafe { core::mem::transmute(stub::bpf_xdp_adjust_head_addr()) };
         helper(xdp, offset)
+    }
+
+    // User can get the customized struct like memcached from the data_slice
+    pub fn convert_slice_to_struct<T>(&self, slice: &[c_char]) -> T {
+        let ptr = slice.as_ptr() as *const T;
+        unsafe { core::ptr::read_unaligned(ptr) }
+    }
+
+    pub fn udp_header(&self, ctx: &xdp_md) -> udphdr {
+        // WARN this assumes packet has ethhdr and iphdr
+        let begin = mem::size_of::<ethhdr>() + mem::size_of::<iphdr>();
+        let part = &ctx.data_slice[begin..];
+        let udp_header = self.convert_slice_to_struct::<udphdr>(part);
+
+        bpf_trace_printk(
+            "port number %d",
+            u16::from_be(udp_header.dest) as u64,
+            0,
+            0,
+        );
+
+        udp_header
+    }
+
+    pub fn tcp_header(&self, ctx: &xdp_md) -> tcphdr {
+        // WARN this assumes packet has ethhdr and iphdr
+        let begin = mem::size_of::<ethhdr>() + mem::size_of::<iphdr>();
+        let part = &ctx.data_slice[begin..];
+        let tcp_header = self.convert_slice_to_struct::<tcphdr>(part);
+
+        bpf_trace_printk(
+            "port number %d\n",
+            u16::from_be(tcp_header.dest) as u64,
+            0,
+            0,
+        );
+
+        unsafe {
+            if 22 == u16::from_be(tcp_header.dest) {
+                printk("dest port 22\n\0");
+            }
+            // TODO why the printk output is not correct?
+            printk("tcp_dest: %d\n\0", u16::from_be(tcp_header.dest) as u32);
+            printk(
+                "tcp_dest port: %llu\n\0",
+                u16::from_be(tcp_header.dest) as u64,
+            );
+        }
+
+        tcp_header
+    }
+
+    pub fn ip_header(&self, ctx: &xdp_md) -> iphdr {
+        // WARN this assumes packet has ethhdr
+        let begin = mem::size_of::<ethhdr>();
+        let part = &ctx.data_slice[begin..];
+        let ip_header = self.convert_slice_to_struct::<iphdr>(part);
+
+        ip_header
+    }
+
+    pub fn eth_header(&self, ctx: &xdp_md) -> ethhdr {
+        let eth_header = self.convert_slice_to_struct::<ethhdr>(ctx.data_slice);
+        unsafe {
+            printk("eth_source: 0x%llx\n\0", eth_header.h_source);
+            printk("eth_dst: 0x%llx\n\0", eth_header.h_dest);
+            printk(
+                "eth_proto: 02%x\n\0",
+                u16::from_be(eth_header.h_proto) as c_uint,
+            );
+        }
+
+        eth_header
+    }
+
+    pub fn trigger_func(&self, ctx: &xdp_md) {
+        let eth_header = self.eth_header(ctx);
+        unsafe {
+            printk("eth_header: %s\n", eth_header.h_dest);
+        }
     }
 
     pub fn bpf_xdp_adjust_tail(&self, xdp: &mut xdp_buff, offset: i32) -> i32 {
