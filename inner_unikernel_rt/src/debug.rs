082622deecd2e7f1b407ac937d553765045dc369
Ruowen Qin <chinqrw@gmail.com>
2023-08-03 21:06:04 -0500

    inner_unikernel_rt/debug: fixed printk() which doesn't correctly process the va_list

--- inner_unikernel_rt/src/debug.rs
+++ inner_unikernel_rt/src/debug.rs
@@ -1,14 +1,15 @@
 use core::any::Any;
 use core::ffi::VaList;
 
-use core::ffi::{c_int, CStr};
+use core::ffi::{c_char, c_int, CStr};
 
 use crate::stub;
 
-// WARN must add "\0" at the end of &str, c_str is differ from rust str
+// WARN must add "\0" at the end of &str, c_str is different from rust str
 #[allow(improper_ctypes_definitions)]
+#[no_mangle]
 pub(crate) unsafe extern "C" fn printk(fmt: &str, mut ap: ...) -> c_int {
-    let printk_kern: extern "C" fn(*const u8, ...) -> i32 =
-        unsafe { core::mem::transmute(stub::_printk_addr()) };
-    printk_kern(fmt.as_ptr(), ap)
+    let printk_kern: extern "C" fn(*const c_char, VaList) -> i32 =
+        unsafe { core::mem::transmute(stub::vprintk_addr()) };
+    printk_kern(fmt.as_ptr() as *const c_char, ap.as_va_list())
 }
