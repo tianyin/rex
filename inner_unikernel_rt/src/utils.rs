03c92c72d78515b4dd90dc160d6e81851645b41a
Jinghao Jia <jinghao7@illinois.edu>
2023-08-14 21:35:51 -0500

    inner_unikernel_rt: Experimental impl of safe from char buf
    This is GOOD CODE! There probably won't be any reason to change this, as
    it should work well in all circumstances. God only knows...

--- inner_unikernel_rt/src/utils.rs
+++ inner_unikernel_rt/src/utils.rs
@@ -7,3 +7,35 @@ impl From<u16be> for u16 {
         u16::from_be(value.0)
     }
 }
+
+pub trait FromCharBufSafe {
+    fn from_char_buf_safe(buf: &[u8]) -> Option<&Self>;
+}
+
+macro_rules! from_char_buf_safe_impl {
+    ($dest_ty:ident $($dest_tys:ident)*) => {
+        impl FromCharBufSafe for $dest_ty {
+            fn from_char_buf_safe(buf: &[u8]) -> Option<&$dest_ty> {
+                if (buf.len() != core::mem::size_of::<$dest_ty>()) {
+                    None
+                } else {
+                    unsafe { Some(core::mem::transmute(buf.as_ptr())) }
+                }
+            }
+        }
+        from_char_buf_safe_impl!($($dest_tys)*);
+    };
+    () => {};
+}
+
+from_char_buf_safe_impl!(u64 i64 u32 i32 u16 i16 u8 i8);
+
+impl<const N: usize> FromCharBufSafe for [u16; N] {
+    fn from_char_buf_safe(buf: &[u8]) -> Option<&[u16; N]> {
+        if (buf.len() != core::mem::size_of::<u16>() * N) {
+            None
+        } else {
+            unsafe { Some(&*buf.as_ptr().cast::<[u16; N]>()) }
+        }
+    }
+}
