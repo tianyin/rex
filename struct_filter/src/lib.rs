d1b4d02e87e1ba5ba71cfc134f75bb83dc2bfa8e
Ruowen Qin <chinqrw@gmail.com>
2023-08-21 13:11:09 -0500

    struct_filter: update syn version, add struct field iterater

--- struct_filter/src/lib.rs
+++ struct_filter/src/lib.rs
@@ -1,7 +1,5 @@
 extern crate proc_macro;
 
-use std::any::type_name;
-
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, Type, TypePath};
@@ -24,28 +22,28 @@ fn type_checking(type_path: &TypePath) -> bool {
     true
 }
 
-fn field_type(ty: Type) -> Result<(), TokenStream> {
+fn field_type_check(ty: Type) -> Result<(), TokenStream> {
     if let Type::Path(type_path) = ty {
         if !type_checking(&type_path) {
             return Err(
-                syn::Error::new_spanned(&type_path, "All fields must be of numeric type")
+                syn::Error::new_spanned(&type_path, "All fields must be numeric type")
                     .to_compile_error()
                     .into(),
             );
         }
     } else if let Type::Array(type_array) = ty {
         // convert [T; N] to T
-        return field_type(*type_array.elem);
+        return field_type_check(*type_array.elem);
     } else if let Type::Reference(type_ref) = ty {
         // convert &'a T to T
-        return field_type(*type_ref.elem);
+        return field_type_check(*type_ref.elem);
     } else if let Type::Slice(type_slice) = ty {
         // convert [T] to T
-        return field_type(*type_slice.elem);
+        return field_type_check(*type_slice.elem);
     } else {
         // TODO: add more detailed type hints
         return Err(
-            syn::Error::new_spanned(&ty, "All fields must be of validate type")
+            syn::Error::new_spanned(&ty, "All fields must be validate type")
                 .to_compile_error()
                 .into(),
         );
@@ -56,28 +54,44 @@ fn field_type(ty: Type) -> Result<(), TokenStream> {
 #[proc_macro_derive(FieldChecker)]
 pub fn ensure_numberic(input: TokenStream) -> TokenStream {
     let original_input = input.clone();
-    let ast: DeriveInput = parse_macro_input!(input);
+    let ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+    let struct_name = ast.ident;
+    let mut struct_fields = vec![];
 
-    match ast.data {
-        Data::Struct(s) => match s.fields {
-            Fields::Named(fields) => {
-                for field in fields.named {
-                    if let Err(err) = field_type(field.ty) {
-                        return err;
-                    }
-                }
-            }
-            _ => (),
-        },
-        _ => (),
+    if let Data::Struct(s) = ast.data {
+        for field in s.fields {
+            struct_fields.push(field);
+        }
     }
+
+    // match ast.data {
+    //     Data::Struct(s) => match s.fields {
+    //         Fields::Named(fields) => {
+    //             for field in fields.named {
+    //                 let field_type = &field.ty;
+    //                 // let tokens = quote! { #field_type };
+    //                 struct_fields.push((field.ident, field_type.clone()));
+
+    //                 if let Err(err) = field_type_check(field.ty) {
+    //                     return err;
+    //                 }
+    //             }
+    //         }
+    //         _ => (),
+    //     },
+    //     _ => (),
+    // }
     TokenStream::new()
-    //  // You can still derive other traits, or just generate an empty implementation
-    //  let name = ast.ident;
-    //  let gen = quote! {
-    //      impl #name {
-    //          // You can insert additional code here
-    //      }
-    //  };
-    //  gen.into()
+    // You can still derive other traits, or just generate an empty implementation
+    // let struct_name = ast.ident;
+    // let gen = quote! {
+    //     impl #struct_name {
+    //         fn new(data: &[u8]) -> Self {
+    //         #struct_name {
+    //                 a
+    //             }
+    //         }
+    //     }
+    // };
+    // gen.into()
 }
