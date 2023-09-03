6afab7e729d0eed3fe4283f59d2aef41d95a7b0e
Ruowen Qin <chinqrw@gmail.com>
2023-09-02 22:24:03 -0500

    samples/xdp: add prepare_packet() for bmc program
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- samples/xdp_test/src/main.rs
+++ samples/xdp_test/src/main.rs
@@ -25,6 +25,7 @@ const BMC_MAX_KEY_IN_PACKET: u32 = BMC_MAX_KEY_IN_MULTIGET;
 
 const FNV_OFFSET_BASIS_32: u32 = 2166136261;
 const FNV_PRIME_32: u32 = 16777619;
+const ETH_ALEN: usize = 6;
 
 #[derive(FieldTransmute)]
 #[repr(C, packed)]
@@ -38,8 +39,8 @@ pub struct memcached_udp_header {
 #[derive(FieldTransmute)]
 #[repr(C, packed)]
 pub struct eth_header {
-    pub h_dest: [u8; 6usize],
-    pub h_source: [u8; 6usize],
+    pub h_dest: [u8; ETH_ALEN],
+    pub h_source: [u8; ETH_ALEN],
     pub h_proto: u16,
 }
 
@@ -87,8 +88,7 @@ MAP_DEF!(
     0
 );
 
-#[inline(always)]
-fn hash_key(obj: &xdp, ctx: &xdp_md, pctx: &parsing_context, payload: &[u8]) -> u32 {
+fn hash_key(obj: &xdp, ctx: &xdp_md, pctx: &mut parsing_context, payload: &[u8]) -> u32 {
     let mut key = match obj.bpf_map_lookup_elem(map_keys, pctx.key_count) {
         None => return XDP_PASS,
         Some(k) => k,
@@ -96,7 +96,7 @@ fn hash_key(obj: &xdp, ctx: &xdp_md, pctx: &parsing_context, payload: &[u8]) -> 
 
     key.hash = FNV_OFFSET_BASIS_32;
 
-    let (mut off, mut done_parsing, mut key_len) = (0usize, 0u32, 0u32);
+    let (mut off, mut done_parsing, mut key_len) = (0usize, 0u32, 0u8);
 
     while off < BMC_MAX_KEY_LENGTH as usize + 1
         && pctx.read_pkt_offset as usize + off + 1 <= ctx.data_length
@@ -114,7 +114,93 @@ fn hash_key(obj: &xdp, ctx: &xdp_md, pctx: &parsing_context, payload: &[u8]) -> 
         off += 1;
     }
 
-    0
+    // no key found
+    if (key_len == 0 || key_len as u32 > BMC_MAX_KEY_LENGTH) {
+        return XDP_PASS;
+    }
+
+    // get the cache entry
+    let cache_idx: u32 = key.hash % BMC_CACHE_ENTRY_COUNT;
+    let entry = match obj.bpf_map_lookup_elem(map_kcache, cache_idx) {
+        // should never happen
+        None => return XDP_PASS,
+        Some(e) => e,
+    };
+    // TODO: should have lock here bpf_spin_lock(&entry->lock);
+
+    // potential cache hit
+    if (entry.valid == 1 && entry.hash == key.hash) {
+        // TODO: bpf_spin_unlock(&entry.lock);
+        for i in pctx.read_pkt_offset..key_len {
+            // end of packet
+            if (i as usize + 1 > ctx.data_length) {
+                break;
+            }
+            key.data[i as usize] = payload[i as usize];
+        }
+        key.len = key_len as u32;
+        pctx.key_count += 1;
+    } else {
+        // cache miss
+        // TODO: bpf_spin_unlock(&entry.lock);
+        // TODO: add stats here
+        // 		bpf_spin_unlock(&entry->lock);
+        // 		struct bmc_stats *stats =
+        // 			bpf_map_lookup_elem(&map_stats, &zero);
+        // 		if (!stats) {
+        // 			return XDP_PASS;
+        // 		}
+        // 		stats->miss_count++;
+    }
+
+    if (done_parsing == 1) {
+        if (pctx.key_count > 0) {
+            return prepare_packet(obj, ctx, payload);
+        }
+    } else {
+        // process more keys
+        off += 1;
+        pctx.read_pkt_offset += off as u8;
+        hash_key(obj, ctx, pctx, payload);
+    }
+
+    XDP_PASS
+}
+
+fn prepare_packet(obj: &xdp, ctx: &xdp_md, payload: &[u8]) -> u32 {
+    // exchange src and dst ip and mac
+
+    // if (payload >= data_end || old_payload + 1 >= data_end)
+    // 	return XDP_PASS;
+    //
+    // // use old headers as a base; then update addresses and ports to create the new headers
+    // memmove(eth, old_data,
+    // 	sizeof(*eth) + sizeof(*ip) + sizeof(*udp) +
+    // 		sizeof(*memcached_udp_hdr));
+    //
+    let mut eth_tmp = [0u8; ETH_ALEN];
+    let mut ip_tmp: u32;
+    let mut port_tmp: u16;
+    // TODO: use memcpy
+    // __builtin_memcpy(tmp_mac, eth->h_source, ETH_ALEN);
+    // __builtin_memcpy(eth->h_source, eth->h_dest, ETH_ALEN);
+    // __builtin_memcpy(eth->h_dest, tmp_mac, ETH_ALEN);
+
+    let ip_header_mut = obj.ip_header_mut(ctx);
+    let udp_header_mut = obj.udp_header_mut(ctx);
+    ip_tmp = ip_header_mut.saddr;
+    ip_header_mut.saddr = ip_header_mut.daddr;
+    ip_header_mut.daddr = ip_tmp;
+
+    port_tmp = udp_header_mut.source;
+    udp_header_mut.source = udp_header_mut.dest;
+    udp_header_mut.dest = port_tmp;
+
+    write_pkt_reply(obj, ctx, payload)
+}
+
+fn write_pkt_reply(obj: &xdp, ctx: &xdp_md, payload: &[u8]) -> u32 {
+    return XDP_PASS;
 }
 
 fn xdp_rx_filter_fn(obj: &xdp, ctx: &xdp_md) -> u32 {
@@ -152,7 +238,7 @@ fn xdp_rx_filter_fn(obj: &xdp, ctx: &xdp_md) -> u32 {
             }
             off += header_len;
 
-            let pctx = parsing_context {
+            let mut pctx = parsing_context {
                 key_count: 0,
                 current_key: 0,
                 read_pkt_offset: off as u8,
@@ -160,7 +246,10 @@ fn xdp_rx_filter_fn(obj: &xdp, ctx: &xdp_md) -> u32 {
             };
 
             // hash the key
-            hash_key(obj, ctx, &pctx, payload);
+            match hash_key(obj, ctx, &mut pctx, payload) {
+                XDP_PASS => return XDP_PASS,
+                _ => (),
+            }
 
             bpf_printk!(obj, "offset is %d\n", off as u64);
         }
