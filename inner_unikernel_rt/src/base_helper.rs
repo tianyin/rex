tianyin/rex

commit e9713e0b10e42aafb48e2af5609a058a7c24051b

inner_unikernel_rt: move to a new map implementation
Previous implementation uses two static variables per map with one
pointing to the other:

  static __MAP1: IUMap<...> = IUMap::new(...);
  static MAP1: &IUMap<...> = &__MAP1;

In the above example the `__MAP1` is the variable that stores all the
map metadata and is parsed by libiu to for map creation. With the new
map created, the value of `MAP1` will be overwritten by libiu to the map
fd (and later to the kernel map pointer by the kernel), i.e. this
point-to relationship no longer holds after this point. However, since
these all happens after compilation, the compiler always assume such
a point-to relationship and it sometimes leads to incorrect optimization
/ code generation (and breaking this assumption is probably by itself an
UB).

Therefore, we are now switching to a new implementation of the maps.
Here we store the kernel side pointer into the `IUMap` struct itself so
that we only need one static variable per map:

  static MAP1: IUMap<...> = IUMap::new(...);

The pointer will be set to null at compile time via the
constantly-evaluated `IUMap::new`. It will be overwritten first by libiu
to the map fd and then by the kernel to the actual kernel map address.

Some other notes:
- The pointer field is set to be crate-private, this prevents the user
  from crating arbitrary pointer values. The pointer can only be `NULL`
  or a valid kernel pointer.
- All the map helpers now requires the `IUMap` struct to have static
  lifetime, this means user cannot use arbitrarily created maps in these
  helpers (e.g. local variables). However, users can still use a static
  `IUMap` not instrumented by libiu and kernel (and therefore has
  a `NULL` kernel map pointer). The helper function will also check the
  validity of the map pointers. Since as mentioned above, the user
  cannot tempt with the pointer value, the `NULL` check is sufficient
  for this.

Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

inner_unikernel_rt/src/base_helper.rs
@@ -1,6 +1,7 @@
 use crate::bindings::uapi::linux::bpf::bpf_spin_lock;
 use crate::debug::printk;
 use crate::linux::bpf::bpf_map_type;
+use crate::linux::errno::EINVAL;
 use crate::map::IUMap;
 use crate::per_cpu::this_cpu_read;
 use crate::random32::bpf_user_rnd_u32;
@@ -23,12 +24,16 @@ pub(crate) fn bpf_trace_printk(
 }
 
 pub(crate) fn bpf_map_lookup_elem<const MT: bpf_map_type, K, V>(
-    map: &IUMap<MT, K, V>,
+    map: &'static IUMap<MT, K, V>,
     key: K,
 ) -> Option<&mut V> {
-    let helper: extern "C" fn(&IUMap<MT, K, V>, *const K) -> *const V =
+    if map.kptr.is_null() {
+        return None;
+    }
+
+    let helper: extern "C" fn(*mut (), *const K) -> *const V =
         unsafe { core::mem::transmute(stub::bpf_map_lookup_elem_addr()) };
-    let value = helper(map, &key) as *mut V;
+    let value = helper(map.kptr, &key) as *mut V;
 
     if value.is_null() {
         None
@@ -38,58 +43,74 @@ pub(crate) fn bpf_map_lookup_elem<const MT: bpf_map_type, K, V>(
 }
 
 pub(crate) fn bpf_map_update_elem<const MT: bpf_map_type, K, V>(
-    map: &IUMap<MT, K, V>,
+    map: &'static IUMap<MT, K, V>,
     key: K,
     value: V,
     flags: u64,
 ) -> i64 {
-    let helper: extern "C" fn(
-        &IUMap<MT, K, V>,
-        *const K,
-        *const V,
-        u64,
-    ) -> i64 =
+    if map.kptr.is_null() {
+        return -(EINVAL as i64);
+    }
+
+    let helper: extern "C" fn(*mut (), *const K, *const V, u64) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_update_elem_addr()) };
-    helper(map, &key, &value, flags)
+    helper(map.kptr, &key, &value, flags)
 }
 
 pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
-    map: &IUMap<MT, K, V>,
+    map: &'static IUMap<MT, K, V>,
     key: K,
 ) -> i64 {
-    let helper: extern "C" fn(&IUMap<MT, K, V>, *const K) -> i64 =
+    if map.kptr.is_null() {
+        return -(EINVAL as i64);
+    }
+
+    let helper: extern "C" fn(*mut (), *const K) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_delete_elem_addr()) };
-    helper(map, &key)
+    helper(map.kptr, &key)
 }
 
 pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
-    map: &IUMap<MT, K, V>,
+    map: &'static IUMap<MT, K, V>,
     value: V,
     flags: u64,
 ) -> i64 {
-    let helper: extern "C" fn(&IUMap<MT, K, V>, *const V, u64) -> i64 =
+    if map.kptr.is_null() {
+        return -(EINVAL as i64);
+    }
+
+    let helper: extern "C" fn(*mut (), *const V, u64) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_push_elem_addr()) };
-    helper(map, &value, flags)
+    helper(map.kptr, &value, flags)
 }
 
 pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
-    map: &IUMap<MT, K, V>,
+    map: &'static IUMap<MT, K, V>,
     value: V,
 ) -> i64 {
-    let helper: extern "C" fn(&IUMap<MT, K, V>, *const V) -> i64 =
+    if map.kptr.is_null() {
+        return -(EINVAL as i64);
+    }
+
+    let helper: extern "C" fn(*mut (), *const V) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_pop_elem_addr()) };
-    helper(map, &value)
+    helper(map.kptr, &value)
 }
 
 pub(crate) fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
-    map: &IUMap<MT, K, V>,
+    map: &'static IUMap<MT, K, V>,
     value: V,
 ) -> i64 {
-    let helper: extern "C" fn(&IUMap<MT, K, V>, *const V) -> i64 =
+    if map.kptr.is_null() {
+        return -(EINVAL as i64);
+    }
+
+    let helper: extern "C" fn(*mut (), *const V) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_peek_elem_addr()) };
-    helper(map, &value)
+    helper(map.kptr, &value)
 }
 
+/*
 pub(crate) fn bpf_for_each_map_elem<const MT: bpf_map_type, K, V>(
     map: &IUMap<MT, K, V>,
     callback_fn: *const (),
@@ -117,6 +138,7 @@ pub(crate) fn bpf_spin_unlock(lock: &mut bpf_spin_lock) -> i64 {
         unsafe { core::mem::transmute(stub::bpf_spin_unlock_addr()) };
     helper(lock as *mut bpf_spin_lock)
 }
+*/
 
 // Design decision: Make the destination a generic type so that probe read
 // kernel can directly fill in variables of certain type. This also achieves
@@ -275,7 +297,7 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn bpf_map_lookup_elem<const MT: bpf_map_type, K, V>(
             &self,
-            map: &'a IUMap<MT, K, V>,
+            map: &'static IUMap<MT, K, V>,
             key: K,
         ) -> Option<&mut V> {
             crate::base_helper::bpf_map_lookup_elem(map, key)
@@ -284,7 +306,7 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn bpf_map_update_elem<const MT: bpf_map_type, K, V>(
             &self,
-            map: &IUMap<MT, K, V>,
+            map: &'static IUMap<MT, K, V>,
             key: K,
             value: V,
             flags: u64,
@@ -295,7 +317,7 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
             &self,
-            map: &IUMap<MT, K, V>,
+            map: &'static IUMap<MT, K, V>,
             key: K,
         ) -> i64 {
             crate::base_helper::bpf_map_delete_elem(map, key)
@@ -304,7 +326,7 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
             &self,
-            map: &IUMap<MT, K, V>,
+            map: &'static IUMap<MT, K, V>,
             value: V,
             flags: u64,
         ) -> i64 {
@@ -314,7 +336,7 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
             &self,
-            map: &IUMap<MT, K, V>,
+            map: &'static IUMap<MT, K, V>,
             value: V,
         ) -> i64 {
             crate::base_helper::bpf_map_pop_elem(map, value)
@@ -323,7 +345,7 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
             &self,
-            map: &IUMap<MT, K, V>,
+            map: &'static IUMap<MT, K, V>,
             value: V,
         ) -> i64 {
             crate::base_helper::bpf_map_peek_elem(map, value)
@@ -374,6 +396,7 @@ macro_rules! base_helper_defs {
             crate::base_helper::bpf_get_prandom_u32()
         }
 
+        /*
         #[inline(always)]
         pub fn bpf_spin_lock(
             &self,
@@ -389,6 +412,7 @@ macro_rules! base_helper_defs {
         ) -> i64 {
             crate::base_helper::bpf_spin_unlock(lock)
         }
+        */
 
         #[inline(always)]
         pub fn bpf_snprintf<const N: usize, const M: usize>(

inner_unikernel_rt/src/perf_event/perf_event_impl.rs
@@ -5,6 +5,7 @@ use crate::bindings::uapi::linux::bpf::{
     bpf_map_type, bpf_perf_event_value, BPF_MAP_TYPE_STACK_TRACE,
     BPF_PROG_TYPE_PERF_EVENT,
 };
+use crate::linux::errno::EINVAL;
 use crate::map::*;
 use crate::prog_type::iu_prog;
 use crate::stub;
@@ -90,16 +91,20 @@ impl<'a> perf_event<'a> {
     pub fn bpf_get_stackid_pe<K, V>(
         &self,
         ctx: &bpf_perf_event_data,
-        map: &'a IUMap<BPF_MAP_TYPE_STACK_TRACE, K, V>,
+        map: &'static IUMap<BPF_MAP_TYPE_STACK_TRACE, K, V>,
         flags: u64,
     ) -> i64 {
+        if map.kptr.is_null() {
+            return -(EINVAL as i64);
+        }
+
         let helper: extern "C" fn(
             *const bpf_perf_event_data_kern,
-            &'a IUMap<BPF_MAP_TYPE_STACK_TRACE, K, V>,
+            *mut (),
             u64,
         ) -> i64 =
             unsafe { core::mem::transmute(stub::bpf_get_stackid_pe_addr()) };
-        helper(ctx.kptr, map, flags)
+        helper(ctx.kptr, map.kptr, flags)
     }
 
     pub fn bpf_get_current_task(&self) -> Option<TaskStruct> {

inner_unikernel_rt/src/map.rs
@@ -1,14 +1,19 @@
 use crate::linux::bpf::bpf_map_type;
-use core::marker::PhantomData;
-use core::mem::size_of;
+use core::{marker::PhantomData, mem, ptr};
 
 #[repr(C)]
 pub struct IUMap<const MT: bpf_map_type, K, V> {
+    // Map metadata
     map_type: u32,
     key_size: u32,
     val_size: u32,
     max_size: u32,
     map_flag: u32,
+
+    // Actual kernel side map pointer
+    pub(crate) kptr: *mut (),
+
+    // Zero-sized marker
     key_type: PhantomData<K>,
     val_type: PhantomData<V>,
 }
@@ -17,27 +22,25 @@ impl<const MT: bpf_map_type, K, V> IUMap<MT, K, V> {
     pub const fn new(ms: u32, mf: u32) -> IUMap<MT, K, V> {
         Self {
             map_type: MT,
-            key_size: size_of::<K>() as u32,
-            val_size: size_of::<V>() as u32,
+            key_size: mem::size_of::<K>() as u32,
+            val_size: mem::size_of::<V>() as u32,
             max_size: ms,
             map_flag: mf,
+            kptr: ptr::null_mut(),
             key_type: PhantomData,
             val_type: PhantomData,
         }
     }
 }
 
+unsafe impl<const MT: bpf_map_type, K, V> Sync for IUMap<MT, K, V> {}
+
 #[macro_export]
 macro_rules! MAP_DEF {
-    ($n:ident, $in:ident, $k:ty, $v:ty, $mt:expr, $ms:expr, $mf:expr) => {
+    ($n:ident, $k:ty, $v:ty, $mt:expr, $ms:expr, $mf:expr) => {
         #[no_mangle]
         #[used]
         #[link_section = ".maps"]
-        static $in: IUMap<$mt, $k, $v> = IUMap::new($ms, $mf);
-
-        #[no_mangle]
-        #[used]
-        #[link_section = ".maps"]
-        static $n: &IUMap<$mt, $k, $v> = &$in;
+        static $n: IUMap<$mt, $k, $v> = IUMap::new($ms, $mf);
     };
 }

commit eaf2d4fbcda9d7a462105ed2634218a3fb7bbf72

inner_unikernel_rt: make access to kernel map ptr volatile
Without the volatile load the compiler is smart enough to use
optimizations (probably SROA and SCCP) to optimize away the entire map
access. This is because from the compiler's perspective the kernel map
ptr is always null and is stored in an immutable static variable.

Use core::ptr::read_volatile to infor the compiler that this is indeed a
volatile variable.

Fixes: e9713e0b10e4 ("inner_unikernel_rt: move to a new map implementation")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

inner_unikernel_rt/src/base_helper.rs
@@ -29,13 +29,14 @@ pub(crate) fn bpf_map_lookup_elem<'a, const MT: bpf_map_type, K, V>(
     map: &'static IUMap<MT, K, V>,
     key: &'a K,
 ) -> Option<&'a mut V> {
-    if map.kptr.is_null() {
+    let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
+    if map_kptr.is_null() {
         return None;
     }
 
     let helper: extern "C" fn(*mut (), *const K) -> *const V =
         unsafe { core::mem::transmute(stub::bpf_map_lookup_elem_addr()) };
-    let value = helper(map.kptr, key) as *mut V;
+    let value = helper(map_kptr, key) as *mut V;
 
     if value.is_null() {
         None
@@ -50,28 +51,30 @@ pub(crate) fn bpf_map_update_elem<const MT: bpf_map_type, K, V>(
     value: &V,
     flags: u64,
 ) -> Result {
-    if map.kptr.is_null() {
+    let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
+    if map_kptr.is_null() {
         return Err(EINVAL as u64);
     }
 
     let helper: extern "C" fn(*mut (), *const K, *const V, u64) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_update_elem_addr()) };
 
-    to_result(helper(map.kptr, key, value, flags))
+    to_result(helper(map_kptr, key, value, flags))
 }
 
 pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
     map: &'static IUMap<MT, K, V>,
     key: &K,
 ) -> Result {
-    if map.kptr.is_null() {
+    let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
+    if map_kptr.is_null() {
         return Err(EINVAL as u64);
     }
 
     let helper: extern "C" fn(*mut (), *const K) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_delete_elem_addr()) };
 
-    to_result((helper(map.kptr, key)))
+    to_result((helper(map_kptr, key)))
 }
 
 pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
@@ -79,42 +82,45 @@ pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
     value: &V,
     flags: u64,
 ) -> Result {
-    if map.kptr.is_null() {
+    let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
+    if map_kptr.is_null() {
         return Err(EINVAL as u64);
     }
 
     let helper: extern "C" fn(*mut (), *const V, u64) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_push_elem_addr()) };
 
-    to_result(helper(map.kptr, value, flags))
+    to_result(helper(map_kptr, value, flags))
 }
 
 pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
     map: &'static IUMap<MT, K, V>,
     value: &V,
 ) -> Result {
-    if map.kptr.is_null() {
+    let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
+    if map_kptr.is_null() {
         return Err(EINVAL as u64);
     }
 
     let helper: extern "C" fn(*mut (), *const V) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_pop_elem_addr()) };
 
-    to_result(helper(map.kptr, value))
+    to_result(helper(map_kptr, value))
 }
 
 pub(crate) fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
     map: &'static IUMap<MT, K, V>,
     value: &V,
 ) -> Result {
-    if map.kptr.is_null() {
+    let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
+    if map_kptr.is_null() {
         return Err(EINVAL as u64);
     }
 
     let helper: extern "C" fn(*mut (), *const V) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_peek_elem_addr()) };
 
-    to_result(helper(map.kptr, value))
+    to_result(helper(map_kptr, value))
 }
 
 /*

inner_unikernel_rt/src/perf_event/perf_event_impl.rs
@@ -96,7 +96,8 @@ impl<'a> perf_event<'a> {
         map: &'static IUMap<BPF_MAP_TYPE_STACK_TRACE, K, V>,
         flags: u64,
     ) -> Result {
-        if map.kptr.is_null() {
+        let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
+        if map_kptr.is_null() {
             return Err(EINVAL as u64);
         }
 
@@ -107,7 +108,7 @@ impl<'a> perf_event<'a> {
         ) -> i64 =
             unsafe { core::mem::transmute(stub::bpf_get_stackid_pe_addr()) };
 
-        to_result(helper(ctx.kptr, map.kptr, flags))
+        to_result(helper(ctx.kptr, map_kptr, flags))
     }
 
     pub fn bpf_get_current_task(&self) -> Option<TaskStruct> {