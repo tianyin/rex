tianyin/rex

commit ee0b0d9bd225a3ab010a868e66ab97c72cdc8888

inner_unikernel_rt: use i32 in Result
Since Linux kernel uses i32 to handle erorrs in Rust part, we update the previous u64 to i32

Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

inner_unikernel_rt/src/utils.rs
@@ -1,3 +1,5 @@
+use core::ffi::c_int;
+
 #[repr(transparent)]
 pub struct u16be(pub(crate) u16);
 
@@ -44,17 +46,22 @@ pub fn direct_packet_access_ok<T: DirectPacketAccessOk>() {}
 /// To be used as the return type for functions that may fail.
 ///
 /// Ref: linux/rust/kernel/error.rs
-pub type Result = core::result::Result<u64, u64>;
+pub type Result = core::result::Result<c_int, c_int>;
 
 /// Converts an integer as returned by a C kernel function to an error if it's
 /// negative, and `Ok(val)` otherwise.
 ///
 /// Ref: linux/rust/kernel/error.rs
-#[inline(always)]
-pub fn to_result(retval: i64) -> Result {
-    if retval < 0 {
-        Err((-retval) as u64)
-    } else {
-        Ok(retval as u64)
-    }
+// genetic specialization to Macro
+#[macro_export]
+macro_rules! to_result {
+    ($retval:expr) => {
+        if $retval < 0 {
+            Err($retval as i32)
+        } else {
+            Ok($retval as i32)
+        }
+    };
 }
+
+pub(crate) use to_result;

inner_unikernel_rt/src/base_helper.rs
@@ -22,7 +22,7 @@ pub(crate) fn bpf_trace_printk(
     let code: extern "C" fn(*const u8, u32, u64, u64, u64) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_trace_printk_iu_addr()) };
 
-    to_result(code(fmt.as_ptr(), fmt.len() as u32, arg1, arg2, arg3))
+    to_result!(code(fmt.as_ptr(), fmt.len() as u32, arg1, arg2, arg3))
 }
 
 pub(crate) fn bpf_map_lookup_elem<'a, const MT: bpf_map_type, K, V>(
@@ -53,13 +53,13 @@ pub(crate) fn bpf_map_update_elem<const MT: bpf_map_type, K, V>(
 ) -> Result {
     let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
     if map_kptr.is_null() {
-        return Err(EINVAL as u64);
+        return Err(EINVAL as i32);
     }
 
     let helper: extern "C" fn(*mut (), *const K, *const V, u64) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_update_elem_addr()) };
 
-    to_result(helper(map_kptr, key, value, flags))
+    to_result!(helper(map_kptr, key, value, flags) as i32)
 }
 
 pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
@@ -68,13 +68,13 @@ pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
 ) -> Result {
     let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
     if map_kptr.is_null() {
-        return Err(EINVAL as u64);
+        return Err(EINVAL as i32);
     }
 
     let helper: extern "C" fn(*mut (), *const K) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_delete_elem_addr()) };
 
-    to_result((helper(map_kptr, key)))
+    to_result!((helper(map_kptr, key)) as i32)
 }
 
 pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
@@ -84,13 +84,13 @@ pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
 ) -> Result {
     let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
     if map_kptr.is_null() {
-        return Err(EINVAL as u64);
+        return Err(EINVAL as i32);
     }
 
     let helper: extern "C" fn(*mut (), *const V, u64) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_push_elem_addr()) };
 
-    to_result(helper(map_kptr, value, flags))
+    to_result!(helper(map_kptr, value, flags) as i32)
 }
 
 pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
@@ -99,13 +99,13 @@ pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
 ) -> Result {
     let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
     if map_kptr.is_null() {
-        return Err(EINVAL as u64);
+        return Err(EINVAL as i32);
     }
 
     let helper: extern "C" fn(*mut (), *const V) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_pop_elem_addr()) };
 
-    to_result(helper(map_kptr, value))
+    to_result!(helper(map_kptr, value) as i32)
 }
 
 pub(crate) fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
@@ -114,13 +114,13 @@ pub(crate) fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
 ) -> Result {
     let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
     if map_kptr.is_null() {
-        return Err(EINVAL as u64);
+        return Err(EINVAL as i32);
     }
 
     let helper: extern "C" fn(*mut (), *const V) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_map_peek_elem_addr()) };
 
-    to_result(helper(map_kptr, value))
+    to_result!(helper(map_kptr, value) as i32)
 }
 
 /*
@@ -162,7 +162,7 @@ pub(crate) fn bpf_probe_read_kernel<T>(
 ) -> Result {
     let helper: extern "C" fn(*mut (), u32, *const ()) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_probe_read_kernel_addr()) };
-    to_result(helper(
+    to_result!(helper(
         dst as *mut T as *mut (),
         core::mem::size_of::<T>() as u32,
         unsafe_ptr,
@@ -279,13 +279,13 @@ pub(crate) fn bpf_snprintf<const N: usize, const M: usize>(
 ) -> Result {
     let helper: extern "C" fn(*mut u8, u32, *const u8, *const u64, u32) -> i64 =
         unsafe { core::mem::transmute(stub::bpf_snprintf_addr()) };
-    to_result(helper(
+    to_result!(helper(
         str.as_mut_ptr(),
         N as u32,
         fmt.as_ptr(),
         data.as_ptr(),
         M as u32,
-    ))
+    ) as i32)
 }
 
 macro_rules! base_helper_defs {

inner_unikernel_rt/src/lib.rs
@@ -60,4 +60,4 @@ define_prog_entry!(xdp);
 define_prog_entry!(sched_cls);
 
 pub use bindings::uapi::*;
-pub use utils::{to_result, Result};
+pub use utils::Result;

inner_unikernel_rt/src/perf_event/perf_event_impl.rs
@@ -87,7 +87,7 @@ impl<'a> perf_event<'a> {
             core::mem::transmute(stub::bpf_perf_prog_read_value_addr())
         };
 
-        to_result(helper(ctx.kptr, buf, size))
+        to_result!(helper(ctx.kptr, buf, size))
     }
 
     pub fn bpf_get_stackid_pe<K, V>(
@@ -98,7 +98,7 @@ impl<'a> perf_event<'a> {
     ) -> Result {
         let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
         if map_kptr.is_null() {
-            return Err(EINVAL as u64);
+            return Err(EINVAL as i32);
         }
 
         let helper: extern "C" fn(
@@ -108,7 +108,7 @@ impl<'a> perf_event<'a> {
         ) -> i64 =
             unsafe { core::mem::transmute(stub::bpf_get_stackid_pe_addr()) };
 
-        to_result(helper(ctx.kptr, map_kptr, flags))
+        to_result!(helper(ctx.kptr, map_kptr, flags))
     }
 
     pub fn bpf_get_current_task(&self) -> Option<TaskStruct> {

inner_unikernel_rt/src/sched_cls/sched_cls_impl.rs
@@ -65,7 +65,7 @@ pub struct __sk_buff<'a> {
 #[repr(C)]
 pub struct sched_cls<'a> {
     rtti: u64,
-    prog: fn(&Self, &__sk_buff) -> u32,
+    prog: fn(&Self, &__sk_buff) -> Result,
     name: &'a str,
 }
 
@@ -74,7 +74,7 @@ impl<'a> sched_cls<'a> {
 
     pub const fn new(
         // TODO update based on signature
-        f: fn(&sched_cls<'a>, &__sk_buff) -> u32,
+        f: fn(&sched_cls<'a>, &__sk_buff) -> Result,
         nm: &'a str,
         rtti: u64,
     ) -> sched_cls<'a> {
@@ -162,6 +162,8 @@ impl<'a> sched_cls<'a> {
 impl iu_prog for sched_cls<'_> {
     fn prog_run(&self, ctx: *const ()) -> u32 {
         let mut newctx = self.convert_ctx(ctx);
-        (self.prog)(self, &mut newctx)
+        // return TC_ACT_OK if error
+        ((self.prog)(self, &mut newctx)).unwrap_or_else(|e| TC_ACT_OK as i32)
+            as u32
     }
 }

inner_unikernel_rt/src/xdp/xdp_impl.rs
@@ -66,7 +66,7 @@ pub unsafe fn convert_slice_to_struct_mut<T>(slice: &mut [c_uchar]) -> &mut T {
 pub struct xdp<'a> {
     rtti: u64,
     // TODO update it
-    prog: fn(&Self, &xdp_md) -> u32,
+    prog: fn(&Self, &xdp_md) -> Result,
     name: &'a str,
 }
 
@@ -75,7 +75,7 @@ impl<'a> xdp<'a> {
 
     pub const fn new(
         // TODO update based on signature
-        f: fn(&xdp<'a>, &xdp_md) -> u32,
+        f: fn(&xdp<'a>, &xdp_md) -> Result,
         nm: &'a str,
         rtti: u64,
     ) -> xdp<'a> {
@@ -261,6 +261,8 @@ impl<'a> xdp<'a> {
 impl iu_prog for xdp<'_> {
     fn prog_run(&self, ctx: *const ()) -> u32 {
         let mut newctx = self.convert_ctx(ctx);
-        (self.prog)(self, &mut newctx)
+        // Return XDP_PASS if Err, i.e. discard event
+        ((self.prog)(self, &mut newctx)).unwrap_or_else(|e| XDP_PASS as i32)
+            as u32
     }
 }

commit bef8fe97ab1e3b8b0b5363b881653d28b1c75e48

inner_unikernel_rt/utils: fixed macro to_result! eval expr twice
Fixes: ee0b0d9bd225("inner_unikernel_rt: use i32 in Result")
Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

inner_unikernel_rt/src/utils.rs
@@ -55,13 +55,14 @@ pub type Result = core::result::Result<c_int, c_int>;
 // genetic specialization to Macro
 #[macro_export]
 macro_rules! to_result {
-    ($retval:expr) => {
-        if $retval < 0 {
-            Err($retval as i32)
+    ($retval:expr) => {{
+        let val = $retval;
+        if val < 0 {
+            Err(val as i32)
         } else {
-            Ok($retval as i32)
+            Ok(val as i32)
         }
-    };
+    }};
 }
 
 pub(crate) use to_result;