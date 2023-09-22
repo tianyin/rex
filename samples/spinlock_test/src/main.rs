a1f23f88275ee0381923840cca46737cc5876033
Jinghao Jia <jinghao7@illinois.edu>
2023-09-22 05:20:18 -0500

    samples: add spinlock_test
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

---
+++ samples/spinlock_test/src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+extern crate inner_unikernel_rt;
+
+use inner_unikernel_rt::{entry_link, Result, MAP_DEF};
+use inner_unikernel_rt::tracepoint::*;
+use inner_unikernel_rt::linux::bpf::{bpf_spin_lock, BPF_MAP_TYPE_ARRAY};
+use inner_unikernel_rt::spinlock::iu_spinlock_guard;
+use inner_unikernel_rt::map::IUMap;
+
+#[repr(C)]
+struct MapEntry {
+    data: u64,
+    lock: bpf_spin_lock,
+}
+
+MAP_DEF!(map_array, u32, MapEntry, BPF_MAP_TYPE_ARRAY, 256, 0);
+
+fn iu_prog1_fn(obj: &tracepoint, _: tp_ctx) -> Result {
+    if let Some(entry) = obj.bpf_map_lookup_elem(&map_array, &0) {
+        let _guard = iu_spinlock_guard::new(&mut entry.lock);
+        entry.data = 1;
+    }
+    Ok(0)
+}
+
+#[entry_link(inner_unikernel/tracepoint/syscalls/sys_enter_dup)]
+static PROG: tracepoint = tracepoint::new(iu_prog1_fn, "iu_prog1", tp_type::Void);
