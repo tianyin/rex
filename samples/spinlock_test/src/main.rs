1dca40987e9dc72fcd950a0e0aa41ea92d3a6ab6
Jinghao Jia <jinghao7@illinois.edu>
2023-09-22 05:25:46 -0500

    samples/pinlock_test: add comments on how lock guard works
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- samples/spinlock_test/src/main.rs
+++ samples/spinlock_test/src/main.rs
@@ -19,8 +19,10 @@ MAP_DEF!(map_array, u32, MapEntry, BPF_MAP_TYPE_ARRAY, 256, 0);
 
 fn iu_prog1_fn(obj: &tracepoint, _: tp_ctx) -> Result {
     if let Some(entry) = obj.bpf_map_lookup_elem(&map_array, &0) {
+        // entry.lock locked in iu_spinlock_guard::new
         let _guard = iu_spinlock_guard::new(&mut entry.lock);
         entry.data = 1;
+        // entry.lock is automatically released when _guard goes out of scope
     }
     Ok(0)
 }
