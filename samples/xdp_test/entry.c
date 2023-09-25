79435585a71e0e9b19da7f0f5682282541a7d48a
Ruowen Qin <ruowenq2@illinois.edu>
2023-09-25 10:49:39 -0500

    samples/xdp_test: compile "core" itself as part of crate compilation
    Confirm the new update works on xdp
    
    check detail at commit 4ce560a8881f("samples/*: compile "core" itself as part of crate compilation")
    
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- samples/xdp_test/entry.c
+++ samples/xdp_test/entry.c
@@ -20,7 +20,7 @@
 #define STATS_PATH "/tmp/bmc_stats.txt"
 #define STATS_INTERVAL_PATH "/tmp/bmc_stats_interval.txt"
 
-#define EXE "./target/debug/xdp_test"
+#define EXE "./target/x86_64-unknown-linux-gnu/release/xdp_test"
 
 static int nr_cpus = 0;
 
