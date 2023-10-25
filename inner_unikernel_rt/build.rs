17d1f645cdab7ebd71eb27484fe71342270f21f4
Jinghao Jia <jinghao7@illinois.edu>
2023-10-25 17:48:54 -0500

    inner_unikernel_rt: refactor libiustub build into build.rs
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- inner_unikernel_rt/build.rs
+++ inner_unikernel_rt/build.rs
@@ -2,10 +2,12 @@
 
 use std::env;
 use std::fs;
+use std::io::Result;
+use std::path::Path;
 use std::process::Command;
 use std::string::String;
 
-fn main() {
+fn main() -> Result<()> {
     let out_dir = env::var("OUT_DIR").unwrap();
     let linux_dir = env::var("LINUX").unwrap();
 
@@ -13,8 +15,7 @@ fn main() {
         .arg("build.py")
         .arg(&linux_dir)
         .arg(&out_dir)
-        .output()
-        .expect("failed to run build.py");
+        .output()?;
 
     output
         .status
@@ -23,17 +24,32 @@ fn main() {
         .map_err(|_| panic!("\n{}", String::from_utf8_lossy(&output.stderr)))
         .unwrap();
 
-    let iustub_pathbuf = fs::canonicalize("./libiustub").unwrap();
-    let iustub_path = iustub_pathbuf.to_str().unwrap();
-    Command::new("make")
-        .arg("-C")
-        .arg(iustub_path)
-        .output()
-        .expect("failed to build libiustub");
+    let mut iustub_outdir = Path::new(&out_dir).join("libiustub");
+    if !iustub_outdir.exists() {
+        fs::create_dir(&iustub_outdir)?;
+    }
+
+    let iustub_so = iustub_outdir.join("libiustub.so");
+    Command::new("gcc")
+        .arg("-fPIC")
+        .arg("-nostartfiles")
+        .arg("-nodefaultlibs")
+        .arg("--shared")
+        .arg("-o")
+        .arg(iustub_so.to_string_lossy().to_mut())
+        .arg("./libiustub/lib.c")
+        .output()?;
+
+    iustub_outdir = iustub_outdir.canonicalize()?;
 
     println!("cargo:rerun-if-changed=Cargo.toml");
     println!("cargo:rerun-if-changed=./src/*");
     println!("cargo:rerun-if-changed=./libiustub/*");
     println!("cargo:rustc-link-lib=dylib=iustub");
-    println!("cargo:rustc-link-search=native={}", iustub_path);
+    println!(
+        "cargo:rustc-link-search=native={}",
+        iustub_outdir.to_string_lossy()
+    );
+
+    Ok(())
 }
