tianyin/rex

commit 1d2e0b3d7a8ccbc60b8589d2c69b85909a6f0459

inner_unikernel_rt: declare all used kernel symbols as extern "C"
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

inner_unikernel_rt/src/per_cpu.rs
@@ -106,13 +106,11 @@ pub(crate) unsafe fn this_cpu_read<T: PerCPURead<T>>(pcp_addr: u64) -> T {
 /// This is more expensive (in terms of # of insns)
 #[inline(always)]
 unsafe fn __this_cpu_ptr(pcp_addr: u64) -> u64 {
-    let cpu_id =
-        unsafe { this_cpu_read::<u32>(stub::cpu_number_addr()) as usize };
+    let cpu_id = unsafe {
+        this_cpu_read::<u32>(&stub::cpu_number as *const i32 as u64) as usize
+    };
     let __per_cpu_offset = unsafe {
-        core::slice::from_raw_parts(
-            stub::__per_cpu_offset_addr() as *const u64,
-            NR_CPUS as usize,
-        )
+        core::slice::from_raw_parts(stub::__per_cpu_offset, NR_CPUS as usize)
     };
     let this_cpu_offset = __per_cpu_offset[cpu_id];
     pcp_addr + this_cpu_offset

inner_unikernel_rt/src/stub.rs
@@ -1,1 +1,176 @@
-include!(concat!(env!("OUT_DIR"), "/stub.rs"));
+/// All kernel symbols we need should be declared here
+use core::ffi::{c_uchar, VaList};
+
+use crate::bindings::linux::kernel::xdp_buff;
+use crate::bindings::uapi::linux::bpf::{bpf_perf_event_value, bpf_spin_lock};
+use crate::perf_event::bpf_perf_event_data_kern;
+
+/// Functions
+extern "C" {
+    /// `long bpf_trace_printk(const char *fmt, u32 fmt_size, ...)`
+    ///
+    /// Helpers takes at most 5 args so this function takes at most 3 fmt args
+    pub(crate) fn bpf_trace_printk_iu(
+        fmt: *const u8,
+        fmt_size: u32,
+        arg1: u64,
+        arg2: u64,
+        arg3: u64,
+    ) -> i64;
+
+    /// `void *bpf_map_lookup_elem(struct bpf_map *map, const void *key)`
+    ///
+    /// `struct bpf_map` is opaque in our case so make it a `*mut ()`
+    pub(crate) fn bpf_map_lookup_elem(map: *mut (), key: *const ()) -> *mut ();
+
+    /// `long bpf_map_update_elem(struct bpf_map *map, const void *key, const
+    /// void *value, u64 flags)`
+    ///
+    /// `struct bpf_map` is opaque in our case so make it a `*mut ()`
+    pub(crate) fn bpf_map_update_elem(
+        map: *mut (),
+        key: *const (),
+        value: *const (),
+        flags: u64,
+    ) -> i64;
+
+    /// `long bpf_map_delete_elem(struct bpf_map *map, const void *key)`
+    ///
+    /// `struct bpf_map` is opaque in our case so make it a `*mut ()`
+    pub(crate) fn bpf_map_delete_elem(map: *mut (), key: *const ()) -> i64;
+
+    /// `long bpf_map_push_elem(struct bpf_map *map, const void *value, u64
+    /// flags)`
+    ///
+    /// `struct bpf_map` is opaque in our case so make it a `*mut ()`
+    pub(crate) fn bpf_map_push_elem(
+        map: *mut (),
+        value: *const (),
+        flags: u64,
+    ) -> i64;
+
+    /// `long bpf_map_pop_elem(struct bpf_map *map, void *value)`
+    ///
+    /// `struct bpf_map` is opaque in our case so make it a `*mut ()`
+    pub(crate) fn bpf_map_pop_elem(map: *mut (), value: *const ()) -> i64;
+
+    /// `long bpf_map_peek_elem(struct bpf_map *map, void *value)`
+    ///
+    /// `struct bpf_map` is opaque in our case so make it a `*mut ()`
+    pub(crate) fn bpf_map_peek_elem(map: *mut (), value: *const ()) -> i64;
+
+    /// `long bpf_probe_read_kernel(void *dst, u32 size, const void
+    /// *unsafe_ptr)`
+    pub(crate) fn bpf_probe_read_kernel(
+        dst: *mut (),
+        size: u32,
+        unsafe_ptr: *const (),
+    ) -> i64;
+
+    /// `u64 notrace ktime_get_mono_fast_ns(void)`
+    pub(crate) fn ktime_get_mono_fast_ns() -> u64;
+
+    /// `u64 notrace ktime_get_boot_fast_ns(void)`
+    pub(crate) fn ktime_get_boot_fast_ns() -> u64;
+
+    /// `u32 get_random_u32(void)`
+    pub(crate) fn get_random_u32() -> u32;
+
+    /// `long bpf_snprintf_btf(char *str, u32 str_size, struct btf_ptr *ptr, u32
+    /// btf_ptr_size, u64 flags)`
+    pub(crate) fn bpf_snprintf(
+        str: *mut u8,
+        str_size: u32,
+        fmt: *const u8,
+        data: *const u64,
+        data_len: u32,
+    ) -> i64;
+
+    /// `asmlinkage int vprintk(const char *fmt, va_list args)`
+    pub(crate) fn vprintk(fmt: *const c_uchar, args: VaList) -> i32;
+
+    /// `__nocfi noinline void notrace __noreturn iu_landingpad(char *msg)`
+    ///
+    /// The in-kernel panic landingpad for panic recovery
+    pub(crate) fn iu_landingpad(msg: *const u8) -> !;
+
+    /// `long bpf_spin_lock(struct bpf_spin_lock *lock)`
+    pub(crate) fn bpf_spin_lock(lock: *mut bpf_spin_lock) -> i64;
+
+    /// `long bpf_spin_unlock(struct bpf_spin_lock *lock)`
+    pub(crate) fn bpf_spin_unlock(lock: *mut bpf_spin_lock) -> i64;
+
+    /// `asmlinkage void just_return_func(void)`
+    pub(crate) fn just_return_func();
+
+    /// `long bpf_get_stackid_pe(struct bpf_perf_event_data_kern *ctx, struct
+    /// bpf_map *map, u64 flags)`
+    ///
+    /// The specialized version of `bpf_get_stackid` for perf event programs
+    pub(crate) fn bpf_get_stackid_pe(
+        ctx: *const bpf_perf_event_data_kern,
+        map: *mut (),
+        flags: u64,
+    ) -> i64;
+
+    /// `long bpf_perf_prog_read_value(struct bpf_perf_event_data *ctx, struct
+    /// bpf_perf_event_value *buf, u32 buf_size)`
+    pub(crate) fn bpf_perf_prog_read_value(
+        ctx: *const bpf_perf_event_data_kern,
+        buf: &mut bpf_perf_event_value,
+        size: u32,
+    ) -> i64;
+
+    /// `long bpf_xdp_adjust_head(struct xdp_buff *xdp, int offset)`
+    ///
+    /// The compiler complains about some non-FFI safe type, but since the
+    /// kernel is using it fine it should be safe for an FFI call using C ABI
+    #[allow(improper_ctypes)]
+    pub(crate) fn bpf_xdp_adjust_head(xdp: *mut xdp_buff, offset: i32) -> i32;
+
+    /// long bpf_xdp_adjust_tail(struct xdp_buff *xdp, int offset)
+    ///
+    /// The compiler complains about some non-FFI safe type, but since the
+    /// kernel is using it fine it should be safe for an FFI call using C ABI
+    #[allow(improper_ctypes)]
+    pub(crate) fn bpf_xdp_adjust_tail(xdp: *mut xdp_buff, offset: i32) -> i32;
+}
+
+/// Global variables
+extern "C" {
+    /// `DEFINE_PER_CPU_READ_MOSTLY(int, cpu_number);`
+    pub(crate) static cpu_number: i32;
+
+    /// `extern unsigned long volatile __cacheline_aligned_in_smp
+    /// __jiffy_arch_data jiffies;`
+    ///
+    /// Real definition done via linker script (`arch/x86/kernel/vmlinux.lds.S`)
+    /// and is made an alias to `jiffies_64` on x86
+    pub(crate) static jiffies: u64;
+
+    /// `DEFINE_PER_CPU(int, numa_node);`
+    pub(crate) static numa_node: i32;
+
+    /// `unsigned long __per_cpu_offset[NR_CPUS] __read_mostly;`
+    pub(crate) static __per_cpu_offset: *const u64;
+
+    /// `DEFINE_PER_CPU(struct iu_cleanup_entry[64], iu_cleanup_entries)
+    /// ____cacheline_aligned = { 0 };`
+    ///
+    /// Used for cleanup upon panic
+    ///
+    /// Pointee type omitted since this per-cpu variable will never be directly
+    /// dereferenced, it is always used for per-cpu address calculation
+    pub(crate) static iu_cleanup_entries: *mut ();
+
+    /// `DEFINE_PER_CPU(void *, iu_stack_ptr);`
+    ///
+    /// Top of the per-cpu stack for iu programs
+    pub(crate) static iu_stack_ptr: u64;
+
+    /// `DEFINE_PER_CPU(struct task_struct *, current_task)
+    /// ____cacheline_aligned = &init_task;`
+    ///
+    /// Per-cpu point of the current task
+    pub(crate) static current_task: *const ();
+}

inner_unikernel_rt/src/base_helper.rs
@@ -10,7 +10,7 @@ use crate::utils::{to_result, Result};
 // use crate::timekeeping::*;
 
 pub(crate) fn bpf_get_smp_processor_id() -> u32 {
-    unsafe { this_cpu_read(stub::cpu_number_addr()) }
+    unsafe { this_cpu_read(&stub::cpu_number as *const i32 as u64) }
 }
 
 pub(crate) fn bpf_trace_printk(
@@ -19,10 +19,15 @@ pub(crate) fn bpf_trace_printk(
     arg2: u64,
     arg3: u64,
 ) -> Result {
-    let code: extern "C" fn(*const u8, u32, u64, u64, u64) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_trace_printk_iu_addr()) };
-
-    to_result!(code(fmt.as_ptr(), fmt.len() as u32, arg1, arg2, arg3))
+    unsafe {
+        to_result!(stub::bpf_trace_printk_iu(
+            fmt.as_ptr(),
+            fmt.len() as u32,
+            arg1,
+            arg2,
+            arg3
+        ))
+    }
 }
 
 pub(crate) fn bpf_map_lookup_elem<'a, const MT: bpf_map_type, K, V>(
@@ -34,9 +39,10 @@ pub(crate) fn bpf_map_lookup_elem<'a, const MT: bpf_map_type, K, V>(
         return None;
     }
 
-    let helper: extern "C" fn(*mut (), *const K) -> *const V =
-        unsafe { core::mem::transmute(stub::bpf_map_lookup_elem_addr()) };
-    let value = helper(map_kptr, key) as *mut V;
+    let value = unsafe {
+        stub::bpf_map_lookup_elem(map_kptr, key as *const K as *const ())
+            as *mut V
+    };
 
     if value.is_null() {
         None
@@ -56,10 +62,14 @@ pub(crate) fn bpf_map_update_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    let helper: extern "C" fn(*mut (), *const K, *const V, u64) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_map_update_elem_addr()) };
-
-    to_result!(helper(map_kptr, key, value, flags) as i32)
+    unsafe {
+        to_result!(stub::bpf_map_update_elem(
+            map_kptr,
+            key as *const K as *const (),
+            value as *const V as *const (),
+            flags
+        ) as i32)
+    }
 }
 
 pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
@@ -71,10 +81,12 @@ pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    let helper: extern "C" fn(*mut (), *const K) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_map_delete_elem_addr()) };
-
-    to_result!((helper(map_kptr, key)) as i32)
+    unsafe {
+        to_result!(stub::bpf_map_delete_elem(
+            map_kptr,
+            key as *const K as *const ()
+        ) as i32)
+    }
 }
 
 pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
@@ -87,10 +99,13 @@ pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    let helper: extern "C" fn(*mut (), *const V, u64) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_map_push_elem_addr()) };
-
-    to_result!(helper(map_kptr, value, flags) as i32)
+    unsafe {
+        to_result!(stub::bpf_map_push_elem(
+            map_kptr,
+            value as *const V as *const (),
+            flags
+        ) as i32)
+    }
 }
 
 pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
@@ -102,10 +117,12 @@ pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    let helper: extern "C" fn(*mut (), *const V) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_map_pop_elem_addr()) };
-
-    to_result!(helper(map_kptr, value) as i32)
+    unsafe {
+        to_result!(stub::bpf_map_pop_elem(
+            map_kptr,
+            value as *const V as *const ()
+        ) as i32)
+    }
 }
 
 pub(crate) fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
@@ -117,10 +134,12 @@ pub(crate) fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    let helper: extern "C" fn(*mut (), *const V) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_map_peek_elem_addr()) };
-
-    to_result!(helper(map_kptr, value) as i32)
+    unsafe {
+        to_result!(stub::bpf_map_peek_elem(
+            map_kptr,
+            value as *const V as *const ()
+        ) as i32)
+    }
 }
 
 /*
@@ -148,13 +167,13 @@ pub(crate) fn bpf_probe_read_kernel<T>(
     dst: &mut T,
     unsafe_ptr: *const (),
 ) -> Result {
-    let helper: extern "C" fn(*mut (), u32, *const ()) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_probe_read_kernel_addr()) };
-    to_result!(helper(
-        dst as *mut T as *mut (),
-        core::mem::size_of::<T>() as u32,
-        unsafe_ptr,
-    ))
+    unsafe {
+        to_result!(stub::bpf_probe_read_kernel(
+            dst as *mut T as *mut (),
+            core::mem::size_of::<T>() as u32,
+            unsafe_ptr,
+        ))
+    }
 }
 
 pub(crate) fn bpf_strcmp(s1: &str, s2: &str) -> i32 {
@@ -218,27 +237,24 @@ pub(crate) fn bpf_strncmp(s1: &str, s2: &str, cnt: usize) -> i32 {
 }
 
 pub(crate) fn bpf_jiffies64() -> u64 {
-    unsafe { core::ptr::read_volatile(stub::jiffies_addr() as *const u64) }
+    unsafe { core::ptr::read_volatile(&stub::jiffies) }
 }
 
 /// Assumes `CONFIG_USE_PERCPU_NUMA_NODE_ID`
 pub(crate) fn bpf_get_numa_node_id() -> i64 {
-    let id = unsafe { this_cpu_read::<u64>(stub::numa_node_addr()) };
+    let id =
+        unsafe { this_cpu_read::<u64>(&stub::numa_node as *const i32 as u64) };
     id as i64
 }
 
 // This two functions call the original helper directly, so that confirm the
 // return value is correct
 pub(crate) fn bpf_ktime_get_ns_origin() -> u64 {
-    let helper: extern "C" fn() -> u64 =
-        unsafe { core::mem::transmute(stub::ktime_get_mono_fast_ns_addr()) };
-    helper()
+    unsafe { stub::ktime_get_mono_fast_ns() }
 }
 
 pub(crate) fn bpf_ktime_get_boot_ns_origin() -> u64 {
-    let helper: extern "C" fn() -> u64 =
-        unsafe { core::mem::transmute(stub::ktime_get_boot_fast_ns_addr()) };
-    helper()
+    unsafe { stub::ktime_get_boot_fast_ns() }
 }
 
 /*
@@ -265,15 +281,15 @@ pub(crate) fn bpf_snprintf<const N: usize, const M: usize>(
     fmt: &str,
     data: &[u64; M],
 ) -> Result {
-    let helper: extern "C" fn(*mut u8, u32, *const u8, *const u64, u32) -> i64 =
-        unsafe { core::mem::transmute(stub::bpf_snprintf_addr()) };
-    to_result!(helper(
-        str.as_mut_ptr(),
-        N as u32,
-        fmt.as_ptr(),
-        data.as_ptr(),
-        M as u32,
-    ) as i32)
+    unsafe {
+        to_result!(stub::bpf_snprintf(
+            str.as_mut_ptr(),
+            N as u32,
+            fmt.as_ptr(),
+            data.as_ptr(),
+            M as u32,
+        ) as i32)
+    }
 }
 
 macro_rules! base_helper_defs {

inner_unikernel_rt/src/debug.rs
@@ -9,7 +9,5 @@ use crate::stub;
 #[allow(improper_ctypes_definitions)]
 #[no_mangle]
 pub(crate) unsafe extern "C" fn printk(fmt: &str, mut ap: ...) -> c_int {
-    let printk_kern: extern "C" fn(*const c_uchar, VaList) -> i32 =
-        unsafe { core::mem::transmute(stub::vprintk_addr()) };
-    printk_kern(fmt.as_ptr() as *const c_uchar, ap.as_va_list())
+    unsafe { stub::vprintk(fmt.as_ptr() as *const c_uchar, ap.as_va_list()) }
 }

inner_unikernel_rt/src/kprobe/kprobe_impl.rs
@@ -49,7 +49,7 @@ impl<'a> kprobe<'a> {
     // not safe to expose to the user progs
     pub fn bpf_override_return(&self, regs: &mut pt_regs, rc: u64) -> i32 {
         regs.rax = rc;
-        regs.rip = unsafe { stub::just_return_func_addr() };
+        regs.rip = unsafe { stub::just_return_func as *const () as u64 };
         return 0;
     }
 }

inner_unikernel_rt/src/panic.rs
@@ -54,8 +54,9 @@ impl<'a> CleanupEntries<'a> {
     pub(crate) fn cleanup_entries_this_cpu() -> CleanupEntries<'a> {
         let entries: &mut [CleanupEntry];
         unsafe {
-            let entries_ptr: *mut CleanupEntry =
-                this_cpu_ptr_mut(stub::iu_cleanup_entries_addr());
+            let entries_ptr: *mut CleanupEntry = this_cpu_ptr_mut(
+                &stub::iu_cleanup_entries as *const *mut () as u64,
+            );
             entries =
                 core::slice::from_raw_parts_mut(entries_ptr, ENTRIES_SIZE);
         }
@@ -115,7 +116,7 @@ unsafe fn __iu_check_stack() {
             "ja 1f",
             "call __iu_handle_stack_overflow",
             "1:",
-            in("r10") stub::iu_stack_ptr_addr(),
+            in("r10") &stub::iu_stack_ptr as *const u64 as u64,
         );
     }
 }
@@ -151,9 +152,5 @@ fn panic(info: &PanicInfo) -> ! {
         msg[..s.len()].copy_from_slice(s.as_bytes());
     }
 
-    unsafe {
-        let landingpad: unsafe extern "C" fn(*const u8) -> ! =
-            core::mem::transmute(stub::iu_landingpad_addr());
-        landingpad(msg.as_ptr())
-    }
+    unsafe { stub::iu_landingpad(msg.as_ptr()) }
 }

inner_unikernel_rt/src/perf_event/mod.rs
@@ -1,4 +1,5 @@
 mod binding;
 mod perf_event_impl;
 
+pub(crate) use binding::bpf_perf_event_data_kern;
 pub use perf_event_impl::*;

inner_unikernel_rt/src/perf_event/perf_event_impl.rs
@@ -79,15 +79,9 @@ impl<'a> perf_event<'a> {
     ) -> Result {
         let size = core::mem::size_of::<bpf_perf_event_value>() as u32;
 
-        let helper: extern "C" fn(
-            *const bpf_perf_event_data_kern,
-            &mut bpf_perf_event_value,
-            u32,
-        ) -> i64 = unsafe {
-            core::mem::transmute(stub::bpf_perf_prog_read_value_addr())
-        };
-
-        to_result!(helper(ctx.kptr, buf, size))
+        unsafe {
+            to_result!(stub::bpf_perf_prog_read_value(ctx.kptr, buf, size))
+        }
     }
 
     pub fn bpf_get_stackid_pe<K, V>(
@@ -101,14 +95,9 @@ impl<'a> perf_event<'a> {
             return Err(EINVAL as i32);
         }
 
-        let helper: extern "C" fn(
-            *const bpf_perf_event_data_kern,
-            *mut (),
-            u64,
-        ) -> i64 =
-            unsafe { core::mem::transmute(stub::bpf_get_stackid_pe_addr()) };
-
-        to_result!(helper(ctx.kptr, map_kptr, flags))
+        unsafe {
+            to_result!(stub::bpf_get_stackid_pe(ctx.kptr, map_kptr, flags))
+        }
     }
 
     pub fn bpf_get_current_task(&self) -> Option<TaskStruct> {

commit 1aac963b58244490af60a56c8a7ecacdb33d489a

inner_unikernel_rt: fix incorrect access to __per_cpu_offset
The previous way of accessing the __per_cpu_offset array is incorrectly
treating __per_cpu_offset[0] as the start address of the array (which
should really be &__per_cpu_offset[0] or just &__per_cpu_offset). This
leads to a page fault that cannot be handled when a per-cpu variable is
accessed using the more expensive this_cpu_ptr() / this_cpu_ptr_mut().

Declare the symbol as an extern array in the RT crate so that we can
directly index into it without manually playing with the address.

Fixes: 1d2e0b3d7a8c ("inner_unikernel_rt: declare all used kernel symbols as extern \"C\"")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

inner_unikernel_rt/src/per_cpu.rs
@@ -109,11 +109,8 @@ unsafe fn __this_cpu_ptr(pcp_addr: u64) -> u64 {
     let cpu_id = unsafe {
         this_cpu_read::<u32>(&stub::cpu_number as *const i32 as u64) as usize
     };
-    let __per_cpu_offset = unsafe {
-        core::slice::from_raw_parts(stub::__per_cpu_offset, NR_CPUS as usize)
-    };
-    let this_cpu_offset = __per_cpu_offset[cpu_id];
-    pcp_addr + this_cpu_offset
+
+    pcp_addr + stub::__per_cpu_offset[cpu_id]
 }
 
 pub(crate) unsafe fn this_cpu_ptr<T>(pcp_addr: u64) -> *const T {

inner_unikernel_rt/src/stub.rs
@@ -1,6 +1,7 @@
 /// All kernel symbols we need should be declared here
 use core::ffi::{c_uchar, VaList};
 
+use crate::bindings::linux::kernel::CONFIG_NR_CPUS as NR_CPUS;
 use crate::bindings::linux::kernel::{sk_buff, xdp_buff};
 use crate::bindings::uapi::linux::bpf::{bpf_perf_event_value, bpf_spin_lock};
 use crate::perf_event::bpf_perf_event_data_kern;
@@ -173,7 +174,7 @@ extern "C" {
     pub(crate) static numa_node: i32;
 
     /// `unsigned long __per_cpu_offset[NR_CPUS] __read_mostly;`
-    pub(crate) static __per_cpu_offset: *const u64;
+    pub(crate) static __per_cpu_offset: [u64; NR_CPUS as usize];
 
     /// `DEFINE_PER_CPU(struct iu_cleanup_entry[64], iu_cleanup_entries)
     /// ____cacheline_aligned = { 0 };`