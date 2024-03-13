57940ffc3be6737a75a840d16c2b017c3533e477
Jinghao Jia <jinghao7@illinois.edu>
2024-03-13 04:59:15 -0500

    samples/memcached_benchmark: revise plot for paper
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- samples/memcached_benchmark/plot.py
+++ samples/memcached_benchmark/plot.py
@@ -3,6 +3,10 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
+from matplotlib import rc
+rc('font',**{'family':'serif','serif':['Times']})
+rc('text', usetex=True)
+
 with open('bmc-results.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
 
@@ -12,29 +16,39 @@ with open('bmc-results.csv', newline='') as csvfile:
     bpf = [float(row[2]) for row in data]
     rust = [float(row[3]) for row in data]
 
-penguin_means = {
-    'Vanilla': np.array(vanilla) / 1000 / 1000,
-    'eBPF': np.array(bpf) / 1000 / 1000,
-    'REX': np.array(rust) / 1000 / 1000,
+max_val = max(*vanilla, *bpf, *rust) / 1000000
+
+bmc_results = {
+    'MemcachedSR': np.array(vanilla) / 1000000,
+    'eBPF-BMC': np.array(bpf) / 1000000,
+    'REX-BMC': np.array(rust) / 1000000,
 }
 
-with plt.style.context('seaborn-v0_8-talk'):
+with plt.style.context('seaborn-v0_8-paper'):
     x = np.arange(len(data))  # the label locations
     width = 0.25  # the width of the bars
     multiplier = 0
+    y = np.arange(0, max_val, 0.2)
 
     fig, ax = plt.subplots(layout='constrained')
-    fig.set_size_inches(8, 4.8)
+    # fig.set_size_inches(8, 4.8) # for slides
+    fig.set_size_inches(4.5, 2.7)
 
-    for attribute, measurement in penguin_means.items():
+    for setup, throughput in bmc_results.items():
         offset = width * multiplier
-        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        rects = ax.bar(x + offset, throughput, width, label=setup)
         multiplier += 1
 
+    y_min, y_max = ax.get_ylim()
+    ax.set_ylim(y_min, y_max * 1.1)
+
     # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_xlabel('# of cores')
-    ax.set_ylabel('Throughput\n(MReq/s)')
+    ax.set_xlabel(r'\# of cores', size='large')
+    ax.set_ylabel('Throughput\n(MReq/s)', size='large')
     ax.set_xticks(x + width, x + 1)
-    ax.legend(loc='upper left', ncols=3)
+    ax.set_xticklabels(ax.get_xticklabels(), size='large')
+    ax.set_yticks(y, list(map(lambda x: '%.1f' % x, y)))
+    ax.set_yticklabels(ax.get_yticklabels(), size='large')
+    ax.legend(loc='upper left', ncols=3, fontsize='small')
 
     plt.savefig("bmc.pdf")
