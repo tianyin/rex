64fb75dafa670ad890c57780b95680e2dabffd44
Ruowen Qin <ruowenq2@illinois.edu>
2024-04-05 03:44:35 +0000

    samples/spinlock_cleanup_benchmark: use xdp instead of tracepoint
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- samples/spinlock_cleanup_benchmark/loader.c
+++ samples/spinlock_cleanup_benchmark/loader.c
@@ -1,26 +1,39 @@
-#include <fcntl.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sched.h>
 #include <string.h>
-#include <sys/ioctl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <locale.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <linux/types.h>
+#include <linux/if_link.h>
 
-#include <linux/perf_event.h>
-#include <linux/unistd.h>
+#include <bpf.h>
+#include <libbpf.h>
 
 #include "libiu.h"
-#include <libbpf.h>
 
 #define EXE "./target/x86_64-unknown-linux-gnu/release/spinlock_cleanup_benchmark"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	int trace_pipe_fd;
-	struct bpf_object *obj;
-	struct bpf_program *prog;
 	struct bpf_link *link = NULL;
-
+	struct bpf_program *prog;
+	struct bpf_object *obj;
+	char filename[256];
 	iu_set_debug(1); // enable debug info
+	int ret;
+
+	int interface_idx = atoi(argv[1]);
+	unsigned int xdp_flags = 0;
+	xdp_flags |= XDP_FLAGS_SKB_MODE;
 
 	obj = iu_object__open(EXE);
 	if (!obj) {
@@ -30,25 +43,15 @@ int main(void)
 
 	prog = bpf_object__find_program_by_name(obj, "iu_prog1");
 	if (!prog) {
- 		fprintf(stderr, "_start not found\n");
- 		exit(1);
- 	}
-
-	link = bpf_program__attach(prog);
-	if (libbpf_get_error(link)) {
-		fprintf(stderr, "ERROR: bpf_program__attach failed\n");
-		link = NULL;
+		printf("finding a prog in obj file failed\n");
 		goto cleanup;
 	}
+	int xdp_main_prog_fd = bpf_program__fd(prog);
 
-	trace_pipe_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/trace_pipe",
-		O_RDONLY);
+	if (bpf_set_link_xdp_fd(interface_idx, xdp_main_prog_fd, xdp_flags) < 0) {
+		fprintf(stderr, "ERROR: xdp failed");
+	}
 
-	for (;;) {
-        char c;
-        if (read(trace_pipe_fd, &c, 1) == 1)
-            putchar(c);
-    }
 
 cleanup:
 	bpf_link__destroy(link);
--- samples/spinlock_cleanup_benchmark/src/main.rs
+++ samples/spinlock_cleanup_benchmark/src/main.rs
@@ -3,12 +3,12 @@
 
 extern crate inner_unikernel_rt;
 
+use inner_unikernel_rt::bpf_printk;
 use inner_unikernel_rt::linux::bpf::{bpf_spin_lock, BPF_MAP_TYPE_ARRAY};
 use inner_unikernel_rt::map::IUMap;
 use inner_unikernel_rt::spinlock::iu_spinlock_guard;
-use inner_unikernel_rt::tracepoint::*;
+use inner_unikernel_rt::xdp::*;
 use inner_unikernel_rt::{entry_link, Result, MAP_DEF};
-use inner_unikernel_rt::bpf_printk;
 
 #[repr(C)]
 struct MapEntry {
@@ -19,7 +19,7 @@ struct MapEntry {
 MAP_DEF!(map_array, u32, MapEntry, BPF_MAP_TYPE_ARRAY, 256, 0);
 
 #[inline(always)]
-fn iu_prog1_fn(obj: &tracepoint, _: tp_ctx) -> Result {
+fn iu_prog1_fn(obj: &xdp, _: &mut xdp_md) -> Result {
     if let Some(entry) = obj.bpf_map_lookup_elem(&map_array, &0) {
         let start = obj.bpf_ktime_get_ns();
         {
@@ -27,12 +27,12 @@ fn iu_prog1_fn(obj: &tracepoint, _: tp_ctx) -> Result {
         }
         let end = obj.bpf_ktime_get_ns();
         bpf_printk!(obj, "Spinlock allocation and cleanup: %llu ns", end - start);
-        Ok(0)
+        Ok(XDP_PASS as i32)
     } else {
         bpf_printk!(obj, "Unable to look up map");
-        Err(0)
+        Err(XDP_PASS as i32)
     }
 }
 
-#[entry_link(inner_unikernel/tracepoint/syscalls/sys_enter_dup)]
-static PROG: tracepoint = tracepoint::new(iu_prog1_fn, "iu_prog1", tp_type::Void);
+#[entry_link(inner_unikernel/xdp)]
+static PROG1: xdp = xdp::new(iu_prog1_fn, "iu_prog1", BPF_PROG_TYPE_XDP as u64);
