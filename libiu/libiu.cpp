tianyin/rex

commit 15d0e80810fa729855001a972101825ad85ee543

implement libiu (lib-inner-unikernel)

libiu/libiu.cpp
@@ -1,0 +1,388 @@
+#include <cerrno>
+#include <cstring>
+#include <cstdint>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <linux/bpf.h>
+#include <linux/unistd.h>
+
+#include <libelf.h>
+
+#include "libiu.h"
+
+#define BPF_PROG_LOAD_DJW 0x1234beef
+
+namespace { // begin anynomous namespace
+
+static int debug = 0;
+
+static inline int64_t get_file_size(int fd)
+{
+	struct stat st;
+	if (fstat(fd, &st) < 0) {
+		perror("fstat");
+		return -1;
+	}
+
+	return st.st_size;
+}
+
+template<typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
+static inline T val_from_buf(const unsigned char *buf)
+{
+	return *(const T *)(buf);
+}
+
+template<typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
+static inline void val_to_buf(unsigned char *buf, const T val)
+{
+	*(T *)(buf) = val;
+}
+
+static inline long bpf(__u64 cmd, union bpf_attr *attr, unsigned int size)
+{
+	return syscall(__NR_bpf, cmd, attr, size);
+}
+
+// This struct is POD, meaning the C++ standard guarantees the same memory
+// layout as that of the equivalent C struct
+// https://stackoverflow.com/questions/422830/structure-of-a-c-object-in-memory-vs-a-struct
+struct map_def {
+	uint32_t map_type;
+	uint32_t key_size;
+	uint32_t val_size;
+	uint32_t max_size;
+	uint32_t map_flag;
+};
+
+class iu_map {
+	map_def def;
+	int fd;
+	const std::string name; // for debug msg
+
+public:
+	iu_map() = delete;
+	iu_map(const Elf_Data *, Elf64_Addr, Elf64_Off, const char *);
+	~iu_map() = default;
+
+	int create(const std::string &name);
+
+	friend class iu_prog; // for debug msg
+};
+
+iu_map::iu_map(const Elf_Data *data, Elf64_Addr base, Elf64_Off off,
+		const char *c_name) : fd(-1), name(c_name)
+{
+	this->def = *(map_def *)((unsigned char *)data->d_buf + off - base);
+
+	if (debug) {
+		std::cerr << "sym_name=" << c_name << std::endl;
+		std::cerr << "map_type=" << this->def.map_type << std::endl;
+		std::cerr << "key_size=" << this->def.key_size << std::endl;
+		std::cerr << "val_size=" << this->def.val_size << std::endl;
+		std::cerr << "max_size=" << this->def.max_size << std::endl;
+		std::cerr << "map_flag=" << this->def.map_flag << std::endl;
+	}
+}
+
+int iu_map::create(const std::string &name)
+{
+	const auto &def = this->def;
+
+	union bpf_attr attr;
+	memset(&attr, 0, sizeof(attr));
+
+	attr.map_type = def.map_type;
+	attr.key_size = def.key_size;
+	attr.value_size = def.val_size;
+	attr.max_entries = def.max_size;
+	attr.map_flags = def.map_flag;
+
+	if (name.size() < BPF_OBJ_NAME_LEN)
+		memcpy(attr.map_name, name.c_str(), name.size());
+
+	this->fd = (int)bpf(BPF_MAP_CREATE, &attr, sizeof(attr));
+	return this->fd;
+}
+
+class iu_prog {
+	std::unordered_map<Elf64_Off, iu_map> map_defs;
+	std::vector<std::pair<Elf64_Sym *, std::string>> map_ptrs;
+	Elf *elf;
+	Elf_Scn *symtab_scn;
+	Elf_Scn *maps_scn;
+	size_t file_size;
+	unsigned char *file_map;
+	int prog_fd;
+
+	int parse_scns();
+	int parse_maps();
+
+public:
+	iu_prog() = delete;
+	explicit iu_prog(const char *);
+	iu_prog(const iu_prog &) = delete;
+	iu_prog(iu_prog &&) = delete;
+	~iu_prog();
+
+	iu_prog &operator=(const iu_prog &) = delete;
+	iu_prog &operator=(iu_prog &&) = delete;
+
+	// Making this a separate function to avoid exceptions in constructor
+	int parse_elf();
+	int fix_maps();
+	int load();
+};
+
+iu_prog::iu_prog(const char *c_path) : map_defs(), map_ptrs(),
+	symtab_scn(nullptr), maps_scn(nullptr), prog_fd(-1)
+{
+	int fd = open(c_path, 0, O_RDONLY);
+	this->elf = elf_begin(fd, ELF_C_READ_MMAP, NULL);
+	file_size = get_file_size(fd);
+	file_map = (unsigned char *)mmap(NULL, file_size, PROT_READ | PROT_WRITE, 
+			MAP_PRIVATE, fd, 0);
+	close(fd);
+}
+
+iu_prog::~iu_prog()
+{
+	if (this->elf)
+		elf_end(this->elf);
+
+	if (file_map)
+		munmap(file_map, file_size);
+}
+
+int iu_prog::parse_scns()
+{	
+	size_t shstrndx;
+
+	if(elf_getshdrstrndx(elf, &shstrndx)) {
+		std::cerr << "elf: failed to get section names section index"
+			<< std::endl;
+		return -1;
+	}
+
+	for (auto scn = elf_nextscn(elf, NULL); scn; scn = elf_nextscn(elf, scn)) {
+		char *name;
+		int idx = elf_ndxscn(scn);
+		Elf64_Shdr *sh = elf64_getshdr(scn);
+		if (!sh) {
+			std::cerr << "elf: failed to get section header, idx=" << idx
+				<< std::endl;
+			return -1;
+		}
+
+		name = elf_strptr(elf, shstrndx, sh->sh_name);
+
+		if (!name) {
+			std::cerr << "nelf: failed to get section name" << std::endl;
+			return -1;
+		}
+
+		if (debug)
+			std::cerr << "section " << name << ", idx=" << idx << std::endl;
+
+		if (sh->sh_type == SHT_SYMTAB)
+			this->symtab_scn = scn;
+		else if (!strcmp(".maps", name))
+			this->maps_scn = scn;
+	}
+	
+	return 0;
+}
+
+int iu_prog::parse_maps()
+{
+	Elf_Data *maps = elf_getdata(maps_scn, 0);
+	Elf_Data *syms = elf_getdata(symtab_scn, 0);
+	int nr_syms, nr_maps = 0, maps_shndx;
+	size_t strtabidx;
+	Elf64_Addr maps_shaddr;
+
+	if (!maps || !syms) {
+		std::cerr << "elf: failed to get map/symbol definitions" << std::endl;
+		return -1;
+	}
+
+	strtabidx = elf64_getshdr(symtab_scn)->sh_link;
+	maps_shndx = elf_ndxscn(maps_scn);
+	maps_shaddr = elf64_getshdr(maps_scn)->sh_addr;
+	nr_syms = syms->d_size / sizeof(Elf64_Sym);
+
+	for (int i = 0; i < nr_syms; i++) {
+		Elf64_Sym *sym = (Elf64_Sym *)syms->d_buf + i;
+		char *name;
+
+		if (sym->st_shndx != maps_shndx ||
+			ELF64_ST_TYPE(sym->st_info) != STT_OBJECT)
+			continue;
+
+		name = elf_strptr(elf, strtabidx, sym->st_name);
+
+		if (debug) {
+			std::cerr << "symbol: " << name << ", st_value=0x" << std::hex
+				<< sym->st_value << ", st_size=" << std::dec << sym->st_size
+				<< std::endl;
+		}
+
+		if (sym->st_size == sizeof(struct map_def)) {
+			map_defs.try_emplace(sym->st_value, maps, maps_shaddr,
+					sym->st_value, name);
+		} else if (sym->st_size == sizeof(struct map_def *)) {
+			map_ptrs.emplace_back(sym, name);
+		}
+
+		nr_maps++;
+	}
+
+	if (debug)
+		std::cerr << "# of symbols in \".maps\": " << nr_maps << std::endl;
+
+	return 0;
+}
+
+int iu_prog::parse_elf()
+{
+	int ret;
+
+	if (!elf) {
+		std::cerr << "elf: failed to open object" << std::endl;
+		return -1;
+	}
+
+	ret = this->parse_scns();
+	ret = ret < 0 ? : this->parse_maps();
+
+	return ret;
+}
+
+int iu_prog::fix_maps()
+{	
+	Elf64_Addr maps_shaddr = elf64_getshdr(maps_scn)->sh_addr; 
+	Elf64_Off maps_shoff = elf64_getshdr(maps_scn)->sh_offset;
+	
+	if (debug) {	
+		std::cerr << ".maps section file offset=0x" << std::hex
+			<< elf64_getshdr(maps_scn)->sh_offset << std::dec << std::endl;
+	}
+
+	if (this->file_size < 0 || (int64_t)this->file_map < 0) {
+		perror("mmap");
+		return -1;
+	}
+
+	for (const auto &ptr: map_ptrs) {
+		size_t pos = ptr.first->st_value - maps_shaddr + maps_shoff;
+		uint64_t map_addr = val_from_buf<uint64_t>(&this->file_map[pos]);
+		const auto it = map_defs.find(map_addr);
+		int map_fd;
+
+		if (it == map_defs.end()) {
+			std::cerr << "map def not found" << std::endl;
+			continue;
+		}
+
+		if (debug) {
+			std::cerr << "map_ptr=0x" << std::hex << map_addr << std::dec
+				<< std::endl;
+			std::cerr << "pointed obj name: " << it->second.name << std::endl;
+			std::cerr << "map_ptr_offset=0x" << std::hex 
+				<< ptr.first->st_value << std::dec << std::endl;
+		}
+
+		map_fd = it->second.create(ptr.second);
+		if (map_fd < 0) {
+			perror("bpf_map_create");
+			return -1;
+		}
+
+		if (debug)
+			std::cerr << "map_fd=" << map_fd << std::endl;
+
+		val_to_buf<uint64_t>(&this->file_map[pos], map_fd);
+	}
+
+	return 0;
+}
+
+int iu_prog::load()
+{
+	int fd;
+	auto arr = std::make_unique<uint64_t[]>(map_ptrs.size());
+	union bpf_attr attr;
+	int idx = 0, ret = 0;
+
+	// TODO: Will have race condition if multiple progs loaded at same time
+	std::ofstream output("rust.out", std::ios::out | std::ios::binary);
+	
+	output.write((char *)this->file_map, this->file_size);
+	output.close();
+
+	fd = open("rust.out", O_RDONLY);
+
+	for (auto &it: map_ptrs) {
+		arr[idx++] = it.first->st_value;
+	}
+
+	memset(&attr, 0, sizeof(attr));
+
+	attr.prog_type = BPF_PROG_TYPE_TRACEPOINT;
+	memcpy(attr.prog_name, "map_test", sizeof("map_test"));
+	attr.rustfd = fd;
+	attr.license = (__u64)"GPL";
+
+	attr.map_offs = (__u64)arr.get();
+	attr.map_cnt = map_ptrs.size();
+
+	ret = bpf(BPF_PROG_LOAD_DJW, &attr, sizeof(attr));
+
+	if (ret < 0) {
+		perror("bpf_prog_load");
+		ret = -1;
+	}
+
+	if (remove("rust.out") < 0) {
+		perror("remove");
+		ret = -1;
+	}
+
+	return ret;
+}
+
+} // end anynomous namespace
+
+void iu_set_debug(const int val)
+{
+	debug = val;
+}
+
+int iu_prog_load(const char *file_path) 
+{
+	int ret;
+
+	if (elf_version(EV_CURRENT) == EV_NONE) {
+		fprintf(stderr, "failed to init libelf");
+		return 1;
+	}
+
+	iu_prog prog(file_path);
+
+	ret = prog.parse_elf();
+	ret = ret ? : prog.fix_maps();
+	ret = ret ? : prog.load();
+
+	return ret;
+}

libiu/.gitignore
@@ -1,0 +1,3 @@
+libiu.a
+libiu.o
+libiu.so

libiu/Makefile
@@ -1,0 +1,21 @@
+LINUX ?= ../linux
+
+CXX = clang++
+CXXFLAGS = -g -pipe -std=c++17 -fPIC -I${LINUX}/usr/include
+LDFLAGS = -Wl,--as-needed
+LD = clang++
+AR = llvm-ar
+
+all: libiu.a libiu.so
+
+libiu.a: libiu.o
+	${AR} rcs $@ $<
+
+libiu.so: libiu.o
+	${LD} ${LDFLAGS} -shared $< -lelf -o $@
+
+libiu.o: libiu.cpp libiu.h
+	${CXX} ${CXXFLAGS} -c libiu.cpp -o $@
+
+clean:
+	rm -f libiu.o libiu.a

libiu/libiu.h
@@ -1,0 +1,15 @@
+#ifndef LIBIU_H
+#define LIBIU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void iu_set_debug(const int val);
+int iu_prog_load(const char *file_path);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBIU_H

samples/map_test/Makefile
@@ -1,14 +1,15 @@
 LINUX ?= $(HOME)/linux
 
-INC = ${LINUX}/usr/include
+LINUX_INC = ${LINUX}/usr/include
+LIBIU_DIR = "../../libiu"
 
 RUST_FLAGS = -Z macro-backtrace -C debuginfo=0 -C opt-level=0 \
 		-C link-arg=-nostartfiles -A unused -A non_camel_case_types \
 		-A non_upper_case_globals -A non_snake_case
 
-all: target/debug/rust loader event-trigger
+all: target/debug/map_test loader event-trigger
 
-target/debug/rust: ./src/*.rs prep_interface.py ${LINUX}/vmlinux
+target/debug/map_test: ./src/*.rs prep_interface.py ${LINUX}/vmlinux
 	python3 prep_interface.py  ${LINUX}
 	cargo rustc -vv -- ${RUST_FLAGS}
 
@@ -21,12 +22,12 @@ mir: ./src/*.rs prep_interface.py ${LINUX}/vmlinux
 asm: ./src/*.rs prep_interface.py ${LINUX}/vmlinux
 	cargo rustc -vv -- ${RUST_FLAGS} --emit asm
 
-loader: loader.cpp
-	clang++ -I${INC} -g $< -o $@ -lelf
+loader: loader.c
+	clang -I${LINUX_INC} -I${LIBIU_DIR} -g $< ${LIBIU_DIR}/libiu.a -lelf -lstdc++ -o $@
 
 event-trigger: event-trigger.c
-	clang -I${INC} -g $< -o $@
+	clang -I${LINUX_INC} -g $< -o $@
 
 clean:
 	cargo clean
-	rm -rf loader even-trigger
+	rm -rf loader event-trigger

samples/map_test/loader.c
@@ -1,0 +1,64 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <linux/perf_event.h>
+#include <linux/unistd.h>
+
+#include "libiu.h"
+
+#define EXE "./target/debug/map_test"
+
+static inline long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+							int cpu, int  group_fd, unsigned long flags)
+{
+	return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+}
+
+int main(void)
+{
+	int prog_fd, trace_id_fd, perf_event_fd, trace_pipe_fd;
+	char config_str[256];
+	struct perf_event_attr p_attr;
+
+	iu_set_debug(1); // enable debug info
+
+	prog_fd = iu_prog_load(EXE);
+	
+	if (prog_fd < 0)
+		exit(1);
+
+	trace_id_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/events/syscalls/sys_enter_dup/id", O_RDONLY);
+	if (trace_id_fd < 0) {
+		perror("openat(/sys/kernel/debug/tracing/events/syscalls/sys_enter_dup/id)");
+		exit(1);
+	}
+	read(trace_id_fd, config_str, 256);
+	close(trace_id_fd);
+
+	memset(&p_attr, 0, sizeof(p_attr));
+	p_attr.type = PERF_TYPE_TRACEPOINT;
+	p_attr.size = PERF_ATTR_SIZE_VER5;
+	p_attr.config = atoi(config_str);
+	perf_event_fd = perf_event_open(&p_attr, -1, 0, -1, PERF_FLAG_FD_CLOEXEC);
+	if (perf_event_fd < 0) {
+		perror("perf_event_open");
+		exit(1);
+	}
+
+	ioctl(perf_event_fd, PERF_EVENT_IOC_SET_BPF, prog_fd);
+	ioctl(perf_event_fd, PERF_EVENT_IOC_ENABLE, 0);
+
+	trace_pipe_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/trace_pipe", O_RDONLY);
+
+	for (;;) {
+        char c;
+        if (read(trace_pipe_fd, &c, 1) == 1)
+            putchar(c);
+    }
+
+	return 0;
+}

samples/map_test/loader.cpp
@@ -1,332 +1,0 @@
-#include <cerrno>
-#include <cstring>
-#include <cstdint>
-#include <cstdio>
-#include <fstream>
-#include <iostream>
-#include <memory>
-#include <unordered_map>
-#include <unordered_set>
-
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include <linux/bpf.h>
-#include <linux/perf_event.h>
-#include <linux/unistd.h>
-
-#include <libelf.h>
-
-#define EXE "./target/debug/map_test"
-
-#define BPF_PROG_LOAD_DJW 0x1234beef
-
-namespace { // begin anynomous namespace
-
-// This struct is POD, meaning the C++ standard guarantees the same memory
-// layout as that of the equivalent C struct
-// https://stackoverflow.com/questions/422830/structure-of-a-c-object-in-memory-vs-a-struct
-struct map_def {
-	uint32_t map_type;
-	uint32_t key_size;
-	uint32_t val_size;
-	uint32_t max_size;
-	uint32_t map_flag;
-};
-
-struct iu_map {
-	const std::string name;
-	const Elf64_Off offset;
-	map_def def;
-	int fd;
-
-	iu_map() = delete;
-	iu_map(char *c_name, Elf64_Off off) : name(c_name), offset(off), def({ 0 }), fd(-1) {}
-	~iu_map() = default;
-};
-
-
-static int collect_map_def(const Elf_Data &data, iu_map &map_obj, Elf64_Addr base)
-{
-	Elf64_Off offset = map_obj.offset;
-	map_def *def = reinterpret_cast<map_def *>((unsigned char *)data.d_buf + offset - base);
-
-	std::cerr << "map_name=" << map_obj.name << std::endl;
-	std::cerr << "map_type=" << def->map_type << std::endl;
-	std::cerr << "key_size=" << def->key_size << std::endl;
-	std::cerr << "val_size=" << def->val_size << std::endl;
-	std::cerr << "max_size=" << def->max_size << std::endl;
-	std::cerr << "map_flag=" << def->map_flag << std::endl;
-
-	map_obj.def = *def;
-
-	return 0;
-}
-
-static size_t get_file_size(int fd)
-{
-	struct stat st;
-	if (fstat(fd, &st) < 0) {
-		perror("fstat");
-		return -1;
-	}
-
-	return st.st_size;
-}
-
-template<typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
-static inline T val_from_buf(const unsigned char *buf)
-{
-	return *reinterpret_cast<const T *>(buf);
-}
-
-template<typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
-static inline void val_to_buf(unsigned char *buf, const T val)
-{
-	*reinterpret_cast<T *>(buf) = val;
-}
-
-static inline long bpf(__u64 cmd, union bpf_attr *attr, unsigned int size)
-{
-	return syscall(__NR_bpf, cmd, attr, size);
-}
-
-static long map_create(const iu_map &map_obj)
-{
-	const auto &def = map_obj.def;
-	const auto &name = map_obj.name;
-
-	union bpf_attr attr;
-	memset(&attr, 0, sizeof(attr));
-
-	attr.map_type = def.map_type;
-	attr.key_size = def.key_size;
-	attr.value_size = def.val_size;
-	attr.max_entries = def.max_size;
-	attr.map_flags = def.map_flag;
-
-	if (name.size() < BPF_OBJ_NAME_LEN)
-		memcpy(attr.map_name, name.c_str(), name.size());
-
-	return bpf(BPF_MAP_CREATE, &attr, sizeof(attr));
-	//return 0x1122334455667788;
-}
-
-static long prog_load(const char *file,
-		const std::unordered_map<Elf64_Sym *, std::string> &map_ptrs)
-{
-	int fd = open(file, O_RDONLY);
-	auto arr = std::make_unique<uint64_t[]>(map_ptrs.size());
-	union bpf_attr attr;
-	int idx = 0;
-
-	for (auto &it: map_ptrs) {
-		arr[idx++] = it.first->st_value;
-	}
-
-	memset(&attr, 0, sizeof(attr));
-
-	attr.prog_type = BPF_PROG_TYPE_TRACEPOINT;
-	memcpy(attr.prog_name, "map_test", sizeof("map_test"));
-	attr.rustfd = fd;
-	attr.license = (__u64)"GPL";
-
-	attr.map_offs = (__u64)arr.get();
-	attr.map_cnt = map_ptrs.size();
-
-	return bpf(BPF_PROG_LOAD_DJW, &attr, sizeof(attr));
-}
-
-static inline long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
-							int cpu, int  group_fd, unsigned long flags)
-{
-	return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
-}
-
-} // end anynomous namespace
-
-
-int main(void)
-{
-	int fd, idx = 0, maps_shndx = -1, nr_syms, nr_maps = 0, i;
-	Elf *elf;
-	Elf_Data *data, *symbols;
-	Elf_Scn *scn = NULL;
-	Elf64_Shdr *sh = NULL;
-	size_t shstrndx, strtabidx;;
-	char *name;
-	std::unordered_map<Elf64_Off, iu_map> map_defs;
-	std::unordered_map<Elf64_Sym *, std::string> map_ptrs;
-	unsigned char *file_map;
-	size_t file_size;
-	int prog_fd, trace_id_fd, perf_event_fd, trace_pipe_fd;
-	char config_str[256];
-	struct perf_event_attr p_attr;
-
-	if (elf_version(EV_CURRENT) == EV_NONE) {
-		fprintf(stderr, "failed to init libelf");
-		return 1;
-	}
-
-	fd = open(EXE, 0, O_RDONLY);
-	elf = elf_begin(fd, ELF_C_READ_MMAP, NULL);
-
-	if (!elf) {
-		std::cerr << "failed to open object" << std::endl;
-		return 1;
-	}
-
-	if(elf_getshdrstrndx(elf, &shstrndx)) {
-		std::cerr << "elf: failed to get section names section index"
-				  << std::endl;
-		return 1;
-	}
-
-	while ((scn = elf_nextscn(elf, scn)) != NULL) {
-		idx = elf_ndxscn(scn);
-		sh = elf64_getshdr(scn);
-		if (!sh)
-			return 1;
-
-		if (sh->sh_type == SHT_SYMTAB) {
-			data = elf_getdata(scn, 0);
-			if (!data)
-				return 1;
-
-			symbols = data;
-			strtabidx = sh->sh_link;
-		}
-		name = elf_strptr(elf, shstrndx, sh->sh_name);
-		if (!name) {
-			std::cerr << "name failed" << std::endl;
-			return 1;
-		}
-
-		// std::cerr << "index " << idx << ": " << name << std::endl;
-
-		if (!strcmp(".maps", name)) {
-			maps_shndx = idx;
-		}
-	}
-
-	scn = elf_getscn(elf, maps_shndx);
-	data = elf_getdata(scn, 0);
-
-	if (!scn || !data) {
-		std::cerr << "elf: failed to get map definitions" << std::endl;
-		return 1;
-	}
-
-	/*
-	 * Count number of maps. Each map has a name.
-	 * Array of maps is not supported: only the first element is
-	 * considered.
-	 *
-	 * TODO: Detect array of map and report error.
-	 */
-	nr_syms = symbols->d_size / sizeof(Elf64_Sym);
-	for (i = 0; i < nr_syms; i++) {
-		Elf64_Sym *sym = (Elf64_Sym *)symbols->d_buf + i;
-
-		if (sym->st_shndx != maps_shndx)
-			continue;
-		if (ELF64_ST_TYPE(sym->st_info) != STT_OBJECT)
-			continue;
-		nr_maps++;
-		name = elf_strptr(elf, strtabidx, sym->st_name);
-		std::cerr << "symbol: " << name << ", st_value=0x" << std::hex
-				  << sym->st_value << ", st_size=" << std::dec << sym->st_size
-				  << std::endl;
-
-		if (sym->st_size == sizeof(struct map_def))
-			map_defs.insert(std::make_pair(sym->st_value, iu_map(name, sym->st_value)));
-		else if (sym->st_size == sizeof(struct map_def *))
-			map_ptrs.insert(std::make_pair(sym, name));
-		else
-			std::cerr << "invalid size in .map section" << std::endl;
-	}
-
-	std::cerr << "# of symbols in \".maps\": " << nr_maps << std::endl;
-
-	std::cerr << ".maps section file offset=0x" << std::hex
-			  << elf64_getshdr(scn)->sh_offset << std::dec << std::endl;
-
-	for (auto &def: map_defs) {
-		collect_map_def(*data, def.second, elf64_getshdr(scn)->sh_addr);
-	}
-
-	std::cerr << "file_size=0x" << std::hex << get_file_size(fd) << std::dec
-			  << std::endl;
-
-	file_size = get_file_size(fd);
-	file_map = (unsigned char *)mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-
-	for (const auto &ptr: map_ptrs) {
-		size_t pos = ptr.first->st_value - elf64_getshdr(scn)->sh_addr + elf64_getshdr(scn)->sh_offset;
-		const auto it = map_defs.find(val_from_buf<uint64_t>(&file_map[pos]));
-		int64_t map_fd;
-
-		if (it == map_defs.end()) {
-			std::cerr << "map def not found" << std::endl;
-			continue;
-		}
-
-		std::cerr << "map_ptr=0x" << std::hex << val_from_buf<uint64_t>(&file_map[pos]) << std::dec << std::endl;
-		std::cerr << "pointed map name: " << it->second.name << std::endl;
-		std::cerr << "map_ptr_offset=0x" << std::hex << ptr.first->st_value << std::dec << std::endl;
-
-		map_fd = map_create(it->second);
-		if (map_fd < 0) {
-			perror("bpf_map_create");
-			exit(1);
-		}
-		std::cerr << "map_fd=" << map_fd << std::endl;
-
-		val_to_buf<uint64_t>(&file_map[pos], map_fd);
-	}
-
-	std::ofstream output("rust.out", std::ios::out | std::ios::binary);
-	output.write((char *)file_map, file_size);
-	output.close();
-
-	// PROG LOAD & ATTACHMENT
-	prog_fd = prog_load("rust.out", map_ptrs);
-	if (prog_fd < 0) {
-		perror("bpf_prog_load");
-		exit(1);
-	}
-
-	trace_id_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/events/syscalls/sys_enter_dup/id", O_RDONLY);
-	if (trace_id_fd < 0) {
-		perror("openat(/sys/kernel/debug/tracing/events/syscalls/sys_enter_dup/id)");
-		exit(1);
-	}
-	read(trace_id_fd, config_str, 256);
-	close(trace_id_fd);
-
-	memset(&p_attr, 0, sizeof(p_attr));
-	p_attr.type = PERF_TYPE_TRACEPOINT;
-	p_attr.size = PERF_ATTR_SIZE_VER5;
-	p_attr.config = atoi(config_str);
-	perf_event_fd = perf_event_open(&p_attr, -1, 0, -1, PERF_FLAG_FD_CLOEXEC);
-	if (perf_event_fd < 0) {
-		perror("perf_event_open");
-		exit(1);
-	}
-
-	ioctl(perf_event_fd, PERF_EVENT_IOC_SET_BPF, prog_fd);
-	ioctl(perf_event_fd, PERF_EVENT_IOC_ENABLE, 0);
-
-	trace_pipe_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/trace_pipe", O_RDONLY);
-
-	for (;;) {
-        char c;
-        if (read(trace_pipe_fd, &c, 1) == 1)
-            putchar(c);
-    }
-
-	return 0;
-}

commit 9b98805d82fb54b0585abd5123e73602a02ea757

libiu: fix hardcoded base prog name
A long overdue.

Fixes: 15d0e80810fa ("implement libiu (lib-inner-unikernel)")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

libiu/libiu.cpp
@@ -13,6 +13,7 @@
 #include <sys/utsname.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
@@ -62,6 +63,11 @@ struct bpf_sec_def {
   bool is_sleepable;
   attach_fn_t attach_fn;
 };
+
+// Conflicts with std::min
+#ifdef min
+#undef min
+#endif
 
 #define BPF_PROG_SEC_IMPL(string, ptype, eatype, eatype_optional, attachable,  \
                           attach_btf)                                          \
@@ -525,6 +531,7 @@ class iu_obj {
   size_t file_size;
   unsigned char *file_map;
   int prog_fd;
+  std::string basename;
 
   int parse_scns(struct bpf_object *);
   int parse_maps(struct bpf_object *);
@@ -715,6 +722,10 @@ iu_obj::iu_obj(const char *c_path, struct bpf_object *bpf_obj)
   // reference: `man 2 mmap`
   file_map = reinterpret_cast<unsigned char *>(
       mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0));
+
+  std::string copy(c_path);
+  copy += "-base";
+  basename = ::basename(copy.data());
   // close(fd);
 }
 
@@ -1130,7 +1141,9 @@ int iu_obj::load(struct bpf_object *obj) {
     arr[idx++] = def.first + offsetof(map_def, kptr);
 
   attr.prog_type = BPF_PROG_TYPE_IU_BASE;
-  memcpy(attr.prog_name, "map_test", sizeof("map_test"));
+  // progname was zero-initialized so we don't copy the null terminator
+  memcpy(attr.prog_name, basename.c_str(),
+         std::min(basename.size(), sizeof(attr.prog_name) - 1));
   attr.rustfd = fd;
   attr.license = reinterpret_cast<__u64>("GPL");
 