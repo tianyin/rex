fa6cab5baf81fa47833058a880f3bb9405e5e4c3
Jinghao Jia <jinghao7@illinois.edu>
2024-04-06 21:40:09 -0500

    samples/startup_overhead_benchmark: update event-trigger
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- samples/startup_overhead_benchmark/event-trigger.c
+++ samples/startup_overhead_benchmark/event-trigger.c
@@ -6,15 +6,21 @@
 
 int main(int argc, char *argv[])
 {
-	int nr_rounds = argc == 2 ? atoi(argv[1]) : 1000;
-	int fd = open("/proc/kprobe_target", O_RDONLY);
+	int nr_rounds, arg, fd;
+
+	if (argc != 3)
+		asm volatile ("ud2");
+
+	nr_rounds = atoi(argv[1]);
+	arg = atoi(argv[2]);
+	fd = open("/proc/kprobe_target", O_RDONLY);
 	if (fd < 0) {
 		perror("open");
 		return 1;
 	}
 
 	for (int i = 0; i < nr_rounds; i++)
-		ioctl(fd, 1313, 0);
+		ioctl(fd, 1313, arg);
 
 	close(fd);
 }
