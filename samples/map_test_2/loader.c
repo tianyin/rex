89a78fb4f14547bd75f4127f93f68ae206da1887
MinhPhan8803 <minhnp2@illinois.edu>
2024-07-26 03:30:11 -0500

    samples/map_test_2: update executable names

--- samples/map_test_2/loader.c
+++ samples/map_test_2/loader.c
@@ -11,7 +11,7 @@
 #include <librex.h>
 #include <libbpf.h>
 
-#define EXE "./target/x86_64-unknown-linux-gnu/release/map_test"
+#define EXE "./target/x86_64-unknown-linux-gnu/release/map_test_2"
 
 int main(void)
 {
