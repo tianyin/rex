e82aa0888c27b9fbf5a5d16aa17ceba816a17f10
Ruowen Qin <ruowenq2@illinois.edu>
2024-09-01 11:28:13 +0800

    samples/termination_check: add new termination example
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

---
+++ samples/termination_test/loader.c
@@ -0,0 +1,56 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <linux/perf_event.h>
+#include <linux/unistd.h>
+
+#include <librex.h>
+#include <libbpf.h>
+
+#define EXE "./target/x86_64-unknown-linux-gnu/release/termination_test"
+
+int main(void)
+{
+	int trace_pipe_fd;
+	struct bpf_object *obj;
+	struct bpf_program *prog;
+	struct bpf_link *link = NULL;
+
+	rex_set_debug(1); // enable debug info
+
+	obj = rex_obj_get_bpf(rex_obj_load(EXE));
+	if (!obj) {
+		fprintf(stderr, "Object could not be opened\n");
+		return 1;
+	}
+
+	prog = bpf_object__find_program_by_name(obj, "rex_prog1");
+	if (!prog) {
+		fprintf(stderr, "_start not found\n");
+		return 1;
+	}
+
+	link = bpf_program__attach(prog);
+	if (libbpf_get_error(link)) {
+		fprintf(stderr, "ERROR: bpf_program__attach failed\n");
+		link = NULL;
+		return 1;
+	}
+
+	trace_pipe_fd = openat(AT_FDCWD, "/sys/kernel/debug/tracing/trace_pipe",
+			       O_RDONLY);
+
+	for (;;) {
+		char c;
+		fflush(stdout);
+		if (read(trace_pipe_fd, &c, 1) == 1)
+			;
+	}
+
+	bpf_link__destroy(link);
+	return 0;
+}
