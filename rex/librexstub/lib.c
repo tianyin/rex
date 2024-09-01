tianyin/rex

commit 48e2bbcb49139f5438a873eed74f935ad5dd647d

rex: add termination_check in base_helper
Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

rex/librexstub/lib.c
@@ -40,3 +40,4 @@ unsigned long *__per_cpu_offset;
 void *rex_cleanup_entries;
 unsigned long rex_stack_ptr;
 void *pcpu_hot;
+unsigned long bpf_termination_flag;

rex/src/base_helper.rs
@@ -3,6 +3,7 @@ use crate::debug::printk;
 use crate::linux::bpf::bpf_map_type;
 use crate::linux::errno::EINVAL;
 use crate::map::*;
+use crate::panic::__rex_handle_timeout;
 use crate::per_cpu::{cpu_number, this_cpu_read};
 use crate::random32::bpf_user_rnd_u32;
 use crate::stub;
@@ -12,6 +13,39 @@ use core::mem::{self, MaybeUninit};
 // use crate::timekeeping::*;
 
 use core::intrinsics::unlikely;
+
+macro_rules! termination_check {
+    ($func:expr) => {{
+        // Declare and initialize the termination flag pointer
+        // TODO: change this u64 to u8
+        let mut termination_flag: *mut u64;
+        unsafe {
+            termination_flag = crate::per_cpu::this_cpu_ptr_mut(
+                core::ptr::addr_of_mut!(crate::stub::bpf_termination_flag)
+                    as *mut u64 as *mut () as u64,
+            );
+
+            // Set the termination flag
+            *termination_flag = 1;
+        }
+
+        // Call the provided function
+        let res = $func;
+
+        // Check the termination flag and handle timeout
+        unsafe {
+            if *termination_flag == 2 {
+                crate::panic::__rex_handle_timeout();
+            } else {
+                // Reset the termination flag upon exiting
+                *termination_flag = 0;
+            }
+        }
+
+        // Return the result of the function call
+        res
+    }};
+}
 
 pub(crate) fn bpf_get_smp_processor_id() -> u32 {
     unsafe { this_cpu_read(cpu_number() as *const i32 as u64) }
@@ -23,7 +57,7 @@ pub(crate) fn bpf_trace_printk(
     arg2: u64,
     arg3: u64,
 ) -> Result {
-    unsafe {
+    termination_check!(unsafe {
         to_result!(stub::bpf_trace_printk(
             fmt.as_ptr(),
             (fmt.count_bytes() + 1) as u32,
@@ -31,7 +65,7 @@ pub(crate) fn bpf_trace_printk(
             arg2,
             arg3
         ))
-    }
+    })
 }
 
 pub(crate) fn bpf_map_lookup_elem<'a, const MT: bpf_map_type, K, V>(
@@ -43,10 +77,10 @@ pub(crate) fn bpf_map_lookup_elem<'a, const MT: bpf_map_type, K, V>(
         return None;
     }
 
-    let value = unsafe {
+    let value = termination_check!(unsafe {
         stub::bpf_map_lookup_elem(map_kptr, key as *const K as *const ())
             as *mut V
-    };
+    });
 
     if value.is_null() {
         None
@@ -66,14 +100,14 @@ pub(crate) fn bpf_map_update_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    unsafe {
+    termination_check!(unsafe {
         to_result!(stub::bpf_map_update_elem(
             map_kptr,
             key as *const K as *const (),
             value as *const V as *const (),
             flags
         ) as i32)
-    }
+    })
 }
 
 pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
@@ -85,12 +119,12 @@ pub(crate) fn bpf_map_delete_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    unsafe {
+    termination_check!(unsafe {
         to_result!(stub::bpf_map_delete_elem(
             map_kptr,
             key as *const K as *const ()
         ) as i32)
-    }
+    })
 }
 
 pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
@@ -103,13 +137,13 @@ pub(crate) fn bpf_map_push_elem<const MT: bpf_map_type, K, V>(
         return Err(EINVAL as i32);
     }
 
-    unsafe {
+    termination_check!(unsafe {
         to_result!(stub::bpf_map_push_elem(
             map_kptr,
             value as *const V as *const (),
             flags
         ) as i32)
-    }
+    })
 }
 
 pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
@@ -122,12 +156,12 @@ pub(crate) fn bpf_map_pop_elem<const MT: bpf_map_type, K, V>(
 
     let mut value: MaybeUninit<V> = MaybeUninit::uninit();
 
-    let res = unsafe {
+    let res = termination_check!(unsafe {
         to_result!(stub::bpf_map_pop_elem(
             map_kptr,
             value.as_mut_ptr() as *mut ()
         ) as i32)
-    };
+    });
     res.map(|_| unsafe { value.assume_init() }).ok()
 }
 
@@ -141,12 +175,12 @@ pub(crate) fn bpf_map_peek_elem<const MT: bpf_map_type, K, V>(
 
     let mut value: MaybeUninit<V> = MaybeUninit::uninit();
 
-    let res = unsafe {
+    let res = termination_check!(unsafe {
         to_result!(stub::bpf_map_peek_elem(
             map_kptr,
             value.as_mut_ptr() as *mut ()
         ) as i32)
-    };
+    });
 
     res.map(|_| unsafe { value.assume_init() }).ok()
 }
@@ -174,13 +208,13 @@ pub(crate) fn bpf_probe_read_kernel<T>(
     dst: &mut T,
     unsafe_ptr: *const (),
 ) -> Result {
-    unsafe {
+    termination_check!(unsafe {
         to_result!(stub::bpf_probe_read_kernel(
             dst as *mut T as *mut (),
             core::mem::size_of::<T>() as u32,
             unsafe_ptr,
         ))
-    }
+    })
 }
 
 pub(crate) fn bpf_strcmp(s1: &str, s2: &str) -> i32 {
@@ -267,15 +301,15 @@ pub(crate) fn bpf_ktime_get_boot_ns_origin() -> u64 {
 */
 
 pub(crate) fn bpf_ktime_get_ns() -> u64 {
-    unsafe { stub::bpf_ktime_get_ns() }
+    termination_check!(unsafe { stub::bpf_ktime_get_ns() })
 }
 
 pub(crate) fn bpf_ktime_get_boot_ns() -> u64 {
-    unsafe { stub::bpf_ktime_get_boot_ns() }
+    termination_check!(unsafe { stub::bpf_ktime_get_boot_ns() })
 }
 
 pub(crate) fn bpf_ktime_get_coarse_ns() -> u64 {
-    unsafe { stub::bpf_ktime_get_coarse_ns() }
+    termination_check!(unsafe { stub::bpf_ktime_get_coarse_ns() })
 }
 
 /*
@@ -293,7 +327,7 @@ pub(crate) fn bpf_ktime_get_coarse_ns() -> u64 {
 */
 
 pub(crate) fn bpf_get_prandom_u32() -> u32 {
-    bpf_user_rnd_u32()
+    termination_check!(bpf_user_rnd_u32())
 }
 
 // In document it says that data is a pointer to an array of 64-bit values.
@@ -302,7 +336,7 @@ pub(crate) fn bpf_snprintf<const N: usize, const M: usize>(
     fmt: &str,
     data: &[u64; M],
 ) -> Result {
-    unsafe {
+    termination_check!(unsafe {
         to_result!(stub::bpf_snprintf(
             str.as_mut_ptr(),
             N as u32,
@@ -310,7 +344,7 @@ pub(crate) fn bpf_snprintf<const N: usize, const M: usize>(
             data.as_ptr(),
             M as u32,
         ) as i32)
-    }
+    })
 }
 
 pub(crate) fn bpf_ringbuf_reserve<T>(
@@ -322,19 +356,23 @@ pub(crate) fn bpf_ringbuf_reserve<T>(
         return core::ptr::null_mut();
     }
 
-    let data = unsafe {
+    let data = termination_check!(unsafe {
         stub::bpf_ringbuf_reserve(map_kptr, mem::size_of::<T>() as u64, 0)
-    };
+    });
 
     data as *mut T
 }
 
 pub(crate) fn bpf_ringbuf_submit<T>(data: &mut T, flags: u64) {
-    unsafe { stub::bpf_ringbuf_submit(data as *mut T as *mut (), flags) }
+    termination_check!(unsafe {
+        stub::bpf_ringbuf_submit(data as *mut T as *mut (), flags)
+    })
 }
 
 pub(crate) fn bpf_ringbuf_discard<T>(data: &mut T, flags: u64) {
-    unsafe { stub::bpf_ringbuf_discard(data as *mut T as *mut (), flags) }
+    termination_check!(unsafe {
+        stub::bpf_ringbuf_discard(data as *mut T as *mut (), flags)
+    })
 }
 
 pub(crate) fn bpf_ringbuf_query(
@@ -345,7 +383,9 @@ pub(crate) fn bpf_ringbuf_query(
     if unlikely(map_kptr.is_null()) {
         return None;
     }
-    Some(unsafe { stub::bpf_ringbuf_query(map_kptr, flags) })
+    Some(termination_check!(unsafe {
+        stub::bpf_ringbuf_query(map_kptr, flags)
+    }))
 }
 
 macro_rules! base_helper_defs {
@@ -491,6 +531,53 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn bpf_ringbuf_submit<T>(&self, data: &mut T, flags: u64) {
             crate::base_helper::bpf_ringbuf_submit(data, flags)
+        }
+
+        // NOTE: test only
+        #[inline(always)]
+        pub fn dummy_long_running_helper(&self) -> u32 {
+            unsafe {
+                let termination_flag: *mut u64 =
+                    crate::per_cpu::this_cpu_ptr_mut(core::ptr::addr_of_mut!(
+                        crate::stub::bpf_termination_flag
+                    ) as *mut u64
+                        as *mut ()
+                        as u64);
+
+                *termination_flag = 1;
+                // simulate long runtime
+                let mut target = 1000;
+                loop {
+                    target -= 1;
+                    if target == *termination_flag {
+                        break;
+                    }
+                    crate::base_helper::bpf_trace_printk(
+                        c"Inside loop of helper",
+                        0,
+                        0,
+                        0,
+                    );
+                }
+                crate::base_helper::bpf_trace_printk(
+                    c"Exit loop of helper. ",
+                    0,
+                    0,
+                    0,
+                );
+                if *termination_flag == 2 {
+                    crate::base_helper::bpf_trace_printk(
+                        c"Helper finds termination flag set. Calling panic! ",
+                        0,
+                        0,
+                        0,
+                    );
+                    crate::panic::__rex_handle_timeout();
+                } else {
+                    *termination_flag = 0; // exiting
+                }
+                0
+            }
         }
     };
 }

rex/src/stub.rs
@@ -217,4 +217,10 @@ extern "C" {
     /// Per-cpu point of the current task
     #[allow(improper_ctypes)]
     pub(crate) static pcpu_hot: pcpu_hot;
+
+    ///  `DEFINE_PER_CPU(int, bpf_termination_flag);`
+    ///  
+    ///  Used to indidicate whether a BPF program in a CPU is executing
+    ///  inside a helper, or inside a panic handler, or just in BPF text.
+    pub(crate) static mut bpf_termination_flag: u64;
 }

rex/src/panic.rs
@@ -149,7 +149,7 @@ unsafe fn __rex_check_stack() {
 }
 
 #[no_mangle]
-unsafe fn __rex_handle_timeout() -> ! {
+pub(crate) unsafe fn __rex_handle_timeout() -> ! {
     panic!("Timeout in Rex program");
 }
 

commit 096810317651dc0bee0643d5124e909c211d5f71

rex: change u64 termination_flag to u8
Fixes: 48e2bbcb4913 ("rex: add termination_check in base_helper")
Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

rex/librexstub/lib.c
@@ -40,4 +40,4 @@ unsigned long *__per_cpu_offset;
 void *rex_cleanup_entries;
 unsigned long rex_stack_ptr;
 void *pcpu_hot;
-unsigned long bpf_termination_flag;
+unsigned char bpf_termination_flag;

rex/src/base_helper.rs
@@ -17,12 +17,11 @@ use core::intrinsics::unlikely;
 macro_rules! termination_check {
     ($func:expr) => {{
         // Declare and initialize the termination flag pointer
-        // TODO: change this u64 to u8
-        let mut termination_flag: *mut u64;
+        let mut termination_flag: *mut u8;
         unsafe {
             termination_flag = crate::per_cpu::this_cpu_ptr_mut(
                 core::ptr::addr_of_mut!(crate::stub::bpf_termination_flag)
-                    as *mut u64 as *mut () as u64,
+                    as u64,
             );
 
             // Set the termination flag
@@ -537,21 +536,14 @@ macro_rules! base_helper_defs {
         #[inline(always)]
         pub fn dummy_long_running_helper(&self) -> u32 {
             unsafe {
-                let termination_flag: *mut u64 =
+                let termination_flag: *mut u8 =
                     crate::per_cpu::this_cpu_ptr_mut(core::ptr::addr_of_mut!(
                         crate::stub::bpf_termination_flag
-                    ) as *mut u64
-                        as *mut ()
-                        as u64);
+                    ) as u64);
 
                 *termination_flag = 1;
                 // simulate long runtime
-                let mut target = 1000;
-                loop {
-                    target -= 1;
-                    if target == *termination_flag {
-                        break;
-                    }
+                for _ in 0..10000 {
                     crate::base_helper::bpf_trace_printk(
                         c"Inside loop of helper",
                         0,

rex/src/stub.rs
@@ -222,5 +222,5 @@ extern "C" {
     ///  
     ///  Used to indidicate whether a BPF program in a CPU is executing
     ///  inside a helper, or inside a panic handler, or just in BPF text.
-    pub(crate) static mut bpf_termination_flag: u64;
+    pub(crate) static mut bpf_termination_flag: u8;
 }

samples/termination_test/tests/runtest.py
@@ -1,97 +1,0 @@
-#!/bin/python
-
-import re
-import subprocess
-from time import sleep
-
-process = 0
-
-
-def count_bpf_programs():
-    try:
-        # Run bpftool to list all loaded BPF programs
-        result = subprocess.run(
-            "bpftool prog show",
-            capture_output=True,
-            shell=True,
-            text=True,
-        )
-
-        # Process the output to count programs
-        if result.stdout:
-            # Each program details start on a new line
-            output = result.stdout.strip().split("\n")
-            programs = [line for line in output if "name" in line]
-            return len(programs)
-        else:
-            return 0
-    except FileNotFoundError:
-        print("bpftool is not installed or not found in the PATH.")
-        return 0
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return 0
-
-
-def run_loader():
-    global process
-    process = subprocess.Popen(
-        ["./loader"],
-        text=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-
-def trigger_prog():
-    try:
-        subprocess.run("./event-trigger", shell=True)
-    except subprocess.CalledProcessError:
-        print("CalledProcessError")
-
-
-def capture_output() -> bool:
-    try:
-        global process
-        trigger_prog()
-        trigger_prog()
-
-        sleep(2)
-        process.kill()
-        std_out, std_err = process.communicate(timeout=7)
-        re_match = re.findall(
-            r"bpf_trace_printk: Rust triggered from PID \d+ on CPU .+", std_out, re.M
-        )
-        if len(re_match) == 2:
-            print("Success")
-            return True
-        else:
-            print("Failed")
-            return False
-
-    except subprocess.CalledProcessError:
-        return False
-    except subprocess.TimeoutExpired:
-        process.kill()
-        return False
-
-
-def main():
-
-    old_prog_num = count_bpf_programs()
-    run_loader()
-    count = 0
-    while old_prog_num == count_bpf_programs():
-        sleep(1)
-        count += 1
-        if count == 5:
-            break
-    grade_file = open("auto_grade.txt", "w")
-    if capture_output():
-        grade_file.write("success")
-    else:
-        grade_file.write("fail")
-
-
-if __name__ == "__main__":
-    main()