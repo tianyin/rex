4c2bed48c9078768290c12603d425e6c1005ecd4
Ruowen Qin <ruowenq2@illinois.edu>
2024-09-05 22:04:30 +0800

    samples/termination_check: use endless loop for testing
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- samples/termination_test/src/main.rs
+++ samples/termination_test/src/main.rs
@@ -10,16 +10,19 @@ use rex::Result;
 
 #[rex_tracepoint(name = "syscalls/sys_enter_dup", tp_type = "Void")]
 fn rex_prog1(obj: &tracepoint, _: tp_ctx) -> Result {
-    bpf_printk!(
-        obj,
-        c"BPF program to test program termination.\n
-			  Calls dummy_long_running_helper which simulates a long running helper"
-    );
-    //let mut target = obj.bpf_ktime_get_ns();
-    for i in 0..10000000 {
-        bpf_printk!(obj, c"Rust triggered %u.\n", i as u64);
-        obj.dummy_long_running_helper();
+    // bpf_printk!(
+    //     obj,
+    //     c"BPF program to test program termination.\n
+    //  Calls dummy_long_running_helper which simulates a long running helper"
+    // );
+    // //let mut target = obj.bpf_ktime_get_ns();
+    // for i in 0..10000000 {
+    //     bpf_printk!(obj, c"Rust triggered %u.\n", i as u64);
+    //     obj.dummy_long_running_helper();
+    // }
+    // bpf_printk!(obj, c"Done\n");
+    // panic!("Termination panic")
+    loop {
+        
     }
-    bpf_printk!(obj, c"Done\n");
-    panic!("Termination panic")
 }
