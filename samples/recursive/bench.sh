tianyin/rex

commit 7b495855f11d38ee81a14ea8f42836a33491aba2

sample/recursive: polish the old recursive benchmark method, use function param as input
Update event-trigger for atomic and recursive

Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

samples/recursive/bench.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+
+for i in {0..33}; do
+	./event-trigger 5000 $i
+	# cat /sys/kernel/debug/tracing/trace > "./output/rust_${i}"
+	cat /sys/kernel/debug/tracing/trace > "./output/bpf_${i}"
+	echo > /sys/kernel/debug/tracing/trace
+	echo $i
+	sleep 1
+done

samples/recursive/Makefile
@@ -26,7 +26,7 @@ ifeq ($(V),2)
 CARGO_FLAGS += -vv
 endif
 
-all: target/x86_64-unknown-linux-gnu/release/recursive loader 
+all: target/x86_64-unknown-linux-gnu/release/recursive loader event-trigger
 
 target/x86_64-unknown-linux-gnu/debug/recursive: Cargo.toml ./src/*.rs
 	cargo rustc ${CARGO_FLAGS} -- ${RUST_FLAGS}
@@ -46,6 +46,9 @@ loader: loader.c
 clean:
 	rm -rf loader ./src/linux ./src/stub.rs
 	cargo clean
+
+event-trigger: event-trigger.c
+	${CC} -I${LINUX_INC} ${CFLAGS} ${LDFLAGS} -o $@ $<
 
 vmcopy: target/x86_64-unknown-linux-gnu/release/recursive loader
 	cp target/x86_64-unknown-linux-gnu/release/recursive loader ../../rootfs/guest/

samples/recursive/loader.c
@@ -36,14 +36,14 @@ int main(void)
  	}
 
 	// Populate map with starting conditions
-	int data_map_fd = bpf_object__find_map_fd_by_name(obj, "data_map");
-	int pid_key = 0;
-	int n_key = 1;
-	int pid = getpid();
-	printf("PID: %d\n", pid);
-	int n_init = 10;
-	bpf_map_update_elem(data_map_fd, &pid_key, &pid, BPF_ANY);
-	bpf_map_update_elem(data_map_fd, &n_key, &n_init, BPF_ANY);
+	// int data_map_fd = bpf_object__find_map_fd_by_name(obj, "data_map");
+	// int pid_key = 0;
+	// int n_key = 1;
+	// int pid = getpid();
+	// printf("PID: %d\n", pid);
+	// int n_init = 10;
+	// bpf_map_update_elem(data_map_fd, &pid_key, &pid, BPF_ANY);
+	// bpf_map_update_elem(data_map_fd, &n_key, &n_init, BPF_ANY);
 
 	link = bpf_program__attach(prog);
 	if (libbpf_get_error(link)) {

samples/recursive/src/main.rs
@@ -16,53 +16,46 @@ use inner_unikernel_rt::MAP_DEF;
 MAP_DEF!(data_map, u32, u32, BPF_MAP_TYPE_ARRAY, 2, 0);
 
 #[inline(always)]
-fn iu_recursive(obj: &kprobe, _:&mut pt_regs) -> Result {
-    let curr_pid: i32 = if let Some(task_struct) = obj.bpf_get_current_task() {
-        task_struct.get_pid()
-    } else {
-    bpf_printk!(obj, "error 1");
-        return Err(0);
-    };
-
-    let stored_pid: u32 = if let Some(val) = obj.bpf_map_lookup_elem(&data_map, &0) {
-        *val
-    } else {
-    bpf_printk!(obj, "error 2");
-        return Err(0);
-    };
-
-    // if (stored_pid as i32) != curr_pid {
-    // bpf_printk!(obj, "error 3");
+fn iu_recursive(obj: &kprobe, ctx: &mut pt_regs) -> Result {
+    // let curr_pid: i32 = if let Some(task_struct) = obj.bpf_get_current_task() {
+    //     task_struct.get_pid()
+    // } else {
     //     return Err(0);
-    // }
+    // };
 
+    // let stored_pid: u32 = if let Some(val) = obj.bpf_map_lookup_elem(&data_map, &0) {
+    //     *val
+    // } else {
+    //     return Err(0);
+    // };
+
+    let n = ctx.rdi() as u32;
+    // let n: u32 = if let Some(val) = obj.bpf_map_lookup_elem(&data_map, &1) {
+    //     *val
+    // } else {
+    //     return Err(0);
+    // };
+
+
+    // bpf_printk!(obj, "Received n: %d", n as u64);
     let start_time: u64 = obj.bpf_ktime_get_ns();
-    bpf_printk!(obj, "Start time: %ul", start_time);
-    let n: u32 = if let Some(val) = obj.bpf_map_lookup_elem(&data_map, &1) {
-        *val
-    } else {
-    bpf_printk!(obj, "error 4");
-        return Err(0);
-    };
-
-    bpf_printk!(obj, "Received n: %d", n as u64);
-    let result = calculate_tail_factorial(n, 1);
-    bpf_printk!(obj, "Result: %d", result as u64);
-
+    calculate_tail_fib(n, 1);
     let end_time: u64 = obj.bpf_ktime_get_ns();
-    bpf_printk!(obj, "End time: %ul", end_time);
+    // bpf_printk!(obj, "Result: %d", result as u64);
+
+    bpf_printk!(obj, "Time: %llu", end_time - start_time);
 
     Ok(0)
 }
 
 use core::hint::black_box;
 #[inline(never)]
-fn calculate_tail_factorial(n: u32, accum: u32) -> u32 {
-    if n == 1 {
+fn calculate_tail_fib(n: u32, accum: u32) -> u32 {
+    if n == 0 {
         return accum;
     }
 
-    return black_box(calculate_tail_factorial(n - 1, accum * n));
+    return black_box(calculate_tail_fib(n - 1, accum + n));
 }
 
 #[entry_link(inner_unikernel/kprobe/kprobe_target_func)]

commit 256967590bbeedd1eec98d75a55b7030a34be04b

samples/recursive: fix off-by-one in bench.sh
Fixes: 7b495855f11d ("sample/recursive: polish the old recursive benchmark method, use function param as input")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

samples/recursive/bench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..33}; do
+for i in {0..32}; do
 	./event-trigger 5000 $i
 	cat /sys/kernel/debug/tracing/trace > "./output/rust_${i}"
 	# cat /sys/kernel/debug/tracing/trace > "./output/bpf_${i}"