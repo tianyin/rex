aa916a9a4dc480ddae1ba616b7f61809ef09ede6
Jinghao Jia <jinghao7@illinois.edu>
2024-09-25 22:34:53 -0500

    samples/map_bench: update sample name
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- samples/map_bench/loader.c
+++ samples/map_bench/loader.c
@@ -11,7 +11,7 @@
 #include <librex.h>
 #include <libbpf.h>
 
-#define EXE "./target/x86_64-unknown-linux-gnu/release/ktime"
+#define EXE "./target/x86_64-unknown-linux-gnu/release/map_bench"
 
 int main(void)
 {
