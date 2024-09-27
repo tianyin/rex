06c7638d1f3750551b1c5a7c4d68df3dd8671665
Ruowen Qin <ruowenq2@illinois.edu>
2024-09-27 19:55:07 +0800

    samples/recursive: update benchmark script for recursive
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- samples/recursive/bench.sh
+++ samples/recursive/bench.sh
@@ -1,10 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-for i in {0..32}; do
-	./event-trigger 5000 $i
-	cat /sys/kernel/debug/tracing/trace > "./output/rust_${i}"
-	# cat /sys/kernel/debug/tracing/trace > "./output/bpf_${i}"
-	echo > /sys/kernel/debug/tracing/trace
-	echo $i
-	sleep 0.5
-done
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <bpf|rex>"
+	exit 1
+fi
+
+mkdir output
+
+if [[ "$1" == "rex" ]]; then
+	for i in {0..32}; do
+		./event-trigger 5000 $i
+		cat /sys/kernel/debug/tracing/trace >"./output/rust_${i}"
+		echo >/sys/kernel/debug/tracing/trace
+		echo $i
+		sleep 0.5
+	done
+elif [[ "$1" == "bpf" ]]; then
+	for i in {0..32}; do
+		./event-trigger 5000 $i
+		cat /sys/kernel/debug/tracing/trace >"./output/bpf_${i}"
+		echo >/sys/kernel/debug/tracing/trace
+		echo $i
+		sleep 0.5
+	done
+fi
