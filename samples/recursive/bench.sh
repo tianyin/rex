6bfba3e06bf3ba5bf5dae57534ffd1d170134da4
Ruowen Qin <ruowenq2@illinois.edu>
2024-09-28 20:03:02 +0800

    samples/recursive: increase trace buffer size
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- samples/recursive/bench.sh
+++ samples/recursive/bench.sh
@@ -5,22 +5,26 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 
+echo 8192 > /sys/kernel/debug/tracing/buffer_size_kb
 mkdir output
 
 if [[ "$1" == "rex" ]]; then
 	for i in {0..32}; do
 		./event-trigger 5000 $i
-		cat /sys/kernel/debug/tracing/trace >"./output/rust_${i}"
+		cat /sys/kernel/debug/tracing/trace | head -5000 >"./output/rust_${i}"
 		echo >/sys/kernel/debug/tracing/trace
 		echo $i
 		sleep 0.5
 	done
 elif [[ "$1" == "bpf" ]]; then
 	for i in {0..32}; do
-		./event-trigger 5000 $i
-		cat /sys/kernel/debug/tracing/trace >"./output/bpf_${i}"
+		./event-trigger 6000 $i
+		cat /sys/kernel/debug/tracing/trace | head -5000  >"./output/bpf_${i}"
 		echo >/sys/kernel/debug/tracing/trace
 		echo $i
 		sleep 0.5
 	done
 fi
+
+# unlink
+rm -rf /sys/fs/bpf/*
