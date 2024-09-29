tianyin/rex

commit 9148d36974a35ca96b40c4613ae25e1f9a353053

samples/map_bench: add bench script
Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

samples/map_bench/bench.sh
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: $0 <rex|bmc> <atomic|hash|array>"
+	exit 1
+fi
+
+mkdir output
+echo 8192 > /sys/kernel/debug/tracing/buffer_size_kb
+
+if [[ "$1" == "rex" ]]; then
+	./event-trigger 6000 1
+	cat /sys/kernel/debug/tracing/trace | tail -5000 >"./output/rex_${2}.txt"
+elif [[ "$1" == "bpf" ]]; then
+	./event-trigger 6000 1
+	cat /sys/kernel/debug/tracing/trace | tail -5000 >"./output/bpf_${2}.txt"
+fi
+
+
+# clean buffer
+echo >/sys/kernel/debug/tracing/trace
+
+# unlink
+rm -rf /sys/fs/bpf/*

samples/recursive/bench.sh
@@ -11,7 +11,7 @@ mkdir output
 if [[ "$1" == "rex" ]]; then
 	for i in {0..32}; do
 		./event-trigger 5000 $i
-		cat /sys/kernel/debug/tracing/trace | head -5000 >"./output/rust_${i}"
+		cat /sys/kernel/debug/tracing/trace | tail -5000 >"./output/rust_${i}"
 		echo >/sys/kernel/debug/tracing/trace
 		echo $i
 		sleep 0.5
@@ -19,7 +19,7 @@ if [[ "$1" == "rex" ]]; then
 elif [[ "$1" == "bpf" ]]; then
 	for i in {0..32}; do
 		./event-trigger 6000 $i
-		cat /sys/kernel/debug/tracing/trace | head -5000  >"./output/bpf_${i}"
+		cat /sys/kernel/debug/tracing/trace | tail -5000  >"./output/bpf_${i}"
 		echo >/sys/kernel/debug/tracing/trace
 		echo $i
 		sleep 0.5

commit 02e77f61a53a50fd215658b392ebef2441cbe274

samples/map_bench: fix usage string in bench.sh
Fixes: 9148d36974a3 ("samples/map_bench: add bench script")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

samples/map_bench/bench.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 <rex|bmc> <atomic|hash|array>"
+	echo "Usage: $0 <rex|bpf> <atomic|hash|array>"
 	exit 1
 fi
 