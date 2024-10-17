tianyin/rex

commit 41646bf8ee0ef38601f2d256ff00b5ecd87af1d3

rex/panic: optimize __rex_check_stack
- Remove hardcoded use of r10 and let compiler to figure out which
  register to use.
- Explicitly mark clobbered regsters to avoid UB.

Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

rex/src/panic.rs
@@ -126,24 +126,24 @@ impl<'a> CleanupEntries<'a> {
     }
 }
 
-// TODO: manually optimize
 // The best way to deal with this is probably insert it directly in LLVM IR as
 // an inline asm block
 // For now, use inline(always) to hint the compiler for inlining if LTO is on
 #[no_mangle]
 #[inline(always)]
 unsafe fn __rex_check_stack() {
-    // subtract 0x4000 because these 4 pages are either used by the kernel or
-    // panic handling
+    // The program can only use the top 4 pages of the stack, therefore subtract
+    // 0x4000
     unsafe {
         core::arch::asm!(
-            "mov r10, gs:[r10]",
-            "sub r10, 0x4000",
+            "mov {1:r}, gs:[{0:r}]",
+            "sub {1:r}, 0x4000",
             "cmp rsp, r10",
             "ja 2f",
             "call __rex_handle_stack_overflow",
             "2:",
-            in("r10") &stub::rex_stack_ptr as *const u64 as u64,
+            in(reg) &stub::rex_stack_ptr as *const u64 as u64,
+            lateout(reg) _,
         );
     }
 }

commit 576f0dd91b62bbda8201342775b1a0e32ed8f932

rex: fix incorrect register usage in __rex_check_stack()
Fixes: 41646bf8ee0e ("rex/panic: optimize __rex_check_stack")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

rex/src/panic.rs
@@ -138,7 +138,7 @@ unsafe fn __rex_check_stack() {
         core::arch::asm!(
             "mov {1:r}, gs:[{0:r}]",
             "sub {1:r}, 0x4000",
-            "cmp rsp, r10",
+            "cmp rsp, {1:r}",
             "ja 2f",
             "call __rex_handle_stack_overflow",
             "2:",