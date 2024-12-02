ec1ed6a645f4f1fb0b744b69e036561dfd411a09
Ruowen Qin <ruowenq2@illinois.edu>
2024-12-02 23:02:23 +0800

    samples/error_injection: more closures
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- samples/error_injection/src/main.rs
+++ samples/error_injection/src/main.rs
@@ -14,8 +14,9 @@ static pid_to_errno: RexHashMap<i32, u64> = RexHashMap::new(1, 0);
 
 #[rex_kprobe]
 pub fn err_injector(obj: &kprobe, ctx: &mut PtRegs) -> Result {
-    let pid = obj.bpf_get_current_task().map(|t| t.get_pid()).ok_or(0)?;
-    obj.bpf_map_lookup_elem(&pid_to_errno, &pid)
-        .map(|e| obj.bpf_override_return(ctx, *e))
+    obj.bpf_get_current_task()
+        .map(|t| t.get_pid())
+        .and_then(|p| obj.bpf_map_lookup_elem(&pid_to_errno, &p).cloned())
+        .map(|e| obj.bpf_override_return(ctx, e))
         .ok_or(0)
 }
