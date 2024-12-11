tianyin/rex

commit a03f2d091aa46ac344643dc35d87506739b0e80e

samples: add new syscall error injection sample
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

samples/error_injection/loader.c
@@ -1,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <librex.h>
+#include <libbpf.h>
+
+#define EXE "./target/x86_64-unknown-linux-gnu/release/error_injection"
+
+#define USAGE "./loader <syscall> <errno>"
+
+int main(int argc, char *argv[])
+{
+	struct bpf_object *obj;
+	struct bpf_program *prog;
+	struct bpf_link *link = NULL;
+	struct bpf_map *errno_map, *pid_map;
+	struct rex_obj *rex_obj;
+	unsigned long errno_to_inject;
+	int pid, key = 0;
+	LIBBPF_OPTS(bpf_ksyscall_opts, opts);
+
+	if (argc < 3) {
+		fprintf(stderr, USAGE);
+		return 1;
+	}
+
+	errno_to_inject = -strtoul(argv[2], NULL, 10);
+
+	rex_set_debug(1);
+
+	rex_obj = rex_obj_load(EXE);
+	if (!rex_obj) {
+		fprintf(stderr, "rex_obj_load failed\n");
+		return 1;
+	}
+
+	obj = rex_obj_get_bpf(rex_obj);
+	if (!obj) {
+		fprintf(stderr, "rex_obj_get_bpf failed\n");
+		return 1;
+	}
+
+	prog = bpf_object__find_program_by_name(obj, "err_injector");
+	if (!prog) {
+		fprintf(stderr, "bpf_object__find_program_by_name failed\n");
+		return 1;
+	}
+
+	opts.retprobe = 0;
+	link = bpf_program__attach_ksyscall(prog, argv[1], &opts);
+	if (libbpf_get_error(link)) {
+		fprintf(stderr, "ERROR: bpf_program__attach failed\n");
+		link = NULL;
+		return 1;
+	}
+
+	errno_map = bpf_object__find_map_by_name(obj, "errno_map");
+	if (libbpf_get_error(errno_map)) {
+		fprintf(stderr, "ERROR: Could not find map: errno_map\n");
+		goto cleanup;
+	}
+
+	if (bpf_map__update_elem(errno_map, &key, sizeof(key), &errno_to_inject,
+				 sizeof(errno_to_inject), BPF_ANY) < 0) {
+		fprintf(stderr, "ERROR: updating errno_map failed\n");
+		goto cleanup;
+	}
+
+	pid_map = bpf_object__find_map_by_name(obj, "pid_map");
+	if (libbpf_get_error(pid_map)) {
+		fprintf(stderr, "ERROR: Could not find map: pid_map\n");
+		goto cleanup;
+	}
+
+	pid = fork();
+
+	if (pid < 0) {
+		perror("fork");
+	} else if (!pid) {
+		pid = getpid();
+		if (bpf_map__update_elem(pid_map, &key, sizeof(key), &pid,
+					 sizeof(pid), BPF_ANY) < 0) {
+			fprintf(stderr, "ERROR: updating pid_map failed\n");
+		}
+
+		bpf_link__destroy(link);
+
+		execl("./userapp", "./userapp", NULL);
+		perror("executing userapp failed");
+		return 1;
+	}
+
+	wait(NULL);
+
+cleanup:
+	bpf_link__destroy(link);
+	return 0;
+}

samples/error_injection/.cargo/config.toml
@@ -1,0 +1,14 @@
+[build]
+target = "x86_64-unknown-linux-gnu"
+rustflags = [
+  "-Cno-redzone",
+  "-Ccode-model=kernel",
+  "-Cforce-unwind-tables=n",
+  "-Csymbol-mangling-version=v0",
+  "-Ccodegen-units=1",
+  "-Ctarget-feature=-sse,-sse2,-sse3,-ssse3,-sse4.1,-sse4.2,-avx,-avx2,+soft-float",
+]
+
+[unstable]
+build-std = ["core"]
+build-std-features = ["compiler-builtins-mem"]

samples/error_injection/.gitignore
@@ -1,0 +1,1 @@
+target

samples/error_injection/Cargo.toml
@@ -1,0 +1,20 @@
+[package]
+name = "error_injection"
+version = "0.1.0"
+edition = "2021"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[dependencies]
+
+[dependencies.rex]
+path = "../../rex"
+
+[profile.dev]
+panic = "abort"
+debug = false
+
+[profile.release]
+panic = "abort"
+debug = false
+lto = true

samples/error_injection/Makefile
@@ -1,0 +1,52 @@
+LINUX ?= $(HOME)/linux
+
+LINUX_INC = ${LINUX}/usr/include
+LIBBPF_DIR = $(abspath ${LINUX}/tools/lib/bpf)
+LIBREX_DIR := $(shell if [ -d "/rex-kernel/librex" ]; then echo "/rex-kernel/librex"; else echo `realpath ../../librex`; fi)
+
+RUST_FLAGS = -Funsafe_code -Clink-arg=-nostartfiles -Cenable_rex
+CARGO_FLAGS =
+
+CC = gcc
+
+CFLAGS += -O2 -march=native -pipe -std=gnu11
+CFLAGS += -ffunction-sections -fdata-sections -fno-semantic-interposition
+LDFLAGS += -fuse-ld=mold -Wl,--as-needed -Wl,-O1 -Wl,--gc-sections
+
+LOADER_INC += -I${LINUX_INC} -I${LIBBPF_DIR} -I${LIBREX_DIR}
+LOADER_LDFLAGS = -L${LIBBPF_DIR} -L${LIBREX_DIR} -lbpf -lrex -Wl,-rpath=${LIBBPF_DIR} -Wl,-rpath=${LIBREX_DIR}
+
+V ?= 0
+
+ifeq ($(V),1)
+CARGO_FLAGS += -v
+endif
+
+ifeq ($(V),2)
+CARGO_FLAGS += -vv
+endif
+
+all: target/x86_64-unknown-linux-gnu/release/error_injection loader userapp
+
+target/x86_64-unknown-linux-gnu/debug/error_injection: Cargo.toml ./src/*.rs
+	cargo rustc ${CARGO_FLAGS} -- ${RUST_FLAGS}
+
+mir:
+	cargo rustc ${CARGO_FLAGS} -- ${RUST_FLAGS} --emit=mir
+
+llvm-ir:
+	cargo rustc ${CARGO_FLAGS} -- ${RUST_FLAGS} --emit=llvm-ir
+
+target/x86_64-unknown-linux-gnu/release/error_injection: Cargo.toml ./src/*.rs
+	cargo rustc ${CARGO_FLAGS} --release -- ${RUST_FLAGS}
+
+loader: loader.c
+	${CC} ${LOADER_INC} ${CFLAGS} ${LDFLAGS} -o $@ $< ${LOADER_LDFLAGS}
+
+event-trigger: event-trigger.c
+	${CC} -I${LINUX_INC} ${CFLAGS} ${LDFLAGS} -o $@ $<
+
+clean:
+	rm -rf loader event-trigger ./src/linux ./src/stub.rs
+	cargo clean
+

samples/error_injection/src/main.rs
@@ -1,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+use rex::kprobe::kprobe;
+use rex::map::RexArrayMap;
+use rex::pt_regs::PtRegs;
+use rex::rex_kprobe;
+use rex::rex_map;
+use rex::Result;
+
+#[allow(non_upper_case_globals)]
+#[rex_map]
+static pid_map: RexArrayMap<i32> = RexArrayMap::new(1, 0);
+
+#[allow(non_upper_case_globals)]
+#[rex_map]
+static errno_map: RexArrayMap<u64> = RexArrayMap::new(1, 0);
+
+#[rex_kprobe]
+pub fn err_injector(obj: &kprobe, ctx: &mut PtRegs) -> Result {
+    let current = obj.bpf_get_current_task().ok_or(0)?;
+    obj.bpf_map_lookup_elem(&pid_map, &0u32)
+        .filter(|x| **x == current.get_pid())
+        .ok_or(0)?;
+    let errno = *obj.bpf_map_lookup_elem(&errno_map, &0u32).ok_or(0)?;
+
+    obj.bpf_override_return(ctx, errno);
+
+    Ok(0)
+}

samples/error_injection/userapp.c
@@ -1,0 +1,11 @@
+#define _DEFAULT_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/unistd.h>
+
+int main(void)
+{
+	if (syscall(__NR_dup, 0) < 0) {
+		perror("dup");
+	}
+}

commit 7f38ca26ec250ceac98753aba0a5e2445e6626ca

samples/error_injection: add missing newline in usage string
Fixes: a03f2d091aa4 ("samples: add new syscall error injection sample")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

samples/error_injection/loader.c
@@ -9,7 +9,7 @@
 
 #define EXE "./target/x86_64-unknown-linux-gnu/release/error_injection"
 
-#define USAGE "./loader <syscall> <errno>"
+#define USAGE "./loader <syscall> <errno>\n"
 
 int main(int argc, char *argv[])
 {