tianyin/rex

commit 6b1f7224619e8564c137f65ffac3bf6e1c79cb88

rex: update ringbuf API + add test (does not work yet)
Signed-off-by: MinhPhan8803 <minhnp2@illinois.edu>

rex/src/map.rs
@@ -68,8 +68,13 @@ pub struct IUHashMap<K, V> {
 
 #[repr(C)]
 pub struct IURingBuf {
-    map: IUMapHandle<BPF_MAP_TYPE_RINGBUF, (), ()>,
+    map_type: u32,
+    max_size: u32,
+    map_flag: u32,
+    pub(crate) kptr: *mut (),
 }
+
+unsafe impl Sync for IURingBuf {}
 
 #[repr(C)]
 pub struct IUStack<V> {
@@ -131,37 +136,39 @@ impl<'a, V> IUArray<V> {
 
 impl IURingBuf {
     pub const fn new(ms: u32, mf: u32) -> IURingBuf {
-        IURingBuf {
-            map: IUMapHandle::new(ms, mf),
+        IURingBuf { map_type: BPF_MAP_TYPE_RINGBUF, max_size: ms, map_flag: mf, kptr: ptr::null_mut() }
+    }
+
+    pub fn reserve<T>(
+        &'static self,
+        submit_by_default: bool,
+        value: T
+    ) -> Option<IURingBufEntry<T>> {
+        let data: *mut T = bpf_ringbuf_reserve(&self, 0);
+        if data.is_null() {
+            None
+        } else {
+            unsafe { data.write(value); };
+            Some(IURingBufEntry { data: unsafe {
+                 { &mut *data }
+            }, submit_by_default, has_used: false })
         }
     }
 
-    pub fn reserve(
-        &'static self,
-        size: u64,
-        submit_by_default: bool,
-    ) -> Option<IURingBufEntry> {
-        bpf_ringbuf_reserve(&self.map, size, 0).map(|data| IURingBufEntry {
-            data,
-            submit_by_default,
-            has_used: false,
-        })
-    }
-
     pub fn available_bytes(&'static self) -> Option<u64> {
-        bpf_ringbuf_query(&self.map, BPF_RB_AVAIL_DATA as u64)
+        bpf_ringbuf_query(&self, BPF_RB_AVAIL_DATA as u64)
     }
 
     pub fn size(&'static self) -> Option<u64> {
-        bpf_ringbuf_query(&self.map, BPF_RB_RING_SIZE as u64)
+        bpf_ringbuf_query(&self, BPF_RB_RING_SIZE as u64)
     }
 
     pub fn consumer_position(&'static self) -> Option<u64> {
-        bpf_ringbuf_query(&self.map, BPF_RB_CONS_POS as u64)
+        bpf_ringbuf_query(&self, BPF_RB_CONS_POS as u64)
     }
 
     pub fn producer_position(&'static self) -> Option<u64> {
-        bpf_ringbuf_query(&self.map, BPF_RB_PROD_POS as u64)
+        bpf_ringbuf_query(&self, BPF_RB_PROD_POS as u64)
     }
 }
 
@@ -213,13 +220,13 @@ impl<V> IUQueue<V> {
     }
 }
 
-pub struct IURingBufEntry<'a> {
-    data: &'a mut [u8],
+pub struct IURingBufEntry<'a, T> {
+    data: &'a mut T,
     submit_by_default: bool,
     has_used: bool,
 }
 
-impl<'a> IURingBufEntry<'a> {
+impl<'a, T> IURingBufEntry<'a, T> {
     pub fn submit(mut self) {
         self.has_used = true;
         bpf_ringbuf_submit(self.data, 0)
@@ -229,9 +236,13 @@ impl<'a> IURingBufEntry<'a> {
         self.has_used = true;
         bpf_ringbuf_discard(self.data, 0)
     }
+
+    pub fn write(&mut self, value: T) {
+        *self.data = value
+    }
 }
 
-impl<'a> core::ops::Drop for IURingBufEntry<'a> {
+impl<'a, T> core::ops::Drop for IURingBufEntry<'a, T> {
     fn drop(&mut self) {
         if !self.has_used {
             if self.submit_by_default {

rex/src/base_helper.rs
@@ -7,7 +7,7 @@ use crate::per_cpu::this_cpu_read;
 use crate::random32::bpf_user_rnd_u32;
 use crate::stub;
 use crate::utils::{to_result, Result};
-use core::mem::MaybeUninit;
+use core::mem::{MaybeUninit, self};
 // use crate::timekeeping::*;
 
 use core::intrinsics::unlikely;
@@ -316,36 +316,27 @@ pub(crate) fn bpf_ringbuf_reserve(
     map: &'static RexRingBuf,
     size: u64,
     flags: u64,
-) -> Option<&mut [u8]> {
+) -> *mut T {
     let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
     if unlikely(map_kptr.is_null()) {
-        return None;
+        return core::ptr::null_mut();
     }
 
-    let data = unsafe { stub::bpf_ringbuf_reserve(map_kptr, size, flags) };
+    let data = unsafe { stub::bpf_ringbuf_reserve(map_kptr, mem::size_of::<T>() as u64, flags) };
 
-    if data.is_null() {
-        None
-    } else {
-        unsafe {
-            Some(core::slice::from_raw_parts_mut(
-                data as *mut u8,
-                size as usize,
-            ))
-        }
-    }
+    data as *mut T
 }
 
-pub(crate) fn bpf_ringbuf_submit(data: &mut [u8], flags: u64) {
-    unsafe { stub::bpf_ringbuf_submit(data.as_mut_ptr() as *mut (), flags) }
+pub(crate) fn bpf_ringbuf_submit<T>(data: &mut T, flags: u64) {
+    unsafe { stub::bpf_ringbuf_submit(data as *mut T as *mut (), flags) }
 }
 
-pub(crate) fn bpf_ringbuf_discard(data: &mut [u8], flags: u64) {
-    unsafe { stub::bpf_ringbuf_discard(data.as_mut_ptr() as *mut (), flags) }
+pub(crate) fn bpf_ringbuf_discard<T>(data: &mut T, flags: u64) {
+    unsafe { stub::bpf_ringbuf_discard(data as *mut T as *mut (), flags) }
 }
 
-pub(crate) fn bpf_ringbuf_query<const MT: bpf_map_type>(
-    map: &'static IUMapHandle<MT, (), ()>,
+pub(crate) fn bpf_ringbuf_query(
+    map: &'static IURingBuf,
     flags: u64,
 ) -> Option<u64> {
     let map_kptr = unsafe { core::ptr::read_volatile(&map.kptr) };
@@ -486,17 +477,17 @@ macro_rules! base_helper_defs {
         }
 
         #[inline(always)]
-        pub fn bpf_ringbuf_reserve<const MT: bpf_map_type>(
+        pub fn bpf_ringbuf_reserve<T>(
             &self,
             map: &'static RexRingBuf,
             size: u64,
             flags: u64,
-        ) -> Option<&mut [u8]> {
-            crate::base_helper::bpf_ringbuf_reserve(map, size, flags)
+        ) -> *mut T {
+            crate::base_helper::bpf_ringbuf_reserve(map, flags)
         }
 
         #[inline(always)]
-        pub fn bpf_ringbuf_submit(&self, data: &mut [u8], flags: u64) {
+        pub fn bpf_ringbuf_submit<T>(&self, data: &mut T, flags: u64) {
             crate::base_helper::bpf_ringbuf_submit(data, flags)
         }
     };

samples/map_test_2/src/main.rs
@@ -19,6 +19,9 @@ static STACK: IUStack<i64> = IUStack::new(256, 0);
 #[rex_map]
 static QUEUE: IUQueue<i64> = IUQueue::new(256, 0);
 
+// #[rex_map]
+// static RINGBUF: IURingBuf = IURingBuf::new(4096, 0);
+
 fn map_test_hash(obj: &tracepoint) -> Result {
     let key: u32 = 0;
 
@@ -157,6 +160,34 @@ fn map_test_queue(obj: &tracepoint) -> Result {
     Ok(0)
 }
 
+/*
+fn map_test_ringbuf(obj: &tracepoint) -> Result {
+    bpf_printk!(obj, "Map Testing Ring Buffer Start\n");
+    let pid = if let Some(task) = obj.bpf_get_current_task() {
+        task.get_pid()
+    } else {
+        -1
+    };
+    bpf_printk!(obj, "Rust program triggered from PID %llu\n", pid as u64);
+
+    bpf_printk!(obj, "Available bytes in ringbuf: %llu\n", RINGBUF.available_bytes().unwrap());
+
+    let entry = match RINGBUF.reserve::<i64>(true, pid as i64) {
+        None => {
+            bpf_printk!(obj, "Unable to reserve ringbuf.\n");
+            return Err(0);
+        }
+        Some(entry) => entry,
+    };
+
+    entry.submit();
+
+    bpf_printk!(obj, "Available bytes in ringbuf: %llu\n", RINGBUF.available_bytes().unwrap());
+
+    Ok(0)
+}
+*/
+
 #[inline(always)]
 fn iu_prog1_fn(obj: &tracepoint, _: tp_ctx) -> Result {
     map_test_hash(obj).map_err(|e| {
@@ -171,6 +202,10 @@ fn iu_prog1_fn(obj: &tracepoint, _: tp_ctx) -> Result {
         bpf_printk!(obj, "map_test2 failed with %lld.\n", e as u64);
         e
     })?;
+    // map_test_ringbuf(obj).map_err(|e| {
+    //     bpf_printk!(obj, "map_test2 failed with %lld.\n", e as u64);
+    //     e
+    // })?;
     map_test_queue(obj).map_err(|e| {
         bpf_printk!(obj, "map_test2 failed with %lld.\n", e as u64);
         e

commit 4a26bf076787cf7d50802505372dfe9cd6cb4e9e

rex/map: fix broken RexRingBuf ABI
Rex maps always need to have the following layout for librex to
recognize them:

  #[repr(C)]
  pub struct RexMapHandle {
      map_type: u32,
      key_size: u32,
      val_size: u32,
      max_size: u32,
      map_flag: u32,
      pub(crate) kptr: *mut (),
      // Some other zero-sized markers
  }

RexRingBuf is previously missing the key_size and val_size fields.
Adding them to be compliant to the ABI.

Side note: RingBufs are still experimental and we currently do not even
have a user in the tree. It is likely the code is still broken somewhere
else.

Fixes: 6b1f7224619e ("rex: update ringbuf API + add test (does not work yet)")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

rex/src/map.rs
@@ -60,6 +60,8 @@ pub type RexQueue<V> = RexMapHandle<BPF_MAP_TYPE_QUEUE, (), V>;
 #[repr(C)]
 pub struct RexRingBuf {
     map_type: u32,
+    key_size: u32,
+    val_size: u32,
     max_size: u32,
     map_flag: u32,
     pub(crate) kptr: *mut (),
@@ -107,6 +109,8 @@ impl RexRingBuf {
     pub const fn new(ms: u32, mf: u32) -> RexRingBuf {
         RexRingBuf {
             map_type: BPF_MAP_TYPE_RINGBUF,
+            key_size: 0,
+            val_size: 0,
             max_size: ms,
             map_flag: mf,
             kptr: ptr::null_mut(),