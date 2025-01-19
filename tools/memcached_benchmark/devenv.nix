c98571e02c1bcbf402199e1437ecc0a62577c311
Ruowen Qin <ruowenq2@illinois.edu>
2025-01-19 16:41:08 +0800

    tools/memcached_benchmark: use devenv for rustc version
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

---
+++ tools/memcached_benchmark/devenv.nix
@@ -0,0 +1,58 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+    channel = "nightly";
+    components = [
+      "cargo"
+      "rust-src"
+      "rustc"
+     "clippy"
+      "rust-analyzer"
+      "rustfmt"
+    ];
+    enable = true;
+  };
+
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+    rustc --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
