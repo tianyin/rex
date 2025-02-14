3753ce6bac36efc5acb4c842a7cc72c20c3fb2f3
Ruowen Qin <ruowenq2@illinois.edu>
2025-02-14 13:26:29 +0800

    nix: update flake and remove unused overlays
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- flake.nix
+++ flake.nix
@@ -7,23 +7,23 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      # overlays = [
-      #   (self: super: {
-      #     bindgen = super.bindgen.overrideAttrs (oldAttrs: {
-      #       src = super.fetchCrate {
-      #         pname = "bindgen-cli";
-      #         version = "0.68.1";
-      #         sha256 = "sha256-5fwJq1WsL3IEcVUjsyqKdQU8VufbbPk6TglwJg3C1Gw=";
-      #       };
-      #     });
-      #   })
-      # ];
-
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         # overlays = overlays;
       };
+
+      wrapCC = cc: pkgs.wrapCCWith {
+        inherit cc;
+        extraBuildCommands = ''
+          # Remove the line that contains "-nostdlibinc"
+          sed -i 's|-nostdlibinc||g' "$out/nix-support/cc-cflags"
+          echo " -resource-dir=${pkgs.llvmPackages.clang}/resource-root" >> "$out/nix-support/cc-cflags"
+        '';
+      };
+
+      wrappedClang = wrapCC pkgs.llvmPackages.clang.cc;
+      lib = nixpkgs.lib;
 
       # Use unwrapped clang & lld to avoid warnings about multi-target usage
       rexPackages = with pkgs; [
@@ -32,19 +32,23 @@
         bc
         binutils
         bison
-        elfutils
+        cmake
+        diffutils
+        elfutils.dev
         fakeroot
         flex
         gcc
+        glibc.dev
         getopt
         gnumake
         libelf
         ncurses
-        openssl
+        openssl.dev
         pahole
         pkg-config
-        xz
+        xz.dev
         zlib
+        zlib.dev
 
         ninja
         rust-bindgen
@@ -52,14 +56,27 @@
         strace
         zstd
         eza
-        zlib
+        linuxKernel.packages.linux_latest_libre.perf
 
         # Clang kernel builds
-        llvmPackages.clang
-        llvmPackages.bintools
+        # llvmPackages.clang
+        # llvmPackages.clang
+        wrappedClang
+        # llvmPackages.libcxxStdenv
+        lld
+        mold
+        # llvmPackages.bintools
+
+        qemu
+        busybox
+        perf-tools
 
         # for llvm/Demangle/Demangle.h
         libllvm
+        libllvm.dev
+        libgcc
+        libclang.lib
+        libclang.dev
 
 
         bear # generate compile commands
@@ -81,14 +98,28 @@
         openssh # q-script ssh support
       ];
 
-      fhs = pkgs.buildFHSEnv {
-        name = "rex-env";
-        targetPkgs = pkgs: rexPackages;
-        runScript = "./scripts/start.sh";
-        profile = ''
-          export NIX_ENFORCE_NO_NATIVE=0
-        '';
-      };
+      # (pkgs.buildFHSEnv.override { stdenv = pkgs.llvmPackages.stdenv; })
+      fhs = (pkgs.buildFHSEnv.override { stdenv = pkgs.llvmPackages.stdenv; })
+        {
+          name = "rex-env";
+          targetPkgs = pkgs: rexPackages;
+          runScript = "./scripts/start.sh";
+
+          # NIX_CFLAGS_COMPILE = lib.strings.makeLibraryPath [ ] + " -isystem ${pkgs.libclang.lib}/lib/clang/19/include";
+          # If you want clang in /usr/bin/clang etc. inside the chroot:
+          # export LD_LIBRARY_PATH=${pkgs.libgcc.lib}/lib:$LD_LIBRARY_PATH
+          # export LIBCLANG_PATH="${pkgs.libclang.lib}/lib/libclang.so"
+          extraShellInit = ''
+            # Symlink the wrapped clang binaries into /usr/bin
+          '';
+
+            # export PATH=${wrappedClang}/bin:"$PATH"
+          profile = ''
+            export LD_LIBRARY_PATH="${pkgs.libclang.lib}/lib/clang/19/include:$LD_LIBRARY_PATH"
+            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib/libclang.so"
+            export NIX_ENFORCE_NO_NATIVE=0
+          '';
+        };
     in
     {
       devShells."${system}" = {
@@ -105,7 +136,7 @@
           hardeningDisable = [ "all" ];
 
 
-            # export LIBCLANG_PATH="${pkgs.llvmPackages_19.libclang.lib}/lib/libclang.so"
+          # export LIBCLANG_PATH="${pkgs.llvmPackages_19.libclang.lib}/lib/libclang.so"
           shellHook = ''
             echo "loading rex env"
             source ./scripts/env.sh
