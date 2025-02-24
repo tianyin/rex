d07c0c6daf6513610fdcaddaf5a20fb9f16eb236
Ruowen Qin <ruowenq2@illinois.edu>
2025-02-24 09:51:09 +0800

    meson: fix samples Makefile and env.sh script
    Signed-off-by: Ruowen Qin <ruowenq2@illinois.edu>

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,5 @@
 export LINUX=$(realpath ./linux)
-if [[ -z "./rust/dist/bin" ]]; then
+if [[ -f "./rust/dist/bin/rustc" ]]; then
 	export PATH=$(realpath ./rust/dist/bin):$PATH
 fi
 export RUST_BACKTRACE=1
