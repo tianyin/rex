tianyin/rex

commit 447eba6cc89a6161bed4704910a24f5e9c0a0447

rex/panic: enable formatted panic message via rex::log::LogBuf
This allows us to report much better panic messages, such as for
out-of-bound accesses.

Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

rex/src/panic.rs
@@ -1,7 +1,9 @@
 //! Implementation of various routines/frameworks related to EH/termination
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
 
+use crate::log::LogBuf;
 use crate::per_cpu::this_cpu_ptr_mut;
 use crate::stub;
 
@@ -173,17 +175,10 @@ fn panic(info: &PanicInfo) -> ! {
 
     unsafe { CleanupEntries::cleanup_all() };
 
-    // Print the msg
-    let mut msg = [0u8; 128];
-    let args = info.message();
-    // Only works in the most trivial case: no format args
-    if let Some(s) = args.as_str() {
-        let len = core::cmp::min(msg.len() - 1, s.len());
-        msg[..len].copy_from_slice(&(*s).as_bytes()[..len]);
-        msg[len] = 0u8;
-    } else {
-        let s = "Rust program panicked\n\0";
-        msg[..s.len()].copy_from_slice(s.as_bytes());
+    let mut buf = LogBuf::new();
+    if write!(&mut buf, "{}", info).is_err() {
+        write!(&mut buf, "unknown rust panic");
     }
-    unsafe { stub::rex_landingpad(msg.as_ptr()) }
+
+    unsafe { stub::rex_landingpad() }
 }

rex/src/stub.rs
@@ -103,7 +103,7 @@ unsafe extern "C" {
     /// `__nocfi noinline void notrace __noreturn rex_landingpad(char *msg)`
     ///
     /// The in-kernel panic landingpad for panic recovery
-    pub(crate) fn rex_landingpad(msg: *const u8) -> !;
+    pub(crate) fn rex_landingpad() -> !;
 
     /// `long bpf_spin_lock(struct bpf_spin_lock *lock)`
     pub(crate) fn bpf_spin_lock(lock: *mut bpf_spin_lock) -> i64;

commit 8537e177c95bf04c7eb666a56e8f2611998bd581

rex/{log,panic}: reset log buffer if panic message does not fit
Fixes: 447eba6cc89a ("rex/panic: enable formatted panic message via rex::log::LogBuf")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

rex/src/log.rs
@@ -19,6 +19,13 @@ impl LogBuf {
         };
         Self { buf, off: 0 }
     }
+
+    /// Reset the offset of the buffer, i.e. discarding all contents not yet
+    /// sent to the kernel
+    #[inline(always)]
+    pub(crate) fn reset(&mut self) {
+        self.off = 0;
+    }
 }
 
 impl Write for LogBuf {

rex/src/panic.rs
@@ -176,7 +176,9 @@ fn panic(info: &PanicInfo) -> ! {
 
     let mut buf = LogBuf::new();
     if write!(&mut buf, "{}", info).is_err() {
-        write!(&mut buf, "unknown rust panic");
+        buf.reset();
+        // This string always fits in the buffer, so we ignore the Result
+        write!(&mut buf, "unknown rust panic").ok();
     }
 
     unsafe { ffi::rex_landingpad() }