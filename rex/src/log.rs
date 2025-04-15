tianyin/rex

commit bcb32b53c7fc0913079dc7b2d22b4e9693a60b6c

rex: introduce rust-style printk
Previously we had C-style bpf_trace_printk() for logging into the trace
ring buffer. This implementation can only take 3 positional arguments
(we did not support bpf_trace_vprintk()) and all of them has to be
explicitly casted to u64 due to Rust's strict type system, resulting
verbose boilerplate code.

While the Rust-style format operation seemingly requires dynamic
allocation, the only feature it actually requires is the ability to
write (i.e. via the core::fmt::Write trait) formatted arguments (in the
form of string slices) to some buffer.  Therefore, we introduce a new
LogBuf struct that abstracts the static per-CPU log buffer of rex in the
kernel and implement Write, which copies the formatted slices into the
buffer and properly null-terminates the buffer content.

By building on top of LogBuf, this commit further implements user-facing
rex_printk() and rex_trace_printk() APIs that allows idiomatic-rust
format strings.

Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

rex/src/log.rs
@@ -1,0 +1,69 @@
+use core::fmt::{self, Arguments, Write};
+use core::mem;
+
+use crate::bindings::uapi::linux::errno::E2BIG;
+use crate::per_cpu::this_cpu_ptr_mut;
+use crate::stub;
+
+/// An abstraction over the in-kernel per-cpu log buffer
+/// This struct implements [`Write`], and therefore can be used for formatting
+pub(crate) struct LogBuf {
+    buf: &'static mut [u8],
+    off: usize,
+}
+
+impl LogBuf {
+    /// Construct a new `LogBuf` from the kernel log buffer on the current CPU
+    pub(crate) fn new() -> Self {
+        let buf = unsafe {
+            &mut *this_cpu_ptr_mut(&raw mut stub::rex_log_buf).as_mut_slice()
+        };
+        Self { buf, off: 0 }
+    }
+}
+
+impl Write for LogBuf {
+    /// Writes a string slice into the kernel buffer on this CPU
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        let input_len = s.len();
+        let available = self.buf.len() - self.off - 1;
+
+        // Make sure we have enough space
+        if input_len > available {
+            return Err(fmt::Error);
+        }
+
+        // Copy and null-terminated the buf
+        let end = self.off + input_len;
+        self.buf[self.off..end].copy_from_slice(s.as_bytes());
+        self.buf[end] = 0;
+
+        // Update the write offset
+        self.off = end;
+
+        Ok(())
+    }
+}
+
+/// Prints a message defined by `args` to the TraceFS file
+/// `/sys/kernel/debug/tracing/trace`.
+pub fn rex_trace_printk(args: fmt::Arguments<'_>) -> crate::Result {
+    // Format and write message to the per-cpu buf, then print it out
+    write!(&mut LogBuf::new(), "{}", args).map_err(|_| -(E2BIG as i32))?;
+    unsafe {
+        stub::rex_trace_printk();
+    }
+
+    Ok(0)
+}
+
+/// `println`-style convenience macro for [`rex_trace_printk`].
+/// Different from `println`, this macro produces the value of [`crate::Result`]
+/// from [`rex_trace_printk`]
+#[macro_export]
+macro_rules! rex_printk {
+    ($($arg:tt)*) => {{
+        $crate::rex_trace_printk(format_args!($($arg)*))
+    }};
+}
+pub use rex_printk;

rex/build.py
@@ -19,7 +19,8 @@ k_structs = ['task_struct', 'tk_read_base', 'seqcount_raw_spinlock_t',
              'bpf_perf_event_data_kern']
 
 bindgen_kernel_cmd = '''bindgen %s --allowlist-type="%s"
---allowlist-var="(___GFP.*|CONFIG_.*)" --opaque-type xregs_state
+--allowlist-var="(___GFP.*|CONFIG_.*|MAX_BPRINTF_BUF)"
+--opaque-type xregs_state
 --opaque-type desc_struct --opaque-type arch_lbr_state --opaque-type
 local_apic --opaque-type alt_instr --opaque-type x86_msi_data --opaque-type
 x86_msi_addr_lo --opaque-type kunit_try_catch --opaque-type spinlock

rex/librexstub/lib.c
@@ -32,6 +32,7 @@ KSYM_FUNC(bpf_ringbuf_query)
 KSYM_FUNC(bpf_ktime_get_ns)
 KSYM_FUNC(bpf_ktime_get_boot_ns)
 KSYM_FUNC(bpf_ktime_get_coarse_ns)
+KSYM_FUNC(rex_trace_printk)
 
 // Global variables
 unsigned long jiffies;
@@ -41,3 +42,4 @@ unsigned long rex_stack_ptr;
 void *pcpu_hot;
 unsigned char rex_termination_state;
 unsigned long this_cpu_off;
+char *rex_log_buf;

rex/src/lib.rs
@@ -29,6 +29,7 @@ pub mod xdp;
 mod base_helper;
 mod bindings;
 mod debug;
+mod log;
 mod panic;
 mod per_cpu;
 mod random32;
@@ -67,4 +68,5 @@ define_prog_entry!(xdp);
 define_prog_entry!(sched_cls);
 
 pub use bindings::uapi::*;
+pub use log::rex_trace_printk;
 pub use utils::Result;

rex/src/stub.rs
@@ -2,7 +2,7 @@
 use core::ffi::{c_char, c_uchar, VaList};
 
 use crate::bindings::linux::kernel::{
-    bpf_perf_event_data_kern, pcpu_hot, sk_buff, xdp_buff,
+    bpf_perf_event_data_kern, pcpu_hot, sk_buff, xdp_buff, MAX_BPRINTF_BUF,
 };
 use crate::bindings::uapi::linux::bpf::{bpf_perf_event_value, bpf_spin_lock};
 use crate::panic::{CleanupEntry, ENTRIES_SIZE};
@@ -180,6 +180,9 @@ unsafe extern "C" {
 
     /// u64 bpf_ringbuf_query(void *ringbuf, u64 flags)
     pub(crate) fn bpf_ringbuf_query(ringbuf: *mut (), flags: u64) -> u64;
+
+    /// void rex_trace_printk(void)
+    pub(crate) fn rex_trace_printk();
 }
 
 /// Global variables
@@ -229,4 +232,7 @@ unsafe extern "C" {
     ///
     /// Offset on the current
     pub(crate) static this_cpu_off: u64;
+
+    /// DEFINE_PER_CPU(char[MAX_BPRINTF_BUF], rex_log_buf) = { 0 };
+    pub(crate) static mut rex_log_buf: [u8; MAX_BPRINTF_BUF as usize];
 }

commit c336fec317c1bee4c0f39db37cf0bf25d3d9aa5a

rex: add missing termination check in rex_trace_printk()
Fixes: bcb32b53c7fc ("rex: introduce rust-style printk")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

rex/src/log.rs
@@ -1,5 +1,6 @@
 use core::fmt::{self, Write};
 
+use crate::base_helper::termination_check;
 use crate::bindings::uapi::linux::errno::E2BIG;
 use crate::ffi;
 use crate::per_cpu::this_cpu_ptr_mut;
@@ -56,9 +57,7 @@ impl Write for LogBuf {
 pub fn rex_trace_printk(args: fmt::Arguments<'_>) -> crate::Result {
     // Format and write message to the per-cpu buf, then print it out
     write!(&mut LogBuf::new(), "{}", args).map_err(|_| -(E2BIG as i32))?;
-    unsafe {
-        ffi::rex_trace_printk();
-    }
+    termination_check!(unsafe { ffi::rex_trace_printk() });
 
     Ok(0)
 }