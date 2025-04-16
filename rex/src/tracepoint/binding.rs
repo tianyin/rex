e544699d6ce2e7be2ebbeb9932e427ce57e2bd8a
MinhPhan8803 <nhatminhvinh8803@gmail.com>
2025-04-15 23:07:58 -0500

    rex: remove context type and conversion code from tracepoint struct
    Signed-off-by: MinhPhan8803 <nhatminhvinh8803@gmail.com>

--- rex/src/tracepoint/binding.rs
+++ rex/src/tracepoint/binding.rs
@@ -1,6 +1,6 @@
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct SyscallsEnterOpenArgs {
+pub struct SyscallsEnterOpenCtx {
     unused: u64,
     pub syscall_nr: i64,
     pub filename_ptr: i64,
@@ -10,7 +10,18 @@ pub struct SyscallsEnterOpenArgs {
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct SyscallsExitOpenArgs {
+pub struct SyscallsEnterOpenatCtx {
+    unused: u64,
+    pub syscall_nr: i64,
+    pub dfd: i64,
+    pub filename_ptr: i64,
+    pub flags: i64,
+    pub mode: i64,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct SyscallsExitOpenCtx {
     unused: u64,
     pub syscall_nr: i64,
     pub ret: i64,
@@ -18,7 +29,15 @@ pub struct SyscallsExitOpenArgs {
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct SyscallsEnterDupArgs {
+pub struct SyscallsExitOpenatCtx {
+    unused: u64,
+    pub syscall_nr: i64,
+    pub ret: i64,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct SyscallsEnterDupCtx {
     unused: u64,
     pub syscall_nr: i64,
     pub fildes: u64,
