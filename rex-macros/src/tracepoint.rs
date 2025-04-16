b73e6aa58705ed2d2227204575168e297d4a3e5a
MinhPhan8803 <nhatminhvinh8803@gmail.com>
2025-04-15 19:54:56 -0500

    rex_macros: modify rex_tracepoint to allow directly naming the context
    Signed-off-by: MinhPhan8803 <nhatminhvinh8803@gmail.com>

--- rex-macros/src/tracepoint.rs
+++ rex-macros/src/tracepoint.rs
@@ -1,12 +1,11 @@
 use proc_macro2::TokenStream;
-use proc_macro_error::OptionExt;
+use proc_macro_error::{abort_call_site, OptionExt};
 use quote::{format_ident, quote};
-use syn::{parse2, ItemFn, Result};
+use syn::{parse2, FnArg, ItemFn, Result, Type};
 
 use crate::args::parse_string_args;
 
 pub(crate) struct TracePoint {
-    tp_type: Option<String>,
     name: Option<String>,
     item: ItemFn,
 }
@@ -23,17 +22,46 @@ impl TracePoint {
         let args = parse_string_args(attrs)?;
 
         let name = pop_string_args!(args, "name");
-        let tp_type = pop_string_args!(args, "tp_type");
 
-        Ok(TracePoint {
-            tp_type,
-            name,
-            item,
-        })
+        Ok(TracePoint { name, item })
     }
 
     pub(crate) fn expand(&self) -> Result<TokenStream> {
         let fn_name = self.item.sig.ident.clone();
+
+        // get context type and corresponding tp_type name
+        let FnArg::Typed(context_arg) =
+            self.item.sig.inputs.last().unwrap().clone()
+        else {
+            abort_call_site!("Program needs non-self arguments");
+        };
+        let Type::Reference(context_type_ref) = *context_arg.ty else {
+            abort_call_site!("Context type needs to be behind a reference");
+        };
+        if context_type_ref
+            .lifetime
+            .expect_or_abort("Context reference needs to be static")
+            .ident
+            != "static"
+        {
+            abort_call_site!("Context reference needs to be static");
+        }
+        let context_type = match *context_type_ref.elem {
+            Type::Verbatim(t) => parse2(t).unwrap(),
+            Type::Path(p) => p.path.segments.last().unwrap().clone().ident,
+            _ => {
+                abort_call_site!("Tracepoint context needs to be a literal type or a path to such")
+            }
+        };
+        let context_type_name = format!("{}", context_type);
+        let ctx_variant = format_ident!(
+            "{}",
+            context_type_name
+                .strip_suffix("Args")
+                .expect_or_abort("Not valid context type")
+        );
+
+        // other tracepoint pieces
         let item = &self.item;
         let function_name = format!("{}", fn_name);
         let prog_ident =
@@ -46,27 +74,31 @@ impl TracePoint {
             )
         );
 
-        let tp_type_str = self
-            .tp_type
-            .as_ref()
-            .expect_or_abort("Please provide valid tracepoint attached point")
-            .as_str();
-
-        let tp_type = match tp_type_str {
-            "Void" => quote!(tp_type::Void),
-            "SyscallsEnterOpen" => quote!(tp_type::SyscallsExitOpen),
+        let tp_type = match ctx_variant.to_string().as_str() {
+            "SyscallsEnterOpen" => quote!(tp_type::SyscallsEnterOpen),
             "SyscallsExitOpen" => quote!(tp_type::SyscallsExitOpen),
-            _ => panic!("Please provide valid tp_type"),
+            "SyscallsEnterDup" => quote!(tp_type::SyscallsEnterDup),
+            _ => abort_call_site!("Please provide valid context type"),
         };
+
+        let wrapper_name = format_ident!("{}_wrapper", fn_name);
 
         let function_body_tokens = quote! {
             #[inline(always)]
             #item
 
+            #[inline(always)]
+            fn #wrapper_name(obj: &tracepoint, ctx_wrapper: tp_ctx) -> Result {
+                let tp_ctx::#ctx_variant(ctx) = ctx_wrapper else {
+                    return Err(0);
+                };
+                #fn_name(obj, ctx)
+            }
+
             #[used]
             #[unsafe(link_section = #attached_name)]
             static #prog_ident: tracepoint =
-                tracepoint::new(#fn_name, #function_name, #tp_type);
+                tracepoint::new(#wrapper_name, #function_name, #tp_type);
         };
 
         Ok(function_body_tokens)
