tianyin/rex

commit 80a2b488195f15a7157d016d2c263f0b84d8d622

samples/trace_event: use the new TaskStruct::get_comm signature
Signed-off-by: MinhPhan8803 <nhatminhvinh8803@gmail.com>

samples/trace_event/src/main.rs
@@ -15,7 +15,7 @@ pub const TASK_COMM_LEN: usize = 16;
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct KeyT {
-    pub comm: [i8; TASK_COMM_LEN],
+    pub comm: [u8; TASK_COMM_LEN],
     pub kernstack: u32,
     pub userstack: u32,
 }
@@ -50,7 +50,10 @@ fn rex_prog1(obj: &perf_event, ctx: &bpf_perf_event_data) -> Result {
 
     obj.bpf_get_current_task()
         .map(|t| {
-            t.get_comm(&mut key.comm);
+            let prog_name = t.get_comm().unwrap_or_default();
+            key.comm
+                .copy_from_slice(&prog_name.to_bytes()[..TASK_COMM_LEN]);
+            key.comm[TASK_COMM_LEN - 1] = 0;
             0u64
         })
         .ok_or(0i32)?;

commit 8959d1e4c7beeb2b4b6ffe15bb43b4184563577a

samples/trace_event: fix size mismatch when copying from task comm
Fixes: 80a2b488195f ("samples/trace_event: use the new TaskStruct::get_comm signature")
Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

samples/trace_event/src/main.rs
@@ -51,9 +51,8 @@ fn rex_prog1(obj: &perf_event, ctx: &bpf_perf_event_data) -> Result {
     obj.bpf_get_current_task()
         .map(|t| {
             let prog_name = t.get_comm().unwrap_or_default();
-            key.comm
-                .copy_from_slice(&prog_name.to_bytes()[..TASK_COMM_LEN]);
-            key.comm[TASK_COMM_LEN - 1] = 0;
+            key.comm[..prog_name.count_bytes() + 1]
+                .copy_from_slice(prog_name.to_bytes_with_nul());
             0u64
         })
         .ok_or(0i32)?;